2831809a6ee024f28c2f9102cfab07367436ef4b
Phil Cockfield <phil@cockfield.net>
2024-09-26 15:59:16 +1200

    Update mod.-unit.test.ts

--- code/sys.ui/ui-react/src/mod.-unit.test.ts
+++ code/sys.ui/ui-react/src/mod.-unit.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from './-test.ts';
 
 describe('Module', () => {
-  it('placeholder', () => {
+  it('🐷 placeholder', () => {
     expect(123).to.equal(123);
   });
 });
