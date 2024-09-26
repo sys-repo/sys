f6a493f9e5694ffe48625cf1059a7b70f791d544
Phil Cockfield <phil@cockfield.net>
2024-09-27 10:30:48 +1200

    🌳  (#2)
    * refs
    
    * Update mod.ts
    
    * Update deno.json
    
    * stub in tmp module (test)
    
    * deno.json/lock updates
    
    * Create notes-for-@coty.md
    
    * notes
    
    * tmp: suppress lint in CI

---
+++ code/sys.ui/ui-tmp/src/mod.-unit.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from './-test.ts';
+
+describe('Module', () => {
+  it('🐷 placeholder', () => {
+    expect(123).to.equal(123);
+  });
+});
---
+++ code/sys.ui/ui-tmp/src/server/mod.ts
@@ -0,0 +1,14 @@
+import * as esbuild from 'npm:esbuild';
+import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader';
+
+const result = await esbuild.build({
+  plugins: [...denoPlugins()],
+  entryPoints: ['https://deno.land/std@0.185.0/bytes/mod.ts'],
+  outfile: './dist/bytes.esm.js',
+  bundle: true,
+  format: 'esm',
+});
+
+console.log('result', result);
+
+esbuild.stop();
