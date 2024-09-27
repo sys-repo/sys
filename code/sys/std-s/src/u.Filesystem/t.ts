031d22e8acc7c84f3d46e0be48a356476b391b78
Phil Cockfield <phil@cockfield.net>
2024-09-27 20:16:16 +1200

    type defs

--- code/sys/std-s/src/u.Filesystem/t.ts
+++ code/sys/std-s/src/u.Filesystem/t.ts
@@ -14,3 +14,13 @@ export type Glob = {
    */
   dir(...subdir: (string | undefined)[]): Glob;
 };
+
+/**
+ * Copy all files in a directory.
+ */
+export type CopyDir = (sourceDir: string, targetDir: string) => Promise<void>;
+
+/**
+ * Delete a directory (and it's contents).
+ */
+export type RemoveDir = (path: string, options?: { dry?: boolean; log?: boolean }) => Promise<void>;
