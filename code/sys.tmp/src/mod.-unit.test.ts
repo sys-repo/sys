3840c9a024696ac5fac7e79e56463744b0f9d426
Phil Cockfield <phil@cockfield.net>
2024-09-27 22:17:18 +1200

    Update mod.-unit.test.ts

--- code/sys.tmp/src/mod.-unit.test.ts
+++ code/sys.tmp/src/mod.-unit.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from './-test.ts';
 
 describe('Module', () => {
-  it('🐷 placeholder', () => {
+  it('🐷 placeholder → /dev/null', () => {
     expect(123).to.equal(123);
   });
 });
