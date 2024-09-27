b762a1baba0075ccdd7281bc29b4eed1d590f10a
Phil Cockfield <phil@cockfield.net>
2024-09-27 13:01:05 +1200

    tmp: rename entry filename

--- code/sys.ui/ui-tmp/src/server/esbuild.ts
+++ code/sys.ui/ui-tmp/src/server/esbuild.ts
@@ -3,8 +3,9 @@ import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader';
 
 const result = await esbuild.build({
   plugins: [...denoPlugins()],
-  entryPoints: ['https://deno.land/std@0.185.0/bytes/mod.ts'],
-  outfile: './dist/bytes.esm.js',
+  entryPoints: ['https://deno.land/std@0.185.0/bytes/mod.ts', './src/ui/Components.tsx'],
+  // outfile: './dist/bytes.esm.js',
+  outdir: './dist',
   bundle: true,
   format: 'esm',
 });
