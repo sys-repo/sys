3d498bdafb9e1ac367ca593495032029d2aba730
Phil Cockfield <phil@cockfield.net>
2024-09-29 18:35:42 +1300

    docs

--- code/sys/std/src/u.Id/mod.ts
+++ code/sys/std/src/u.Id/mod.ts
@@ -1,5 +1,14 @@
 import { Id } from './Id.ts';
-
 export { Id };
+
+/**
+ * A CUID ← "Secure, collision-resistant id"
+ * https://github.com/paralleldrive/cuid2
+ */
 export const cuid = Id.cuid;
+
+/**
+ * A compact collision resistent ID.
+ * https://github.com/paralleldrive/cuid2
+ */
 export const slug = Id.slug;
