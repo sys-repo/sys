c2405d980a741e7d64e8370f0936ff452ae81081
Phil Cockfield <phil@cockfield.net>
2024-10-01 18:14:04 +1300

    Update u.log.ts

--- code/sys.ui/ui-vite/src/server/ViteCmd/u.log.ts
+++ code/sys.ui/ui-vite/src/server/ViteCmd/u.log.ts
@@ -5,6 +5,7 @@ export const Log = {
    * Startup log.
    */
   entry(pkg: t.Pkg, input: t.StringPath) {
+    input = input.replace(/^\.\//, ''); // trim leading "./" relative prefix (reduce visual noise).
     console.info();
     console.info(c.gray(`Module:       ${c.white(c.bold(pkg.name))} ${pkg.version}`));
     console.info(c.brightGreen(`entry point:  ${c.gray(input)}`));
