bf14faab7ba10e22db0701cb92235b6f2811c0ca
Phil Cockfield <phil@cockfield.net>
2024-10-01 16:26:34 +1300

    simplify entry script (example)

--- code/sys.ui/ui-vite/scripts/-start.vite-dev.ts
+++ code/sys.ui/ui-vite/scripts/-start.vite-dev.ts
@@ -5,9 +5,4 @@
 import { Pkg, ViteCmd } from '../src/server/mod.ts';
 
 const input = './src/-test/vite.sample-1/index.html';
-
-// console.info();
-// ViteCmd.Log.entry(Pkg, input);
-
-const svc = ViteCmd.dev({ input, Pkg });
-await svc.whenReady();
+ViteCmd.dev({ Pkg, input });
