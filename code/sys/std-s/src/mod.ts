4c5173ad61163325be5d5b77a9b62bf509bd9d42
Phil Cockfield <phil@cockfield.net>
2024-10-02 11:06:51 +1300

    refactor folder

--- code/sys/std-s/src/mod.ts
+++ code/sys/std-s/src/mod.ts
@@ -11,5 +11,5 @@ export { Color, DateTime, Http, Pkg, Semver, c, rx } from './common.ts';
 
 export { Env } from './u.Env/mod.ts';
 export { Fs, Path } from './u.Fs/mod.ts';
-export { HttpServer } from './u.HttpServer/mod.ts';
+export { HttpServer } from './u.Server.Http/mod.ts';
 export { Cmd } from './u.Process/mod.ts';
--- code/sys/std-s/src/types.ts
+++ code/sys/std-s/src/types.ts
@@ -4,6 +4,6 @@
  */
 export type * from './u.Env/t.ts';
 export type * from './u.Fs/t.ts';
-export type * from './u.HttpServer/t.ts';
 export type * from './u.Process/t.ts';
 export type * from './u.Server.Auth/t.ts';
+export type * from './u.Server.Http/t.ts';
