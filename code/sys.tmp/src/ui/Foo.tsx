4cf24bac86f8955d70525259740d8145ae4b044f
Phil Cockfield <phil@cockfield.net>
2024-10-03 20:15:41 +1300

    Update Foo.tsx

--- code/sys.tmp/src/ui/Foo.tsx
+++ code/sys.tmp/src/ui/Foo.tsx
@@ -5,6 +5,7 @@ import { Pkg } from '../common.ts';
  * Sample Component.
  */
 export type FooProps = { enabled?: boolean };
+
 export const Foo: FC<FooProps> = (_props = {}) => {
   const text = `${Pkg.name}@${Pkg.version}:Foo`;
   return <code>{text}</code>;
