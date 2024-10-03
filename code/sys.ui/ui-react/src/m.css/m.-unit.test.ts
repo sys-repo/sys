d14131ea5cfe9023967cab22cd006d59a27aa595
Phil Cockfield <phil@cockfield.net>
2024-10-04 06:32:40 +1300

    Create m.-unit.test.ts

---
+++ code/sys.ui/ui-react/src/m.css/m.-unit.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect, type t } from '../-test.ts';
+import { Css } from './mod.ts';
+
+describe('Css', () => {
+  it('exists', () => {
+    expect(Css).to.to.exist;
+  });
+});
