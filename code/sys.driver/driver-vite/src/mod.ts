f4cfd2b3b0a1b95accc4ec57bb29d99d4b599e32
Phil Cockfield <phil@cockfield.net>
2024-10-03 15:43:11 +1300

    code sample

--- code/sys.driver/driver-vite/src/mod.ts
+++ code/sys.driver/driver-vite/src/mod.ts
@@ -3,8 +3,15 @@
  * Tools for working with Vite as a bundler and dev-harness within Deno.
  *
  * @example
+ * Within the `vite.config.ts` in the root of the module:
  * ```ts
- * import { ViteConfig } from '@sys/driver-vite';
+ * import { ViteProcess } from '@sys/driver-vite';
+ * import { defineConfig } from 'vite';
+ * import reactPlugin from 'vite-plugin-react-swc';
+ *
+ * export default defineConfig((_ctx) => {
+ *   return { plugins: [reactPlugin(), ViteProcess.workspacePlugin()] };
+ * });
  * ```
  */
 export { Pkg, c } from './common.ts';
