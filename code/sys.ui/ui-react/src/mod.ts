801ea77560110df03aa0cf16e238532063df08d7
Phil Cockfield <phil@cockfield.net>
2024-10-08 17:14:05 +1300

    public: @sys/ui-react

--- code/sys.ui/ui-react/src/mod.ts
+++ code/sys.ui/ui-react/src/mod.ts
@@ -5,6 +5,15 @@
  * @example
  * ```ts
  * import { Color, Style, css } from '@sys/ui-react/style';
+ *
+ * const theme = Color.theme(props.theme);
+ * const styles = {
+ *   base: css({ color: Color.alpha(theme.fg, 0.3) }),
+ * };
+ *
+ * <div {...css(styles.base, props.style)}>
+ *   <div>{`👋 Hello World`}</div>
+ * </div>
  * ```
  */
 export { Pkg } from './common.ts';
