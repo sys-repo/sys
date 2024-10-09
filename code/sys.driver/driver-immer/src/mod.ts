b94c8f6a3d7de63a819e6eef07730e5c327305b2
Phil Cockfield <phil@cockfield.net>
2024-10-09 13:35:08 +1300

    stub in module: @sys/driver-immer

---
+++ code/sys.driver/driver-immer/src/mod.ts
@@ -0,0 +1,10 @@
+/**
+ * @module
+ * Tools for working with Immer immutability library.
+ *
+ * @example
+ * ```ts
+ * import { Pkg, Json, Patch, PatchState } from '@sys/driver-immer';
+ * ```
+ */
+export { Pkg } from './common.ts';
