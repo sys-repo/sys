fdc40f06658ff960f5d557cfcd07dafedaaad010
Phil Cockfield <phil@cockfield.net>
2024-10-10 12:49:40 +1300

    timestamp types

--- code/sys/types/src/types/t.Time.ts
+++ code/sys/types/src/types/t.Time.ts
@@ -1,2 +1,20 @@
 /* A number representing milliseconds. */
 export type Msecs = number;
+
+/* Number represening seconds. */
+export type Secs = number;
+
+/* Number represening minutes. */
+export type Mins = number;
+
+/**
+ * Represents a Unix Epoch timestamp in seconds. The Unix Epoch time is
+ * the number of seconds that have elapsed since January 1, 1970, 00:00:00 UTC.
+ */
+export type UnixEpoch = Secs;
+
+/**
+ * Represents a Unix timestamp in milliseconds. This offers higher precision
+ * than UnixEpoch by including milliseconds since January 1, 1970, 00:00:00 UTC.
+ */
+export type UnixTimestamp = Msecs;
