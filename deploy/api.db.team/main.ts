20be4eccdc4ec66aed1885da45b440a32b39e0c6
Phil Cockfield <phil@cockfield.net>
2024-10-10 20:53:28 +1300

    deploy tweaks

--- deploy/api.db.team/main.ts
+++ deploy/api.db.team/main.ts
@@ -1,4 +1,5 @@
 import './u.env-patch.ts';
-import { DenoCloud } from '@sys/driver-deno/cloud/server';
+import { DenoCloud } from 'jsr:@sys/driver-deno@0.0.6/cloud/server';
+import { default as pkg } from './deno.json' with { type: 'json' };
 
-DenoCloud.serve(8080);
+DenoCloud.serve(8080, pkg);
