62c4facc12cd2f9cf14b1b73400af13b0a9ee696
Phil Cockfield <phil@cockfield.net>
2024-10-10 14:44:55 +1300

    more types

--- code/sys/types/src/types/mod.ts
+++ code/sys/types/src/types/mod.ts
@@ -1,8 +1,10 @@
 export type * from './ext.ts';
 
+export type * from './t.Boolean.ts';
 export type * from './t.Dispose.ts';
 export type * from './t.Error.ts';
 export type * from './t.Immutable.ts';
+export type * from './t.Io.ts';
 export type * from './t.Json.ts';
 export type * from './t.Number.ts';
 export type * from './t.Object.ts';
@@ -10,17 +12,9 @@ export type * from './t.Observable.ts';
 export type * from './t.Pkg.ts';
 export type * from './t.Readonly.ts';
 export type * from './t.String.ts';
+export type * from './t.Text.ts';
 export type * from './t.Theme.ts';
 export type * from './t.Time.ts';
+export type * from './t.Utility.ts';
 
-/**
- * Common
- */
-export type Falsy = false | 0 | '' | null | undefined;
-
-/**
- * A response value from a function that is not used by the function
- * (allows for artibrary return values signalling to the consumer
- * that whatever is returned will not be used.)
- */
-export type IgnoredResponse = any | Promise<any>;
+export type * from './t.ts';
--- code/sys/types/src/types/t.Time.ts
+++ code/sys/types/src/types/t.Time.ts
@@ -7,6 +7,12 @@ export type Secs = number;
 /* Number represening minutes. */
 export type Mins = number;
 
+/* Number represening hours. */
+export type Hours = number;
+
+/* Number represening days. */
+export type Days = number;
+
 /**
  * Represents a Unix Epoch timestamp in seconds. The Unix Epoch time is
  * the number of seconds that have elapsed since January 1, 1970, 00:00:00 UTC.
@@ -18,3 +24,8 @@ export type UnixEpoch = Secs;
  * than UnixEpoch by including milliseconds since January 1, 1970, 00:00:00 UTC.
  */
 export type UnixTimestamp = Msecs;
+
+/**
+ * The kind of unit of time a number represents.
+ */
+export type TimeUnit = 'msec' | 'ms' | 'sec' | 's' | 'min' | 'm' | 'hour' | 'h' | 'day' | 'd';
