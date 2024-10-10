8616684336b4c6cfdc3b1c9de5df704aff0fcf59
Phil Cockfield <phil@cockfield.net>
2024-10-10 20:53:14 +1300

    report error

--- code/sys/std-s/src/m.Server.Http/u.ts
+++ code/sys/std-s/src/m.Server.Http/u.ts
@@ -8,6 +8,8 @@ export const print: t.HttpServerLib['print'] = (addr, pkg) => {
   const port = c.bold(c.brightCyan(String(addr.port)));
   const host = c.cyan(`http://localhost:${port}/`);
   if (pkg) {
+    pkg.name = pkg.name ?? '<🐷 deno.json:name Not Found 🐷>';
+    pkg.version = pkg.version ?? '<🐷 deno.json:version Not Found 🐷>';
     console.info();
     console.info(c.gray(`Module   ${c.white(pkg.name)} ${pkg.version}`));
     console.info(c.gray(`         ${host}`));
