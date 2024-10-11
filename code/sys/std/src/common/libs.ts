e9605d8d2664a9d92ed207b0d6d34a6cd72b131e
Phil Cockfield <phil@cockfield.net>
2024-10-11 14:04:48 +1300

    swap module: "ramda" → "rambda" (TS types)

--- code/sys/std/src/common/libs.ts
+++ code/sys/std/src/common/libs.ts
@@ -1,7 +1,7 @@
 /**
  * @ext
  */
-import { clone, equals, toString, uniq, uniqBy } from 'ramda';
+import { clone, equals, toString, uniq, uniqBy } from 'rambda';
 
 /* Ramda functional utilities. */
 export const R = { clone, equals, toString, uniq, uniqBy } as const;
