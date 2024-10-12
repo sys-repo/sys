d293777a8457053b0eb5010d3f19b269821022ab
Phil Cockfield <phil@cockfield.net>
2024-10-13 12:55:19 +1300

    Update -tmp.ts

--- scripts/-tmp.ts
+++ scripts/-tmp.ts
@@ -12,8 +12,8 @@ const table = Cli.table(['Foo', 'Bar']).indent(2);
 table.push();
 table.push(['123456', 'abc']);
 table.push(['333', 'Hello World 👋']);
-table.render();
-console.info();
+// table.render();
+// console.info();
 
 /**
  * Nylas
@@ -55,8 +55,10 @@ export async function sampleOpenAI() {
 // await sampleNylas();
 // await sampleOpenAI();
 
-const m = Cli.args(Deno.args);
-console.log('m', m);
+// const m = Cli.args(Deno.args);
+// console.log('m', m);
+
+await Cli.Prompts.Confirm.prompt('From node to deno?');
 
 /**
  * TODO 🐷 arts passed for try/test/ci
