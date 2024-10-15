a9aca5fe3932791a9181bfe730f08727ac504c45
Phil Cockfield <phil@cockfield.net>
2024-10-15 17:06:11 +1300

    docs

--- code/sys.ui/ui-react/src/ui/m.Dom/mod.ts
+++ code/sys.ui/ui-react/src/ui/m.Dom/mod.ts
@@ -2,7 +2,28 @@
  * @module
  * Tools for working directly with the raw DOM ("document object model")
  * without involvement with any other UI libraries.
+ *
+ * @example
+ * To work with local-storage in a strongly typed manner:
+ *
+ * ```ts
+ * import { LocalStorage } from '@sys/ui-react/ui/dom';
+ *
+ * type T = { count: number; msg?: string };
+ * const localstore = LocalStorage<T>('my-namespace-prefix');
+ * const local = localstore.object({ count: 0 });
+ *
+ * expect(local.count).to.eql(0);
+ * expect(local.msg).to.eql(undefined);
+ *
+ * local.count = 123;
+ * local.msg = 'hello';
+ *
+ * expect(local.count).to.eql(123);
+ * expect(local.msg).to.eql('hello');
+ * ```
  */
 export { File } from './m.File/mod.ts';
 export { Keyboard } from './m.Keyboard/mod.ts';
 export { UserAgent } from './m.UserAgent/mod.ts';
+export { LocalStorage } from './m.LocalStorage/mod.ts';
