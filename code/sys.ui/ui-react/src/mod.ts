a6c814aabc99550294768992e291b6a80ad3994e
Phil Cockfield <phil@cockfield.net>
2024-10-15 17:37:10 +1300

    docs

--- code/sys.ui/ui-react/src/mod.ts
+++ code/sys.ui/ui-react/src/mod.ts
@@ -1,20 +1,33 @@
 /**
  * @module
- * Tools for ...
+ * Tools for working with browser based UI (react bindings).
  *
  * @example
+ * Import pattern for a simple React component
+ *
  * ```ts
+ * import type { t } from '@sys/ui-react/t';
  * import { Color, Style, css } from '@sys/ui-react/style';
  *
- * const theme = Color.theme(props.theme);
- * const styles = {
- *   base: css({ color: Color.alpha(theme.fg, 0.3) }),
- * };
+ * export type FooProps = {
+ *   theme?: t.CommonTheme;
+ *   style?: t.CssValue;
+ * }
  *
- * <div {...css(styles.base, props.style)}>
- *   <div>{`👋 Hello World`}</div>
- * </div>
+ * export const Foo: React.FC<FooProps> = (props) => {
+ *   const theme = Color.theme(props.theme);
+ *   const styles = {
+ *     base: css({ color: Color.alpha(theme.fg, 0.3) }),
+ *   };
+ *
+ *   return (
+ *     <div {...css(styles.base, props.style)}>
+ *       <div>{`👋 Hello World`}</div>
+ *     </div>
+ *   );
+ * };
  * ```
  */
 export { Pkg } from './common.ts';
 export { Color, Style, css } from './u/mod.ts';
+export { Keyboard, LocalStorage, UserAgent } from './ui/m.Dom/mod.ts';
