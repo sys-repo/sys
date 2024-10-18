222e552b68617fe3f47088c161f86fa1f95b2f7c
Phil Cockfield <phil@cockfield.net>
2024-10-18 14:53:44 +1300

    rambda libs

--- code/sys/std/src/common/libs.ts
+++ code/sys/std/src/common/libs.ts
@@ -1,9 +1,21 @@
+import type * as t from './t.ts';
+
 /**
  * @ext
  */
-import { clone, equals, mergeDeepRight, prop, sortBy, toString, uniq, uniqBy } from 'rambda';
+import { equals, mergeDeepRight, prop, sortBy, toString, uniq, uniqBy } from 'rambda';
+import { clone } from 'ramda'; // NB: clone from "rambda" causes errors on circular-references.
 
 /**
  * Ramda functional utilities.
  */
-export const R = { clone, equals, mergeDeepRight, prop, sortBy, toString, uniq, uniqBy } as const;
+export const R: t.RLib = {
+  clone,
+  equals,
+  mergeDeepRight,
+  prop,
+  sortBy,
+  toString,
+  uniq,
+  uniqBy,
+};
--- code/sys/std/src/common/t.ts
+++ code/sys/std/src/common/t.ts
@@ -1,1 +1,18 @@
+import type * as Rambda from 'rambda';
 export * from '../types.ts';
+
+type R = typeof Rambda;
+
+/**
+ * Subset of common rambda helpers.
+ */
+export type RLib = {
+  readonly clone: R['clone'];
+  readonly equals: R['equals'];
+  readonly mergeDeepRight: R['mergeDeepRight'];
+  readonly prop: R['prop'];
+  readonly sortBy: R['sortBy'];
+  readonly toString: R['toString'];
+  readonly uniq: R['uniq'];
+  readonly uniqBy: R['uniqBy'];
+};
