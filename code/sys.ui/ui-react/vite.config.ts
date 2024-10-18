2cfe24eef00f32b6e66c8738ea969c5c1b00dbb9
Phil Cockfield <phil@cockfield.net>
2024-10-18 14:57:12 +1300

    Update vite.config.ts

--- code/sys.ui/ui-react/vite.config.ts
+++ code/sys.ui/ui-react/vite.config.ts
@@ -1,16 +1,17 @@
 import reactPlugin from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 
 import { workspacePlugin } from '@sys/driver-vite';
-import { Style } from './src/u/m.Style/mod.ts';
+import { Style } from '@sys/ui-dom/style/react';
 
 /**
  * Vite setup.
  */
 export default defineConfig(async (_ctx) => {
   const workspace = await workspacePlugin();
+
   // workspace.ws?.log({}); // 🐷
 
   const react = reactPlugin(Style.plugin.emotion());
-  return { plugins: [react, workspace] };
+  return { plugins: [react, workspace] } as UserConfig;
 });
