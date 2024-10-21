3529933db3b2051ef9feb157628b5bf17a18f9eb
Phil Cockfield <phil@cockfield.net>
2024-10-22 12:02:33 +1300

    Update main.tsx

--- code/sys.driver/driver-vite/src/-test/vite.sample-1/main.tsx
+++ code/sys.driver/driver-vite/src/-test/vite.sample-1/main.tsx
@@ -1,9 +1,9 @@
+import { createRoot } from 'react-dom/client';
 import { Pkg } from '../pkg.ts';
 console.info('Pkg', Pkg);
 
 /**
  * Sample: render react component.
  */
-import { createRoot } from 'react-dom/client';
 const root = createRoot(document.getElementById('root'));
 root.render(<div>Hello World 👋</div>);
