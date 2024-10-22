9644fbb4f60ba8ad48b4dba06f0108ac9e8dba85
Phil Cockfield <phil@cockfield.net>
2024-10-22 14:47:02 +1300

    fix bar-loader import

--- code/sys.ui/ui-dev-harness/src/ui/m.Dev.Harness/ui/Spinners/BarSpinner.tsx
+++ code/sys.ui/ui-dev-harness/src/ui/m.Dev.Harness/ui/Spinners/BarSpinner.tsx
@@ -1,21 +1,37 @@
-import { BarLoader } from 'react-spinners';
+import { useEffect } from 'react';
 import { COLORS, css, type t } from '../common.ts';
+
+type BarLoaderProps = { color?: string; width?: number };
+type BarLoaderComponent = (prop: BarLoaderProps) => JSX.Element | null;
+let BarLoader: BarLoaderComponent | undefined;
 
 export type BarSpinnerProps = {
   style?: t.CssValue;
 };
 
 export const BarSpinner: React.FC<BarSpinnerProps> = (props) => {
+  useEffect(() => {
+    /**
+     * HACK: Errors when this file is parsed within Deno on the server
+     *       (because CJS not ESM (??))
+     *       Only import the component when within a browser.
+     */
+    if (!globalThis.window) return;
+    import('react-spinners').then((e) => (BarLoader = e.BarLoader));
+  });
+
   const width = 80;
   const styles = {
     base: css({
       position: 'relative',
-      width,
-      borderRadius: 10,
       overflow: 'hidden',
+      borderRadius: 10,
       opacity: 0.5,
+      width,
     }),
   };
+
+  if (!BarLoader) return null;
 
   return (
     <div {...css(styles.base, props.style)}>
