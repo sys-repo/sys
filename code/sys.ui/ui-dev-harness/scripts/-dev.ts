e6cb7b8134e32361664f8a155255445111211031
Phil Cockfield <phil@cockfield.net>
2024-10-23 12:45:04 +1300

    main entry point

--- code/sys.ui/ui-dev-harness/scripts/-dev.ts
+++ code/sys.ui/ui-dev-harness/scripts/-dev.ts
@@ -3,6 +3,6 @@
  */
 import { Pkg, Vite } from './common.ts';
 
-const input = './src/-test/vite.sample-1/index.html';
+const input = './src/-test/index.html';
 const server = await Vite.dev({ Pkg, input });
 await server.keyboard();
