cb71879da51a9e188fc29c873c10761b61fca5f9
Phil Cockfield <phil@cockfield.net>
2024-10-25 18:56:52 +1300

    Update -.test.ts

--- code/-tmpl/deno/src/-.test.ts
+++ code/-tmpl/deno/src/-.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, Pkg } from './-test.ts';
 
-describe(`test module: ${Pkg.name}@${Pkg.version}`, () => {
+describe(`Pkg: ${Pkg.name}@${Pkg.version}`, () => {
   it('🐷 placeholder', () => {
     expect(123).to.equal(123);
   });
