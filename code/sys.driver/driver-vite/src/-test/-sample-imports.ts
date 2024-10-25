15143e00297da10d4a2f6fd0a03c331aea036d22
Phil Cockfield <phil@cockfield.net>
2024-10-25 22:34:58 +1300

    docs

--- code/sys.driver/driver-vite/src/-test/-sample-imports.ts
+++ code/sys.driver/driver-vite/src/-test/-sample-imports.ts
@@ -1,5 +1,6 @@
 /**
- * @example
+ * @module
+ * Testing
  * Namespace: @std (deno)
  *            @sys (deno)
  *
@@ -8,30 +9,29 @@
  */
 const now = new Date();
 
-console.group('🌳 import: @std (deno)');
+/**
+ * TODO 🐷 move to @sys/tmp
+ */
 
-import * as path from '@std/path';
-console.info('@std/path', path);
-
-import * as datetime from '@std/datetime';
-console.info('@std/datetime', datetime, datetime.format(now, 'yyyy-MM-dd'));
-
-import * as async from '@std/async';
-console.info('@std/async', async);
-
-import * as semver from '@std/semver';
-console.info('@std/semver', semver);
-
-import * as testing from '@std/testing/bdd';
-console.info('@std/testing', testing);
-
-console.groupEnd();
+// console.group('🌳 import: @std (deno)');
+// import * as path from '@std/path';
+// import * as datetime from '@std/datetime';
+// import * as async from '@std/async';
+// import * as semver from '@std/semver';
+// import * as testing from '@std/testing/bdd';
+//
+// console.info('@std/path', path);
+// console.info('@std/datetime', datetime, datetime.format(now, 'yyyy-MM-dd'));
+// console.info('@std/async', async);
+// console.info('@std/semver', semver);
+// console.info('@std/testing', testing);
+// console.groupEnd();
 
 /**
  * Namespace: @sys
  */
 console.group('🌳 import: @sys/std');
-import { Args, D, IndexedDb, Path, Time, rx } from '@sys/std';
+import { Args, D, IndexedDb, Path, Time, rx, Semver } from '@sys/std';
 import { Str } from '@sys/text';
 
 console.info('@sys/std:rx', rx);
@@ -39,6 +39,7 @@ console.info('@sys/std:Path', Path);
 console.info('@sys/std:D (Date)', D, `"${D.format(now, 'E MMM do, yyyy')}"`);
 console.info('@sys/std:Time', Time);
 console.info('@sys/std:Args', Args, 'parsed:', Args.parse(['--foo', '--bar=baz', './file.txt']));
+console.info('@sys/std:Semver', Semver, Semver.parse('1.2.3'));
 console.info('@sys/std:IndexedDb', IndexedDb);
 console.info('@sys/text:Str', Str);
 console.info('@sys/text:Str.bytes', `"${Str.bytes(1337)}"`);
