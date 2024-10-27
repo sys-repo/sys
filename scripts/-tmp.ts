779281e1fa7f3d236d75cbc6dd32782105c8a30b
Phil Cockfield <phil@cockfield.net>
2024-10-27 20:10:07 +1300

    moved to: ./-tmp.OpenAI-prompt.ts

--- scripts/-tmp.ts
+++ scripts/-tmp.ts
@@ -1,22 +1,8 @@
 import { c, Cli, Env, Fs, Path } from '@sys/std-s';
 // import { Q } from './-tmp.quilibrium.ts';
 
-import { Q } from '@sys/driver-quilibrium';
-
 // const match = await Fs.glob().find('code/**/-test.*');
 // console.log('match', match);
-
-/**
- * OpenAI
- */
-
-import OpenAI from 'openai';
-const openai = new OpenAI();
-const completion = await openai.chat.completions.create({
-  model: 'gpt-4o',
-  messages: [{ role: 'user', content: 'write me a type for a haiku datastructure' }],
-});
-console.log('completion', completion);
 
 /**
  * Finish up.
