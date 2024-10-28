c9dcadc48ae5e25264e65897cd80213da54348ae
Phil Cockfield <phil@cockfield.net>
2024-10-28 15:58:29 +1300

    refactor: "dev" to own module

--- code/sys.driver/driver-vite/src/m.Vite/m.Vite.ts
+++ code/sys.driver/driver-vite/src/m.Vite/m.Vite.ts
@@ -1,7 +1,7 @@
 import { Plugin } from '../m.Vite.Plugin/mod.ts';
-import { Cmd, ViteConfig as Config, DEFAULTS, type t } from './common.ts';
-import { build } from './u.build.ts';
-import { keyboardFactory, Log, Wrangle } from './u.ts';
+import { ViteConfig as Config, type t } from './common.ts';
+import { build } from './u.vite.build.ts';
+import { dev } from './u.vite.dev.ts';
 
 /**
  * Tools for running Vite via commands issued to a child process.
@@ -11,37 +11,5 @@ export const Vite: t.ViteLib = {
   Plugin,
   common: Plugin.common,
   build,
-
-  /**
-   * Run the <vite:build> command.
-   * Long running processes (spawn → child process).
-   *
-   * Command:
-   *    $ vite dev --port=<1234>
-   */
-  async dev(input: t.ViteDevArgs): Promise<t.ViteProcess> {
-    const { port = DEFAULTS.port, silent = false, pkg } = input;
-    const { env, args, paths } = Wrangle.command(input, `dev --port=${port}`);
-    const url = `http://localhost:${port}/`;
-
-    if (!silent && pkg) Log.Entry.log(pkg, input.input);
-
-    const proc = Cmd.spawn({ args, env, silent });
-    const { dispose } = proc;
-    const keyboard = keyboardFactory({ pkg, paths, port, url, dispose });
-
-    const listen = async () => {
-      await keyboard();
-    };
-
-    await proc.whenReady();
-    return {
-      proc,
-      port,
-      url,
-      listen,
-      keyboard,
-      dispose,
-    };
-  },
+  dev,
 } as const;
