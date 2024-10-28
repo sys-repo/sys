1b2ad05d281e5f7b0c20827995e41a7522599d3d
Phil Cockfield <phil@cockfield.net>
2024-10-28 16:00:23 +1300

    pig

--- code/sys.driver/driver-vite/scripts/-build.ts
+++ code/sys.driver/driver-vite/scripts/-build.ts
@@ -2,7 +2,7 @@
  * Run in a child-process (hence the `-allow-run` requirement).
  */
 import { Vite } from '@sys/driver-vite';
-import { Pkg } from '../src/pkg.ts';
+import { pkg } from '../src/pkg.ts';
 
 const input = './src/-test/vite.sample-2/index.html';
 const bundle = await Vite.build({ pkg, input });
