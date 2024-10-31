4d298f9687cf43af692ea76c15721700e8da54f5
Phil Cockfield <phil@cockfield.net>
2024-11-01 11:35:34 +1300

    sample - calculate vault hash

--- code/sys.driver/driver-obsidian/scripts/-dir.ts
+++ code/sys.driver/driver-obsidian/scripts/-dir.ts
@@ -1,5 +1,13 @@
+import { Hash } from '@sys/std-s';
 import { Vault } from '@sys/driver-obsidian/s';
 
-const path = '/Users/phil/Documents/Notes';
+/**
+ * Sample Vaul/Directory monitoring.
+ */
+const path = '/Users/phil/Documents/Notes/tdb';
 const dir = await Vault.dir(path);
+
+const res = await Hash.Dir.compute(dir.path);
+console.log('Hash', res.hash, '\n');
+
 await dir.listen({ log: true });
