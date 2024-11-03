e1483f46167a14c938e85cbfb9d3d00fe8135bac
Phil Cockfield <phil@cockfield.net>
2024-11-04 12:11:15 +1300

    test HTTP - return text

--- code/sys/std/src/m.Testing.HttpServer/t.ts
+++ code/sys/std/src/m.Testing.HttpServer/t.ts
@@ -13,9 +13,13 @@ export type TestHttpServer = {
   /** Generate a new HTTP testing server. */
   server(defaultHandler?: Deno.ServeHandler): t.TestHttpServerInstance;
 
-  /** Convert the given value to a Response object. */
+  /** Convert the given JSON value to a Response object. */
   json(body: unknown): Response;
   json(req: Request, body: unknown): Response;
+
+  /** Convert the given text value to a Response object. */
+  text(body: unknown): Response;
+  text(req: Request, body: unknown): Response;
 };
 
 /**
