6c2d95ee4b2b25556979f4e6a52bddeb086c1a9c
Phil Cockfield <phil@cockfield.net>
2024-11-03 16:38:38 +1300

    tweak

--- code/sys.driver/driver-vite/src/m.Vite/u.log.ts
+++ code/sys.driver/driver-vite/src/m.Vite/u.log.ts
@@ -64,8 +64,8 @@ ${c.brightGreen(`entry:    ${c.gray(input)}`)}
       if (hash) {
         const algo = Hash.prefix(hash);
         const short = Hash.shorten(hash, [5, 5], true);
-        let uri = `${c.green('digest')}:${algo}:${short}`;
-        uri = `${uri.slice(0, -5)}${c.green(uri.slice(-5))}`;
+        const endHash = `#${short.slice(-5)}`;
+        const uri = `${c.green('digest')}:${algo}:${c.bold(c.green(endHash))}`;
         digest = c.gray(`${c.green('←')} ${uri}`);
       }
 
