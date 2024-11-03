1e088c1884e06bde1bf1cecbb0f816395093f236
Phil Cockfield <phil@cockfield.net>
2024-11-03 22:22:57 +1300

    manifest

--- deploy/slc.db.team/scripts/-build.ts
+++ deploy/slc.db.team/scripts/-build.ts
@@ -1,6 +1,11 @@
+import { Fs } from '@sys/std-s';
 import { Vite } from '@sys/driver-vite';
 import { pkg } from '../src/pkg.ts';
 
 const input = './src/-test/index.html';
 const bundle = await Vite.build({ pkg, input });
 console.info(bundle.toString({ pad: true }));
+const from = Fs.resolve('./src/manifest.json');
+const to = Fs.resolve('./dist/manifest.json');
+await Fs.ensureDir(Fs.dirname(to));
+await Deno.copyFile(from, to);
