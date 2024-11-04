5e6ac094c5b48a11145cc541200fa1d30bf02ac4
Phil Cockfield <phil@cockfield.net>
2024-11-04 19:36:20 +1300

    Num.round tests

--- code/sys/std/src/m.Value/-.test.ts
+++ code/sys/std/src/m.Value/-.test.ts
@@ -9,39 +9,4 @@ describe('Value', () => {
     expect(Value.round).to.equal(Num.round);
     expect(Value.isObject).to.equal(isObject);
   });
-
-  describe('Number', () => {
-    describe('round', () => {
-      it('should round to no decimal places by default', () => {
-        expect(Value.round(1.2345)).to.eql(1);
-        expect(Value.round(1.5)).to.eql(2);
-        expect(Value.round(1.4)).to.eql(1);
-      });
-
-      it('should round to the specified number of decimal places', () => {
-        expect(Value.round(1.2345, 2)).to.eql(1.23);
-        expect(Value.round(1.235, 2)).to.eql(1.24);
-        expect(Value.round(1.2345, 3)).to.eql(1.235);
-      });
-
-      it('should handle negative precision correctly', () => {
-        expect(Value.round(12345, -1)).to.eql(12350);
-        expect(Value.round(12345, -2)).to.eql(12300);
-      });
-
-      it('should round negative numbers correctly', () => {
-        expect(Value.round(-1.2345)).to.eql(-1);
-        expect(Value.round(-1.5)).to.eql(-1);
-        expect(Value.round(-1.51)).to.eql(-2);
-        expect(Value.round(-1.4)).to.eql(-1);
-        expect(Value.round(-1.2345, 2)).to.eql(-1.23);
-      });
-
-      it('should round zero correctly', () => {
-        expect(Value.round(0)).to.eql(0);
-        expect(Value.round(0.1234, 2)).to.eql(0.12);
-        expect(Value.round(0, -1)).to.eql(0);
-      });
-    });
-  });
 });
