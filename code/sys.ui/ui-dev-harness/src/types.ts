d35d4cea47efd058f89c2e6d928d7552a2178b50
Phil Cockfield <phil@cockfield.net>
2024-11-05 13:32:25 +1300

    refactor

--- code/sys.ui/ui-dev-harness/src/types.ts
+++ code/sys.ui/ui-dev-harness/src/types.ts
@@ -4,15 +4,8 @@
  */
 export type * from './t/mod.ts';
 
-export type * from './ui.use/t.ts';
-export type * from './ui/Harness.Host/t.ts';
-export type * from './ui/Harness.Panel.Debug/t.ts';
-export type * from './ui/Harness.Panel.Edge/t.ts';
-export type * from './ui/Harness/t.ts';
-export type * from './ui/ModuleList/t.ts';
-export type * from './ui/RenderCount/t.ts';
-export type * from './ui/Spinners/t.ts';
-
-export type * from './u/t.ts';
+export type * from './u/types.ts';
+export type * from './ui.use/types.ts';
+export type * from './ui/types.ts';
 
 export type { ColorConstants } from './common/c.COLORS.ts';
