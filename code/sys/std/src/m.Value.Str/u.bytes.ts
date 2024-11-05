f06c06683f673b4346a7e54662e0b1d33bde489b
Phil Cockfield <phil@cockfield.net>
2024-11-05 18:21:47 +1300

    Update u.bytes.ts

--- code/sys/std/src/m.Value.Str/u.bytes.ts
+++ code/sys/std/src/m.Value.Str/u.bytes.ts
@@ -3,8 +3,7 @@ import type { t } from './common.ts';
 
 export const bytes: t.FormatBytes = (num, options = {}) => {
   const { compact = false } = options;
-  if (typeof num !== 'number') return format(0);
-  let str = format(num, options);
+  let str = format(num ?? 0, options);
   if (compact) str = str.replace(/\s/g, '');
   return str;
 };
