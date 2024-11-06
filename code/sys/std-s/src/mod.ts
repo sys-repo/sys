8e3f0cce86e7d6b1ab24ef03e059687c745e4edb
Phil Cockfield <phil@cockfield.net>
2024-11-07 08:54:19 +1300

    Update mod.ts

--- code/sys/std-s/src/mod.ts
+++ code/sys/std-s/src/mod.ts
@@ -35,8 +35,8 @@ export {
 /**
  * Server only.
  *
- * NB: importing these libraries into JS that is bundled
- *     for the browser will fail.
+ * NB: importing these libraries into JS/ESM for bundling for
+ *     the browser (eg. via Vite) will fail.
  */
 export { Cli } from './m.Cli/mod.ts';
 export { Env } from './m.Env/mod.ts';
