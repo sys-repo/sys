0b9e05e0ffdb7b86a6945d5780996aaeff782c72
Phil Cockfield <phil@cockfield.net>
2024-11-07 12:36:30 +1300

    debugging

--- code/sys/std-s/src/m.Process/-Cmd.test.ts
+++ code/sys/std-s/src/m.Process/-Cmd.test.ts
@@ -17,6 +17,10 @@ describe('Cmd', () => {
     it('invoke → stdout', async () => {
       const args = ['eval', 'console.log("👋 hello world")'];
       const res = await Cmd.invoke({ args, silent: true });
+
+      console.log('res', res);
+      console.log('res.toString()', res.toString());
+
       expect(res.code).to.eql(0);
       expect(res.success).to.eql(true);
 
--- scripts/u.paths.ts
+++ scripts/u.paths.ts
@@ -6,34 +6,34 @@ export const Paths = {
     /**
      * @sys: standard libs.
      */
-    'code/sys/std',
+    // 'code/sys/std',
     'code/sys/std-s',
-    'code/sys/cmd',
-    'code/sys/testing',
-    'code/sys/text',
-    'code/sys/types',
+    // 'code/sys/cmd',
+    // 'code/sys/testing',
+    // 'code/sys/text',
+    // 'code/sys/types',
     
     /**
      * UI
      */
-    'code/sys.ui/ui-dev-harness',
-    'code/sys.ui/ui-dom',
-    'code/sys.ui/ui-react',
+    // 'code/sys.ui/ui-dev-harness',
+    // 'code/sys.ui/ui-dom',
+    // 'code/sys.ui/ui-react',
     
     /**
      * Drivers
      */
     // 'code/sys.driver/driver-automerge',
-    'code/sys.driver/driver-deno',
-    'code/sys.driver/driver-immer',
-    'code/sys.driver/driver-obsidian',
-    'code/sys.driver/driver-quilibrium',
-    'code/sys.driver/driver-vite',
+    // 'code/sys.driver/driver-deno',
+    // 'code/sys.driver/driver-immer',
+    // 'code/sys.driver/driver-obsidian',
+    // 'code/sys.driver/driver-quilibrium',
+    // 'code/sys.driver/driver-vite',
 
     /**
      * →| dev/null
      */
-    'code/sys.tmp',
+    // 'code/sys.tmp',
   ],
 
   workspace: Pkg.workspace
