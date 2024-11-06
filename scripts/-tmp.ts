57979e8aea50940f229b4dcef94b33bb8c7c7936
Phil Cockfield <phil@cockfield.net>
2024-11-07 09:17:09 +1300

    Update -tmp.ts

--- scripts/-tmp.ts
+++ scripts/-tmp.ts
@@ -1,17 +1,5 @@
 import { c, Cli, Env, Fs, Path, Hash } from '@sys/std-s';
 
-// const match = await Fs.glob().find('code/**/-test.*');
-// console.log('match', match);
-
-/**
- * Watch an Obsidian directory.
- */
-const dir = '/Users/phil/Documents/Notes/phil';
-const m = await Hash.Dir.compute(dir);
-
-console.log('m', m);
-console.log(`-------------------------------------------`);
-
 /**
  * Finish up.
  */
