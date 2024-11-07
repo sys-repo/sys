20e18917aa22f7730601b8b56bec97578e37e5f7
Phil Cockfield <phil@cockfield.net>
2024-11-08 10:40:12 +1300

    Update libs.ts

--- code/sys.driver/driver-vite/src/common/libs.ts
+++ code/sys.driver/driver-vite/src/common/libs.ts
@@ -10,11 +10,12 @@ export { keypress } from '@cliffy/keypress';
  * System
  */
 export { Denofile } from '@sys/driver-deno/runtime';
-export { Pkg, Str, Time, asArray, slug } from '@sys/std';
+export { Str, Time, asArray, slug } from '@sys/std';
 
 export { c } from '@sys/std-s';
 export { Cli } from '@sys/std-s/cli';
 export { Fs, Path } from '@sys/std-s/fs';
 export { Hash } from '@sys/std-s/hash';
 export { Net } from '@sys/std-s/network';
+export { Pkg } from '@sys/std-s/pkg';
 export { Cmd } from '@sys/std-s/process';
