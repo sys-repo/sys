9756f0603cdb1d2d5ea8a343b44004ed1ef9bb3b
Phil Cockfield <phil@cockfield.net>
2024-11-12 11:08:12 +1300

    refactor

--- code/sys.driver/driver-vitepress/src/m.VitePress/-.test.ts
+++ code/sys.driver/driver-vitepress/src/m.VitePress/-.test.ts
@@ -7,9 +7,9 @@ describe('Vitepress', () => {
     it('process: start → fetch(200) → dispose', async () => {
       const sample = await SAMPLE.setup();
       const { port, path } = sample;
-      const svc = await VitePress.dev({ port, path });
-      expect(svc.port).to.eql(port);
-      expect(svc.path).to.eql(path);
+      const server = await VitePress.dev({ port, path });
+      expect(server.port).to.eql(port);
+      expect(server.path).to.eql(path);
 
       await Testing.wait(1000); // NB: wait another moment for the vite-server to complete it's startup.
       console.info(); // NB: pad the output in the test-runner terminal. The "classic" Vite startup output.
@@ -18,17 +18,17 @@ describe('Vitepress', () => {
       const { signal } = controller;
       const timeout = Time.delay(5000, () => {
         controller.abort();
-        svc?.dispose();
+        server?.dispose();
       });
 
-      const res = await fetch(svc.url, { signal });
+      const res = await fetch(server.url, { signal });
       const html = await res.text();
 
       expect(res.status).to.eql(200);
       expect(html).to.include(`<script type="module"`);
       expect(html).to.include(`node_modules/.deno/vitepress@`);
 
-      await svc.dispose();
+      await server.dispose();
       timeout.cancel();
     });
   });
