896c3d148a69940cdda958f8a5fa053b470efedf
Phil Cockfield <phil@cockfield.net>
2024-11-12 19:44:34 +1300

    tweak

--- code/sys/std-s/src/m.Hash/m.Hash.Console.ts
+++ code/sys/std-s/src/m.Hash/m.Hash.Console.ts
@@ -4,13 +4,13 @@ export const Console: t.HashConsoleLib = {
   digest(input, options = {}) {
     if (!input) return '';
 
-    const { length = 4 } = options;
+    const { length = 6 } = options;
     const hash = Hash.toString(input);
 
     const algo = Hash.prefix(hash);
     const short = Hash.shorten(hash, length, true);
     const endHash = `#${short.slice(-length)}`;
-    const uri = `${c.green('digest')}:${algo}:${c.green(endHash)}`;
+    const uri = `${'digest'}:${algo}:${c.green(endHash)}`;
 
     return c.gray(uri);
   },
