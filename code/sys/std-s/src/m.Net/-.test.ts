d82a373605b6662ad80672eb404794de6b5d471a
Phil Cockfield <phil@cockfield.net>
2024-11-13 12:16:25 +1300

    make test more resilient

--- code/sys/std-s/src/m.Net/-.test.ts
+++ code/sys/std-s/src/m.Net/-.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '../-test.ts';
+import { describe, expect, it, Testing } from '../-test.ts';
 import { R } from './common.ts';
 import { Net, Port } from './mod.ts';
 
@@ -10,22 +10,28 @@ describe('Net', () => {
 
   describe('Net.Port', () => {
     it('Port.random()', () => {
-      const ports = [...Array(50)].map(() => Port.random());
-      expect(R.equals(R.uniq(ports), ports)).to.eql(true);
-      expect(ports.every((v) => typeof v === 'number')).to.eql(true);
+      Testing.retry(3, () => {
+        const ports = [...Array(50)].map(() => Port.random());
+        expect(R.equals(R.uniq(ports), ports)).to.eql(true);
+        expect(ports.every((v) => typeof v === 'number')).to.eql(true);
+      });
     });
 
     it('Port.inUse: false', () => {
-      // NB: check that none of the randomly generated ports are in use.
-      const ports = [...Array(10)].map(() => Port.random());
-      expect(ports.every((p) => !Port.inUse(p))).to.eql(true);
+      Testing.retry(3, () => {
+        // NB: check that none of the randomly generated ports are in use.
+        const ports = [...Array(10)].map(() => Port.random());
+        expect(ports.every((p) => !Port.inUse(p))).to.eql(true);
+      });
     });
 
     it('Port.inUse: true', () => {
-      const port = Port.random();
-      const listener = Deno.listen({ port });
-      expect(Port.inUse(port)).to.eql(true);
-      listener.close();
+      Testing.retry(3, () => {
+        const port = Port.random();
+        const listener = Deno.listen({ port });
+        expect(Port.inUse(port)).to.eql(true);
+        listener.close();
+      });
     });
 
     describe('Port.get', () => {
