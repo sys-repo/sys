98c1d3b181dc5d50c7f2e309e8507402ddc6c99b
Phil Cockfield <phil@cockfield.net>
2024-11-13 21:27:58 +1300

    rename

--- code/sys.driver/driver-vitepress/src/-tmpl/mod.ts
+++ code/sys.driver/driver-vitepress/src/-tmpl/mod.ts
@@ -1,7 +1,7 @@
 import { config } from './u.config.ts';
 import { Pkg, VSCode } from './u.json.ts';
 import { Markdown } from './u.md.ts';
-import { pkg } from './u.pkg.ts';
+import { pkg } from './u.meta.ts';
 import { Script } from './u.script.ts';
 
 const gitignore = `
