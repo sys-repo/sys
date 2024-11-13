d1c4428fcb0cf23ece29c7692e1bbe5e043e641f
Phil Cockfield <phil@cockfield.net>
2024-11-14 12:18:18 +1300

    Update u.typescript.ts

--- code/sys.driver/driver-vitepress/src/-tmpl/u.typescript.ts
+++ code/sys.driver/driver-vitepress/src/-tmpl/u.typescript.ts
@@ -9,7 +9,7 @@ import { default as deno } from './deno.json' with { type: 'json' };
 /**
  * Package meta-data.
  */
-export const pkg: t.Pkg = Pkg.fromJson(deno, 'my-name');
+export const pkg: t.Pkg = Pkg.fromJson(deno, 'unnamed');
 `.slice(1);
 
 /**
