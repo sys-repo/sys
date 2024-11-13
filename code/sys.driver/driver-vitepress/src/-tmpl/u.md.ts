076a1675b079a5ac1728603206ff7e9348fe77a9
Phil Cockfield <phil@cockfield.net>
2024-11-13 21:29:32 +1300

    tweak

--- code/sys.driver/driver-vitepress/src/-tmpl/u.md.ts
+++ code/sys.driver/driver-vitepress/src/-tmpl/u.md.ts
@@ -1,7 +1,10 @@
+import { pkg, Pkg } from '../common.ts';
 const LOREM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec quam lorem. Praesent fermentum, augue ut porta varius, eros nisl euismod ante, ac suscipit elit libero nec dolor. Morbi magna enim, molestie non arcu id, varius sollicitudin neque. In sed quam mauris. Aenean mi nisl, elementum non arcu quis, ultrices tincidunt augue. Vivamus fermentum iaculis tellus finibus porttitor. Nulla eu purus id dolor auctor suscipit. Integer lacinia sapien at ante tempus volutpat.`;
 
 export const index = `
 # Root
+
+Generated with \`${Pkg.toString(pkg)}\`.
 
 - one
 - two
