63a1dc770e72ded5044be6dcf6a8664718b5aca0
Phil Cockfield <phil@cockfield.net>
2024-11-15 08:02:17 +1300

    Update -clean.ts

--- code/sys.driver/driver-vitepress/-scripts/-clean.ts
+++ code/sys.driver/driver-vitepress/-scripts/-clean.ts
@@ -5,4 +5,5 @@ const remove = (path: string) => Fs.remove(Fs.resolve(path), { log: true });
 await remove('./.tmp');
 await remove('./dist');
 await remove('./docs');
+await remove('./node_modules');
 await cleanTestFolder();
