5e694ba3872ba589eea2cbe4530c888bdb14a685
Phil Cockfield <phil@cockfield.net>
2024-11-15 13:17:34 +1300

    Update -main.ts

--- code/sys.driver/driver-vitepress/-scripts/-main.ts
+++ code/sys.driver/driver-vitepress/-scripts/-main.ts
@@ -3,11 +3,12 @@
  * In external usage of the module reference the module directly
  * by it's JSR address, eg:
  *
+ * ```json
  *  {
  *    "tasks": {
  *      "build": "deno run -RWE --allow-run jsr:@sys/driver-vitepress/main --cmd=build"
  *    }
  *  }
- *
+ * ```
  */
 import '../src/-cmd/-main.ts';
