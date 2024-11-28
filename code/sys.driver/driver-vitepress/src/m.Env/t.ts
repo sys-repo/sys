c31698ee895275615ae289cd752afbcfd757352a
Phil Cockfield <phil@cockfield.net>
2024-11-29 11:08:23 +1300

    move types

--- code/sys.driver/driver-vitepress/src/m.Env/t.ts
+++ code/sys.driver/driver-vitepress/src/m.Env/t.ts
@@ -1,9 +1,21 @@
-import type { DefaultTheme } from 'vitepress';
+import type { t } from './common.ts';
 
 /**
- * Data structure representing the Sidebar
- * configuration of the index-tree.
- *
- * See: https://vitepress.dev/reference/default-theme-sidebar
+ * Tools for maintaining the local "Editor Machine/Device" environment state.
  */
-export type VitePressTheme = DefaultTheme.Sidebar;
+export type VitePressEnvLib = {
+  /**
+   * Initialize the local machine environment.
+   */
+  init(args?: t.VitePressEnvInitArgs): Promise<void>;
+};
+
+/** Arguments passed to the `VitePress.init` method. */
+export type VitePressEnvInitArgs = {
+  force?: boolean;
+  inDir?: t.StringDir;
+  srcDir?: t.StringDir;
+  upgrade?: t.StringSemver;
+  silent?: boolean;
+  filter?: (path: t.StringPath) => boolean;
+};
--- code/sys.driver/driver-vitepress/src/m.VitePress/t.ts
+++ code/sys.driver/driver-vitepress/src/m.VitePress/t.ts
@@ -16,7 +16,7 @@ type ToStringOptions = { pad?: boolean };
  */
 export type VitePressLib = {
   /** Tools for maintaining the "Editor Machine/Device" environment. */
-  readonly Env: VitePressEnvLib;
+  readonly Env: t.VitePressEnvLib;
 
   /** Initialize the local machine environment. */
   init: t.VitePressEnvLib['init'];
@@ -80,24 +80,4 @@ export type VitePressBuildResponse = {
   dirs: t.ViteBundleDirs;
   dist: t.DistPkg;
   toString(options?: ToStringOptions): string;
-};
-
-/**
- * Tools for maintaining the local "Editor Machine/Device" environment state.
- */
-export type VitePressEnvLib = {
-  /**
-   * Initialize the local machine environment.
-   */
-  init(args?: t.VitePressEnvInitArgs): Promise<void>;
-};
-
-/** Arguments passed to the `VitePress.init` method. */
-export type VitePressEnvInitArgs = {
-  force?: boolean;
-  inDir?: t.StringDir;
-  srcDir?: t.StringDir;
-  upgrade?: t.StringSemver;
-  silent?: boolean;
-  filter?: (path: t.StringPath) => boolean;
 };
