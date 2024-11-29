9887902801c59058c09a11e1fce0a4400f4aceeb
Phil Cockfield <phil@cockfield.net>
2024-11-29 16:14:14 +1300

    run command

--- code/sys.driver/driver-vitepress/-scripts/-c.init.ts
+++ code/sys.driver/driver-vitepress/-scripts/-c.init.ts
@@ -1,1 +1,10 @@
-console.log(`-------------------------------------------`);
+/**
+ * Sample pass-through from deno.json task.
+ * In external usage of the module reference the module directly
+ * by it's JSR address, eg:
+ *
+ * ```bash
+ * deno run -RWNE --allow-run jsr:@sys/driver-vitepress/init
+ * ```
+ */
+import '../src/m.Cmd/-init.ts';
