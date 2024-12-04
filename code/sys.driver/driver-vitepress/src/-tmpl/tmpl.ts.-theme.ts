a8badb47cbde7d84edcb92fee97bdc18e77bde1b
Phil Cockfield <phil@cockfield.net>
2024-12-05 12:26:24 +1300

    Update tmpl.ts.-theme.ts

--- code/sys.driver/driver-vitepress/src/-tmpl/tmpl.ts.-theme.ts
+++ code/sys.driver/driver-vitepress/src/-tmpl/tmpl.ts.-theme.ts
@@ -1,14 +1,14 @@
 export const theme = `
 import type { Theme as ThemeType } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import { registerComponents as registerComponentsSystem } from '../../.sys/components/index.ts';
-import { registerComponents as registerComponentsUser } from '../../src/components/index.ts';
+import { registerComponents as registerSystemComponents } from '../../.sys/components/index.ts';
+import { registerComponents as registerUserlandComponents } from '../../src/components/index.ts';
 
 export const Theme: ThemeType = {
   extends: DefaultTheme,
   enhanceApp(ctx) {
-    registerComponentsSystem(ctx);
-    registerComponentsUser(ctx);
+    registerSystemComponents(ctx);
+    registerUserlandComponents(ctx);
   },
 };
 
