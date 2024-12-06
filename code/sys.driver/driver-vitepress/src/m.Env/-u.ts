44c076025209ad94edb23cdc22b98f2e9ca66fbf
Phil Cockfield <phil@cockfield.net>
2024-12-06 15:45:26 +1300

    refactor

--- code/sys.driver/driver-vitepress/src/m.Env/-u.ts
+++ code/sys.driver/driver-vitepress/src/m.Env/-u.ts
@@ -14,6 +14,8 @@ export const assertExists = async (path: t.StringPath, expected = true) => {
  */
 export const assertEnvExists = async (dir: t.StringDir, expected = true) => {
   const assert = (path: string) => assertExists(Fs.join(dir, path), expected);
+  await assert('deno.json');
+  await assert('package.json');
   await assert('.vscode/settings.json');
   await assert('.gitignore');
   await assert('.vitepress/config.ts');
@@ -21,8 +23,6 @@ export const assertEnvExists = async (dir: t.StringDir, expected = true) => {
   await assert('.sys/-main.ts');
   await assert('.sys/components/index.ts');
   await assert('.sys/components/VideoPlayer.vue');
-  await assert('deno.json');
-  await assert('package.json');
   await assert('src/nav.ts');
   await assert('src/components/index.ts');
   await assert('src/components/Sample.vue');
