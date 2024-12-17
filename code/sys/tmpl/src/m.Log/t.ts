2d2017254f3d82bff99beae612d5e53ca76ff540
Phil Cockfield <phil@cockfield.net>
2024-12-18 10:12:51 +1300

    docs

--- code/sys/tmpl/src/m.Log/t.ts
+++ code/sys/tmpl/src/m.Log/t.ts
@@ -4,8 +4,6 @@ import type { t } from './common.ts';
  * Library for converting a Tmpl to console/log output.
  */
 export type TmplLogLib = {
-  /**
-   * Convert a set of template operations to a console table.
-   */
+  /** Convert a set of template operations to a console table. */
   table(ops: t.TmplFileOperation[], options?: { indent?: number; hideExcluded?: boolean }): string;
 };
