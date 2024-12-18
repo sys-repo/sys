59d8f60e61de39396aa164bdb4efa27a9f698ff0
Phil Cockfield <phil@cockfield.net>
2024-12-18 17:00:57 +1300

    fix tests

--- code/sys.driver/driver-vitepress/src/m.Env/-assert.ts
+++ code/sys.driver/driver-vitepress/src/m.Env/-assert.ts
@@ -31,8 +31,6 @@ export const assertEnvExists = async (dir: t.StringDir, expected = true) => {
     '.sys/components/React.Wrapper.Sample.tsx',
     'src/nav.ts',
     'src/config.ts',
-    'src/components/index.ts',
-    'src/components/Sample.vue',
     'docs/index.md',
   ];
 
