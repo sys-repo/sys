ffe96c0520c2929b002f5c772907589f067a3e29
Phil Cockfield <phil@cockfield.net>
2024-12-20 21:13:40 +1300

    Update -.test.ts

--- code/sys/tmpl/src/m.FileMap/-.test.ts
+++ code/sys/tmpl/src/m.FileMap/-.test.ts
@@ -3,14 +3,13 @@ import { FileMap } from './mod.ts';
 
 describe('FileMap', () => {
   const SAMPLE_DIR = 'src/m.FileMap/-sample';
+  const getPaths = async (dir = SAMPLE_DIR) => {
+    const glob = Fs.glob(dir);
+    const paths = await glob.find('**', { includeDirs: false });
+    return paths.map((m) => Path.trimCwd(m.path)).map((path) => path.slice(dir.length + 1));
+  };
 
   describe('FileMap.bundle', () => {
-    const getPaths = async (dir = SAMPLE_DIR) => {
-      const glob = Fs.glob(dir);
-      const paths = await glob.find('**', { includeDirs: false });
-      return paths.map((m) => Path.trimCwd(m.path)).map((path) => path.slice(dir.length + 1));
-    };
-
     it('bundle ← all paths', async () => {
       const dir = SAMPLE_DIR;
       const res = await FileMap.bundle(dir);
@@ -27,6 +26,7 @@ describe('FileMap', () => {
       expect(res['images/vector.svg']).to.eql(undefined);
       expect(res['images/wax.png']).to.eql(undefined);
 
+      console.log('res', res);
     });
   });
 });
