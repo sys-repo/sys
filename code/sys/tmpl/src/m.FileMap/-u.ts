28a7bd9f0fdf149008523104f5a58a26789fab11
Phil Cockfield <phil@cockfield.net>
2024-12-23 17:58:46 +1300

    update call sites

--- code/sys/tmpl/src/m.FileMap/-u.ts
+++ code/sys/tmpl/src/m.FileMap/-u.ts
@@ -1,24 +1,16 @@
-import { type t, Fs, slug } from './common.ts';
+import { Fs, slug } from './common.ts';
 
 export const Sample = {
   source: {
     dir: 'src/m.FileMap/-sample',
-    ls: () => ls(Sample.source.dir),
+    ls: () => Fs.ls(Sample.source.dir),
   },
 
   async init(options: { slug?: boolean } = {}) {
     const target = Fs.join(`./.tmp/tests/m.FileMap`, options.slug ?? true ? slug() : '');
     return {
       target,
-      ls: () => ls(target),
+      ls: () => Fs.ls(target),
     } as const;
   },
 } as const;
-
-/**
- * Helpers
- */
-const ls = async (dir: t.StringDir) => {
-  const files = await Fs.glob(dir, { includeDirs: false }).find('**');
-  return files.map((m) => m.path);
-};
--- code/sys/tmpl/src/m.Tmpl/-u.ts
+++ code/sys/tmpl/src/m.Tmpl/-u.ts
@@ -4,21 +4,13 @@ export const SAMPLE = {
   init(options: { source?: t.StringDir; slug?: boolean } = {}) {
     const source = Fs.resolve(options.source ?? './src/m.Tmpl/-sample');
     const target = Fs.resolve(`./.tmp/tests/m.Tmpl`, options.slug ?? true ? slug() : '');
-    const ls = async (dir: t.StringDir) => {
-      const files = await Fs.glob(dir, { includeDirs: false }).find('**');
-      return files.map((m) => m.path);
-    };
-
-    const exists = (dir: t.StringDir, path: string[]) => {
-      return Fs.exists(Fs.join(dir, ...path));
-    };
-
+    const exists = (dir: t.StringDir, path: string[]) => Fs.exists(Fs.join(dir, ...path));
     return {
       source,
       target,
       ls: {
-        source: () => ls(source),
-        target: () => ls(target),
+        source: () => Fs.ls(source),
+        target: () => Fs.ls(target),
       },
       exists: {
         source: (...path: string[]) => exists(source, path),
