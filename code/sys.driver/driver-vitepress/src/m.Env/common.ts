3ccaade28dbf8dee12f8faee343fe7f2b3af76fc
Phil Cockfield <phil@cockfield.net>
2024-12-30 21:55:35 +1300

    move templates to sub-folder

--- code/sys.driver/driver-vitepress/src/m.Env/common.ts
+++ code/sys.driver/driver-vitepress/src/m.Env/common.ts
@@ -1,7 +1,7 @@
 export * from '../common.ts';
 
 export const PATHS = {
-  source: 'src/-tmpl',
-  json: 'src/m.Env/u.tmpl.bundle.json',
+  source: 'src/-tmpl/files',
   tmp: '.tmp/-tmpl',
+  json: 'src/m.Env/u.tmpl.bundle.json',
 } as const;
