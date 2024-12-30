ff627ccda4724de639f3f473a25f8f8586f247c0
Phil Cockfield <phil@cockfield.net>
2024-12-31 09:18:02 +1300

    docs

--- code/sys/crypto/src/mod.fs.ts
+++ code/sys/crypto/src/mod.fs.ts
@@ -5,4 +5,13 @@
  * This module expands upon the generic/universal tools in `@sys/crypto`
  * with helpers and structures useful in environments that have a
  * file-system.
+ *
+ * @example
+ * ```ts
+ * import { Hash, Pkg } from '@sys/crypto/fs';
+ *
+ * import { Hash } from '@sys/crypto/fs/hash';
+ * import { Pkg } from '@sys/crypto/fs/pkg';
  */
+export { Hash } from './fs.Hash/mod.ts';
+export { Pkg } from './fs.Pkg/mod.ts';
