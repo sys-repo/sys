1c1182390f3736779a549c92fcec04850a1a0f3a
Phil Cockfield <phil@cockfield.net>
2024-12-30 17:22:19 +1300

    stub in module: @sys/fs

---
+++ code/sys/fs/src/mod.ts
@@ -0,0 +1,13 @@
+/**
+ * @module
+ * Tools for working with the file-system.
+ *
+ * @example
+ * ```ts
+ * import { Fs } from '@sys/fs';
+ * ```
+ */
+export { pkg } from './pkg.ts';
+
+/** Module types. */
+export type * as t from './types.ts';
