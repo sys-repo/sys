49bb4d77a0dde167a749b27df41aca12914f6904
Phil Cockfield <phil@cockfield.net>
2024-12-31 18:11:18 +1300

    docs

--- code/sys/crypto/src/fs.Pkg/mod.ts
+++ code/sys/crypto/src/fs.Pkg/mod.ts
@@ -4,6 +4,11 @@
  *
  * Tools for working with the standard system
  * `{pkg}` package meta-data structure.
+ *
+ * @example
+ * ```ts
+ * import { Pkg, Dist } from '@sys/crypto/fs/pkg';
+ * ```
  */
 export { Dist } from './m.Dist.ts';
 export { Pkg } from './m.Pkg.ts';
--- code/sys/crypto/src/mod.ts
+++ code/sys/crypto/src/mod.ts
@@ -4,8 +4,13 @@
  *
  * @example
  * ```ts
+ * // Context: universal (Browser + WinterCG)
  * import { pkg } from '@sys/crypto';
  * import { Hash } from '@sys/crypto/hash';
+ *
+ * // Context: file-system environments (WinterCG)
+ * import { Hash } from '@sys/crypto/fs/hash';
+ * import { Pkg, Dist } from '@sys/crypto/fs/pkg';
  * ```
  */
 export { pkg } from './pkg.ts';
