90d39a8f2b3908118f20bdada2b6deb14acd86d2
Phil Cockfield <phil@cockfield.net>
2025-01-02 17:03:03 +1300

    snapshot filename suffix: .snapshot

--- code/sys/fs/src/m.Dir/m.Dir.ts
+++ code/sys/fs/src/m.Dir/m.Dir.ts
@@ -1,33 +1,10 @@
-import { type t, Err, Fs, Path, Time, slug } from './common.ts';
+import { type t } from './common.ts';
+import { snapshot } from './u.snapshot.ts';
 
 /**
  * Helpers for working with file-system directories.
  */
 export const Dir: t.FsDirLib = {
-  /**
-   * Create a snapshot of the specified directory.
-   */
-  async snapshot(args) {
-    const { filter } = args;
-    const errors = Err.errors();
-    const timestamp = Time.now.timestamp;
-    const id = `${timestamp}.${slug()}`;
-
-    const path: t.DirSnapshot['path'] = {
-      source: args.source,
-      target: Path.join(args.target, id),
-    };
-
-    const copied = await Fs.copyDir(path.source, path.target, { filter });
-    if (copied.error) errors.push(copied.error);
-
-    const res: t.DirSnapshot = {
-      id,
-      timestamp,
-      path,
-      copied: await Fs.ls(path.target),
-      error: errors.toError(),
-    };
-    return res;
-  },
+  /** Create a snapshot of the specified directory. */
+  snapshot,
 };
---
+++ code/sys/fs/src/m.Dir/u.snapshot.ts
@@ -0,0 +1,28 @@
+import { type t, Err, Fs, Path, Time, slug } from './common.ts';
+
+/**
+ * Create a snapshot of the specified directory.
+ */
+export const snapshot: t.FsDirLib['snapshot'] = async (args) => {
+  const { filter } = args;
+  const errors = Err.errors();
+  const timestamp = Time.now.timestamp;
+  const id = `${timestamp}.${slug()}`;
+
+  const path: t.DirSnapshot['path'] = {
+    source: args.source,
+    target: Path.join(args.target, `${id}.snapshot`),
+  };
+
+  const copied = await Fs.copyDir(path.source, path.target, { filter });
+  if (copied.error) errors.push(copied.error);
+
+  const res: t.DirSnapshot = {
+    id,
+    timestamp,
+    path,
+    copied: await Fs.ls(path.target),
+    error: errors.toError(),
+  };
+  return res;
+};
