f63036a0f9c739a77787e54e1963de44529adf2f
Phil Cockfield <phil@cockfield.net>
2025-01-02 16:09:48 +1300

    import/exports

--- code/sys.driver/driver-vitepress/src/common/t.ts
+++ code/sys.driver/driver-vitepress/src/common/t.ts
@@ -4,6 +4,7 @@ export type * from '@sys/types/t';
 export type { DenofileJson } from '@sys/driver-deno/t';
 export type { DirSnapshot, FileMap, FsCopyFilter } from '@sys/fs/t';
 export type { ProcHandle, ProcReadySignalFilter } from '@sys/proc/t';
+export type { Ignore } from '@sys/std/t';
 export type { Tmpl, TmplFileOperation } from '@sys/tmpl/t';
 
 export type * from '../types.ts';
--- code/sys.driver/driver-vitepress/src/m.Cmd/common.ts
+++ code/sys.driver/driver-vitepress/src/m.Cmd/common.ts
@@ -1,5 +1,6 @@
 export * from '../common.ts';
 export { Log } from '../u.Log/mod.ts';
+export { Env } from '../m.Env/mod.ts';
 
 export const DEFAULTS = {
   cmd: 'dev',
