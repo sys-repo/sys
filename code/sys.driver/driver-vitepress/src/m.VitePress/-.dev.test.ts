7f96e8657df6c9c55b94cb04bd937402a1a3b75c
Phil Cockfield <phil@cockfield.net>
2025-01-03 10:51:55 +1300

    harden test

--- code/sys.driver/driver-vitepress/src/m.VitePress/-.dev.test.ts
+++ code/sys.driver/driver-vitepress/src/m.VitePress/-.dev.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '../-test.ts';
+import { describe, expect, it, Testing } from '../-test.ts';
 import { assertEnvExists, Sample } from './-u.ts';
 import { VitePress } from './mod.ts';
 
@@ -6,33 +6,37 @@ describe('VitePress.dev', () => {
   const open = false;
 
   it('process: start → fetch(200) → dispose', async () => {
-    const sample = Sample.init();
-    const { port, inDir } = sample;
-    await VitePress.Env.update({ inDir });
+    Testing.repeat(3, async () => {
+      const sample = Sample.init();
+      const { port, inDir } = sample;
+      await VitePress.Env.update({ inDir });
 
-    const server = await VitePress.dev({ port, inDir, open }); // NB: await returns after Vitepress as completed it's startup.
-    expect(server.port).to.eql(port);
-    expect(server.dirs.in).to.eql(inDir);
+      const server = await VitePress.dev({ port, inDir, open }); // NB: await returns after Vitepress as completed it's startup.
+      expect(server.port).to.eql(port);
+      expect(server.dirs.in).to.eql(inDir);
 
-    console.info(); //            NB: pad the output in the test-runner terminal. The "classic" Vite startup output.
+      console.info(); //            NB: pad the output in the test-runner terminal. The "classic" Vite startup output.
 
-    const res = await fetch(server.url);
-    const html = await res.text();
+      const res = await fetch(server.url);
+      const html = await res.text();
 
-    expect(res.status).to.eql(200);
-    expect(html).to.include(`<script type="module"`);
-    expect(html).to.include(`node_modules/.deno/vitepress@`);
+      expect(res.status).to.eql(200);
+      expect(html).to.include(`<script type="module"`);
+      expect(html).to.include(`node_modules/.deno/vitepress@`);
 
-    await server.dispose();
+      await server.dispose();
+    });
   });
 
   it('process: ensures baseline files ← Env.init()', async () => {
-    const sample = Sample.init();
-    const { port, inDir } = sample;
-    await VitePress.Env.update({ inDir });
+    Testing.repeat(3, async () => {
+      const sample = Sample.init();
+      const { port, inDir } = sample;
+      await VitePress.Env.update({ inDir });
 
-    const server = await VitePress.dev({ port, inDir, open });
-    await server.dispose();
-    await assertEnvExists(inDir);
+      const server = await VitePress.dev({ port, inDir, open });
+      await server.dispose();
+      await assertEnvExists(inDir);
+    });
   });
 });
