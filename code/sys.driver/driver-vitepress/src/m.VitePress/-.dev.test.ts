5e0464f7965d934d973ed2e4f4bca0e09e9cb03e
Phil Cockfield <phil@cockfield.net>
2025-01-03 15:34:07 +1300

    harden test

--- code/sys.driver/driver-vitepress/src/m.VitePress/-.dev.test.ts
+++ code/sys.driver/driver-vitepress/src/m.VitePress/-.dev.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, Testing } from '../-test.ts';
+import { describe, expect, it, Testing, Err } from '../-test.ts';
 import { assertEnvExists, Sample } from './-u.ts';
 import { VitePress } from './mod.ts';
 
@@ -10,21 +10,25 @@ describe('VitePress.dev', () => {
       const sample = Sample.init();
       const { port, inDir } = sample;
       await VitePress.Env.update({ inDir });
-
       const server = await VitePress.dev({ port, inDir, open }); // NB: await returns after Vitepress as completed it's startup.
-      expect(server.port).to.eql(port);
-      expect(server.dirs.in).to.eql(inDir);
 
-      console.info(); //            NB: pad the output in the test-runner terminal. The "classic" Vite startup output.
+      try {
+        expect(server.port).to.eql(port);
+        expect(server.dirs.in).to.eql(inDir);
 
-      const res = await fetch(server.url);
-      const html = await res.text();
+        console.info(); //            NB: pad the output in the test-runner terminal. The "classic" Vite startup output.
 
-      expect(res.status).to.eql(200);
-      expect(html).to.include(`<script type="module"`);
-      expect(html).to.include(`node_modules/.deno/vitepress@`);
+        const res = await fetch(server.url);
+        const html = await res.text();
 
-      await server.dispose();
+        expect(res.status).to.eql(200);
+        expect(html).to.include(`<script type="module"`);
+        expect(html).to.include(`node_modules/.deno/vitepress@`);
+      } catch (error) {
+        throw error;
+      } finally {
+        await server.dispose();
+      }
     });
   });
 
