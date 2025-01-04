a5358327c171d5f009121fb97e6045fa02f58237
Phil Cockfield <phil@cockfield.net>
2025-01-04 16:39:23 +1300

    fix import

--- code/sys/fs/src/common/t.ts
+++ code/sys/fs/src/common/t.ts
@@ -1,6 +1,7 @@
 export type * from '@sys/types';
 
-export type { HashFmtLib, HashInput, HashVerifyResponse } from '@sys/crypto/t';
+export type { HashInput } from '@sys/types/t';
+export type { HashFmtLib, HashVerifyResponse } from '@sys/crypto/t';
 export type { IgnoreLib, PathLib, PkgDistLib } from '@sys/std/t';
 
 export type * from '../types.ts';
