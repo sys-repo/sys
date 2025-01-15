ed275a61894167d2522f61322f712c36a67cb1d6
Phil Cockfield <phil@cockfield.net>
2025-01-15 14:17:42 +1300

    remove pass-through exports from module root

--- code/sys/std-s/src/mod.ts
+++ code/sys/std-s/src/mod.ts
@@ -6,31 +6,10 @@
  * ```ts
  * import { Env } from '@sys/std-s';              // Env-variables.
  * import { HttpServer } from '@sys/std-s/http';  // HTTP server.
- * import { Fs, Path } from '@sys/std-s/fs';      // Filesystem tools (alias to "@sys/fs")
- * import { Colors, c } from '@sys/std-s/fmt';    // Terminal color formatting (alias to "@sys/cli/fmt")
  * ```
  */
 export { pkg } from './pkg.ts';
 export type * as t from './types.ts';
-
-/**
- * Web "AND" Server.
- */
-export {
-  Args,
-  Async,
-  D,
-  Date,
-  Http,
-  Immutable,
-  ObjectPath,
-  R,
-  Rx,
-  rx,
-  Time,
-  Value,
-} from '@sys/std';
-export { Semver } from '@sys/std/semver';
 
 /**
  * Server only.
