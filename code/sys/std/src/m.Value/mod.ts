3a4510bba92d475ede3f1f820403ce33befa2a1d
Phil Cockfield <phil@cockfield.net>
2025-01-15 13:11:18 +1300

    exports

--- code/sys/std/src/m.Value/mod.ts
+++ code/sys/std/src/m.Value/mod.ts
@@ -2,6 +2,6 @@
  * @module
  * Tools for evaluating and manipulating various types of values.
  */
-export { isObject } from '../common.ts';
+export { isObject, isRecord } from '../common.ts';
 export { asArray } from '../m.Value.Array/mod.ts';
 export { Array, Num, Str, Value } from './m.Value.ts';
