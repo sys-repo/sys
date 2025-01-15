66c6f18ea53cffd059162b617b7d4b8a80b33d96
Phil Cockfield <phil@cockfield.net>
2025-01-15 22:00:40 +1300

    file rename

--- code/sys/http/src/.test.ts
+++ code/sys/http/src/.test.ts
@@ -2,7 +2,7 @@ import { type t, describe, it, expect, Pkg, pkg } from './-test.ts';
 
 describe(`module: ${Pkg.toString(pkg)}`, () => {
   it('exists', () => {
-    console.info(`🐷 Module`, pkg);
+    console.info(`💦 Module`, pkg);
     expect(typeof pkg.name === 'string').to.be.true;
   });
 });
