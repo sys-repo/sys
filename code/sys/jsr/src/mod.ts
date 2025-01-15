a48d32edb2ef0c8f6d18abfc217d9341748fc6fb
Phil Cockfield <phil@cockfield.net>
2025-01-15 14:17:52 +1300

    docs/export

--- code/sys/jsr/src/mod.ts
+++ code/sys/jsr/src/mod.ts
@@ -1,6 +1,7 @@
 /**
  * @module
  * Tools for working with the JSR module registry.
+ * https://jsr.io/docs
  *
  * @example
  * ```ts
@@ -11,3 +12,8 @@ export { pkg } from './pkg.ts';
 
 /** Module types. */
 export type * as t from './types.ts';
+
+/**
+ * Library.
+ */
+export { Jsr } from './m.Jsr/mod.ts';
