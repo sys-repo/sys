014190cb3c9f3e62adf302b57e302447a55002ca
Phil Cockfield <phil@cockfield.net>
2025-01-16 15:31:48 +1300

    docs

--- code/sys.driver/driver-vite/src/mod.ts
+++ code/sys.driver/driver-vite/src/mod.ts
@@ -1,33 +1,6 @@
 /**
  * @module
  * Tools for working with Vite as a bundler and dev-harness within Deno.
- *
- * @example
- * Within the `vite.config.ts` in the root of the module:
- *
- * ```ts
- * import { Vite } from '@sys/driver-vite';
- * import { defineConfig } from 'vite';
- * import reactPlugin from '@vitejs/plugin-react-swc';
- *
- * export default defineConfig(() => {
- *   const workspace = Vite.Plugin.workspace();
- *   return { plugins: [reactPlugin(), workspace] };
- * });
- * ```
- *
- * Optionally, you can filter the workspace modules that are exposed
- * to the Vite bundle:
- *
- * ```ts
- * export default defineConfig(() => {
- *   const workspace = Vite.Plugin.workspace({ filter: (e) => e.subpath.startsWith('/client') });
- *   return { plugins: [reactPlugin(), workspace] };
- * });
- * ```
- *
- * Along with the option to manulate the configuration further after the initial
- * baseline settings have initialized, using the `mutate` plugin callback.
  */
 export { pkg } from './pkg.ts';
 export type * as t from './types.ts';
