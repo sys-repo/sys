efe3b5bbdfa08884e9faec790af20671c0ea4454
Phil Cockfield <phil@cockfield.net>
2025-01-17 12:13:09 +1300

    update tmp <Foo>

--- code/sys.tmp/src/ui/Foo.tsx
+++ code/sys.tmp/src/ui/Foo.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
-import { pkg } from '../common.ts';
+// @ts-types="@types/react"
+import React from 'react';
+import { pkg } from './common.ts';
 
 /**
  * Sample component properties.
@@ -9,7 +10,9 @@ export type FooProps = { enabled?: boolean };
 /**
  * Sample component.
  */
-export const Foo: FC<FooProps> = (_props = {}) => {
-  const text = `${pkg.name}@${pkg.version}:Foo`;
+export const Foo: React.FC<FooProps> = (props) => {
+  const { enabled = true } = props;
+  let text = `${pkg.name}@${pkg.version}/ui:<Foo>`;
+  if (!enabled) text += ' (disabled)';
   return <code>{text}</code>;
 };
