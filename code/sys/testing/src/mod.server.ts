a1eb2d87aed9202c26d46d4f742f19ce7791eda6
Phil Cockfield <phil@cockfield.net>
2025-01-21 20:14:52 +1300

    ansi export

--- code/sys/testing/src/mod.server.ts
+++ code/sys/testing/src/mod.server.ts
@@ -20,6 +20,7 @@
  *   });
  * });
  */
+export { c } from '@sys/color/ansi';
 export { Fs, Path } from '@sys/fs';
 export { describe, expect, expectError, it, slug, Time } from '@sys/std/testing';
 export { Testing } from '@sys/std/testing/server';
