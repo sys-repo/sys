497f342ebd6ecad5007f82ed34637177a530120e
Phil Cockfield <phil@cockfield.net>
2025-01-23 11:57:19 +1300

    Update -.test.ts

--- code/sys/jsr/src/m.Manifest/-.test.ts
+++ code/sys/jsr/src/m.Manifest/-.test.ts
@@ -3,26 +3,26 @@ import { Jsr } from '../m.Jsr/mod.ts';
 import { Fetch } from './common.ts';
 import { Manifest } from './mod.ts';
 
-describe('Jsr.Manifest (integration test)', () => {
-  const SAMPLE = {
-    pkg: { name: '@sys/std', version: '0.0.42' },
-    def: {
-      // NB: paths not-ordered.
-      '/src/m.Path/mod.ts': {
-        size: 261,
-        checksum: 'sha256-03d38aeb62a14d34da9f576d12454537d4c6cedff0ad80d9ee4b9b8bb77702ba',
-      },
-      '/src/pkg.ts': {
-        size: 241,
-        checksum: 'sha256-b79790c447397a88ace8c538792fa37742ea38306cd08676947a2cd026b66269',
-      },
-      '/deno.json': {
-        size: 830,
-        checksum: 'sha256-dd9c3b367d8745aef1083b94982689dc7b39c75e16d8da66c78da6450166f3d5',
-      },
+export const SAMPLE = {
+  pkg: { name: '@sys/std', version: '0.0.42' },
+  def: {
+    // NB: paths not-ordered.
+    '/src/m.Path/mod.ts': {
+      size: 261,
+      checksum: 'sha256-03d38aeb62a14d34da9f576d12454537d4c6cedff0ad80d9ee4b9b8bb77702ba',
     },
-  } as const;
+    '/src/pkg.ts': {
+      size: 241,
+      checksum: 'sha256-b79790c447397a88ace8c538792fa37742ea38306cd08676947a2cd026b66269',
+    },
+    '/deno.json': {
+      size: 830,
+      checksum: 'sha256-dd9c3b367d8745aef1083b94982689dc7b39c75e16d8da66c78da6450166f3d5',
+    },
+  },
+} as const;
 
+describe('Jsr.Manifest (integration test)', () => {
   describe('Manifest.create', () => {
     it('unique instances: 200', () => {
       const m = Manifest.create(SAMPLE.pkg, SAMPLE.def);
