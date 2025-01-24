be99f3b6a76755c6cfdd4fb71a2375140ec1bd1f
Phil Cockfield <phil@cockfield.net>
2025-01-24 20:12:54 +1300

    fix callsite

--- code/sys.driver/driver-deno/src/m.Cloud/ns.server/common/libs.ts
+++ code/sys.driver/driver-deno/src/m.Cloud/ns.server/common/libs.ts
@@ -1,7 +1,7 @@
 export { c } from '@sys/cli';
-export { Env } from '@sys/std-s';
-export { Auth } from '@sys/std-s/auth';
+export { Env } from '@sys/fs/env';
 export { HttpServer } from '@sys/http/server';
+export { Auth } from '@sys/std-s/auth';
 
 export { PrivyClient } from '@privy-io/server-auth';
 export { DenoSubhostingAPI };
