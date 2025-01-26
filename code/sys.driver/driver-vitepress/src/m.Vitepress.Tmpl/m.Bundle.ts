986d3259c4402a974252dc5cfc9038a9a07392ab
Phil Cockfield <phil@cockfield.net>
2025-01-27 12:27:32 +1300

    refactor

--- code/sys.driver/driver-vitepress/src/m.Vitepress.Tmpl/m.Bundle.ts
+++ code/sys.driver/driver-vitepress/src/m.Vitepress.Tmpl/m.Bundle.ts
@@ -1,9 +1,30 @@
-import { type t } from './common.ts';
+import bundle from './-bundle.json' with { type: 'json' };
+import { c, FileMap, Fs, PATHS, type t } from './common.ts';
 
-import { prep } from './m.Bundle.u.prep.ts';
-import { saveToFilesystem } from './m.Bundle.u.save.ts';
 
 export const Bundle: t.VitepressBundleLib = {
-  prep,
-  saveToFilesystem,
+  async prep() {
+    const from = PATHS.tmpl.source;
+    const to = PATHS.tmpl.json;
+    await Fs.writeJson(PATHS.tmpl.json, await FileMap.bundle(from));
+    console.info(c.gray(`Templates written into file-map: ${c.white(to)}`));
+    return { from, to };
+  },
+
+  async saveToFilesystem(dir: t.StringDir = PATHS.tmp) {
+    await FileMap.write(dir, bundle);
+    console.info(c.gray(`Template files hydrated to: ${c.white(Fs.Path.trimCwd(dir))}`));
+
+    const ls = await Fs.ls(dir);
+
+    console.info('  ↓');
+    ls.forEach((path) => {
+      const prefix = c.green('template:');
+      const dirname = Fs.Path.trimCwd(Fs.dirname(path));
+      const filename = c.white(Fs.basename(path));
+      console.info(c.gray(`  ${prefix} ${dirname}/${filename}`));
+    });
+    
+    console.info();
+  },
 };
