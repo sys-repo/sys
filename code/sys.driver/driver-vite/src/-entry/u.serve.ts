05ae8d7b90b718805f2051dd816394340cf6f0c7
Phil Cockfield <phil@cockfield.net>
2025-01-28 12:34:03 +1300

    alert: when dir not built

--- code/sys.driver/driver-vite/src/-entry/u.serve.ts
+++ code/sys.driver/driver-vite/src/-entry/u.serve.ts
@@ -1,4 +1,4 @@
-import { type t, HttpServer, pkg, Pkg } from './common.ts';
+import { type t, c, Fs, HttpServer, pkg, Pkg } from './common.ts';
 
 /**
  * Run a local HTTP server from entry command-args.
@@ -6,6 +6,7 @@ import { type t, HttpServer, pkg, Pkg } from './common.ts';
 export async function serve(args: t.ViteEntryArgsServe) {
   if (args.cmd !== 'serve') return;
   const { port = 8080, dir = 'dist', silent = false } = args;
+  const dirExists = await Fs.exists(dir);
 
   const dist = (await Pkg.Dist.load(dir)).dist;
   const hash = dist?.hash.digest ?? '';
@@ -13,6 +14,10 @@ export async function serve(args: t.ViteEntryArgsServe) {
 
   const app = HttpServer.create({ pkg, hash, static: ['/*', dir] });
   const options = HttpServer.options({ port, pkg, hash, silent });
+
+  const fmtDir = c.gray(dir.replace(/^\.\//, ''));
+  const fmtDirExists = c.yellow(!dirExists ? c.bold('(does not exist)') : '');
+  console.info(c.gray(`Dir      ${fmtDir} ${fmtDirExists}`));
 
   Deno.serve(options, app.fetch);
   await HttpServer.keyboard({ port, print: !silent });
