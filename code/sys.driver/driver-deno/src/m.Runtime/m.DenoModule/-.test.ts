aaf120ebd0ae6eafc4c15cc8eb444f64b27c431e
Phil Cockfield <phil@cockfield.net>
2025-01-29 11:38:57 +1300

    refactor

--- code/sys.driver/driver-deno/src/m.Runtime/m.DenoModule/-.test.ts
+++ code/sys.driver/driver-deno/src/m.Runtime/m.DenoModule/-.test.ts
@@ -1,9 +1,11 @@
-import { type t, describe, it, expect, Testing } from '../../-test.ts';
+import { describe, expect, it } from '../../-test.ts';
 import { DenoModule } from './mod.ts';
+import { backup } from './u.backup.ts';
 import { upgrade } from './u.upgrade.ts';
 
 describe('DenoModule', () => {
   it('API', async () => {
     expect(DenoModule.upgrade).to.equal(upgrade);
+    expect(DenoModule.backup).to.equal(backup);
   });
 });
