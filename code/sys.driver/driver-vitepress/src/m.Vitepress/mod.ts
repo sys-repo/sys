5ee323e1017b0a8908c9765a10e93e0a5430caca
Phil Cockfield <phil@cockfield.net>
2025-01-29 14:52:08 +1300

    rename files

--- code/sys.driver/driver-vitepress/src/m.Vitepress/mod.ts
+++ code/sys.driver/driver-vitepress/src/m.Vitepress/mod.ts
@@ -2,8 +2,8 @@ import type { t } from './common.ts';
 
 import { VitepressTmpl as Tmpl } from '../m.Vitepress.Tmpl/mod.ts';
 import { backup } from './u.backup.ts';
-import { build } from './u.cmd.build.ts';
-import { dev } from './u.cmd.dev.ts';
+import { build } from './u.build.ts';
+import { dev } from './u.dev.ts';
 
 /**
  * Tools for working with a VitePress project.
