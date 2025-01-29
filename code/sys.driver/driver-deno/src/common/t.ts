165ae5747635f7d70030d12ff16b031930713bea
Phil Cockfield <phil@cockfield.net>
2025-01-29 14:10:24 +1300

    types

--- code/sys.driver/driver-deno/src/common/t.ts
+++ code/sys.driver/driver-deno/src/common/t.ts
@@ -1,4 +1,4 @@
-export type { DirSnapshot, FsPathFilter, FsReadJsonResponse } from '@sys/fs/t';
+export type { DirSnapshot, DirSnapshotMeta, FsPathFilter, FsReadJsonResponse } from '@sys/fs/t';
 export type * from '@sys/std/t';
 
 export type * from '../types.ts';
