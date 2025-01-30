42473900d255d46a10622cc72c6ad0f7a890e93c
Phil Cockfield <phil@cockfield.net>
2025-01-30 14:29:47 +1300

    -tmpl files (after .gitignore reset)

---
+++ code/sys.driver/driver-vitepress/src/-tmpl/.vitepress/config.ts
@@ -0,0 +1,22 @@
+import type { ConfigEnv } from 'vite';
+import { defineConfig } from 'vitepress';
+import { Config } from '../src/config.ts';
+import { sidebar } from '../src/nav.ts';
+import { markdown } from './config.markdown.ts';
+
+import { ViteConfig } from '@sys/driver-vite';
+
+export default async (env: ConfigEnv) => {
+  const { title, description } = Config;
+  const ws = await ViteConfig.workspace({});
+  const alias = [...ws.aliases];
+
+  return defineConfig({
+    title,
+    description,
+    srcDir: '<SRC_DIR>',
+    markdown,
+    themeConfig: { sidebar, search: { provider: 'local' } },
+    vite: { resolve: { alias } },
+  });
+};
