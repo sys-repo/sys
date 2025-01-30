58a3cf091922744561818f54e87b995b8a62af3a
Phil Cockfield <phil@cockfield.net>
2025-01-30 14:17:00 +1300

    fix

--- code/sys.driver/driver-vitepress/-scripts/-prep.ts
+++ code/sys.driver/driver-vitepress/-scripts/-prep.ts
@@ -1,5 +1,5 @@
 import { Vitepress } from '@sys/driver-vitepress';
-const Bundle = Vitepress.Env.Tmpl.Bundle;
+const Bundle = Vitepress.Tmpl.Bundle;
 
 await Bundle.toFilemap();
 await Bundle.toFilesystem(); // NB: test output
