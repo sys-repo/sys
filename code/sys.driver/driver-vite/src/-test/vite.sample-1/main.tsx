50510f5f9286141d3e13048830fc04244d8706da
Phil Cockfield <phil@cockfield.net>
2025-01-31 12:22:45 +1300

    simplify

--- code/sys.driver/driver-vite/src/-test/vite.sample-1/main.tsx
+++ code/sys.driver/driver-vite/src/-test/vite.sample-1/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { pkg } from '../../pkg.ts';
-console.info('Pkg', pkg);
 
 /**
  * Sample: render react component.
