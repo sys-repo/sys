ad95fccf08a2bb65f6299c6f78d27490a367c628
Phil Cockfield <phil@cockfield.net>
2025-01-31 18:34:24 +1300

    rename

--- code/sys.driver/driver-vite/src/m.Vite/m.Vite.ts
+++ code/sys.driver/driver-vite/src/m.Vite/m.Vite.ts
@@ -2,8 +2,8 @@ import { Plugin } from '../m.Vite.Plugin/mod.ts';
 import { ViteTmpl as Tmpl } from '../m.Vite.Tmpl/mod.ts';
 import { ViteConfig as Config, type t } from './common.ts';
 import { backup } from './u.backup.ts';
-import { build } from './u.cmd.build.ts';
-import { dev } from './u.cmd.dev.ts';
+import { build } from './u.build.ts';
+import { dev } from './u.dev.ts';
 
 /**
  * Tools for running Vite via commands issued to a child process.
