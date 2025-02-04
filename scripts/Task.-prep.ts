6db33d67ca0c23b3fc798f0e75127a6ccfe23fb5
Phil Cockfield <phil@cockfield.net>
2025-02-04 19:39:23 +1300

    print output

--- scripts/Task.-prep.ts
+++ scripts/Task.-prep.ts
@@ -1,4 +1,5 @@
-import { DenoDeps, Fs } from './common.ts';
+import { c, DenoDeps, Fs } from './common.ts';
+const i = c.italic;
 
 /**
  * Prepare the [package.json] and [deno.json] files
@@ -8,6 +9,26 @@ export async function main() {
   const res = await DenoDeps.fromYaml('./config.yaml');
   if (res.error) return console.error(res.error);
 
-  await Fs.writeJson('./package.json', DenoDeps.toPackageJson(res.data));
-  await Fs.writeJson('./deno.imports.json', DenoDeps.toDenoJson(res.data));
+  const PATH = {
+    package: './package.json',
+    deno: './deno.imports.json',
+  };
+
+  /**
+   * Write to file-system.
+   */
+  const deps = res.data?.deps;
+  await Fs.writeJson(PATH.package, DenoDeps.toPackageJson(deps));
+  await Fs.writeJson(PATH.deno, DenoDeps.toDenoJson(deps));
+
+  /**
+   * Output: console.
+   */
+  const fmtSeeFiles = c.gray(`see ${i(PATH.deno)} and ${i(PATH.package)}`);
+  console.info();
+  console.info(c.brightWhite(`${c.bold('Import Maps')}`));
+  console.info(c.green(`(dependencies written)`), fmtSeeFiles);
+  console.info();
+  console.info(DenoDeps.Fmt.deps(deps));
+  console.info();
 }
