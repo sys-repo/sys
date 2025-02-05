41790ee4006f56002171dbc0ab131edb6bfffd4b
Phil Cockfield <phil@cockfield.net>
2025-02-05 17:24:01 +1300

    make "ansi" export on @sys/std more explicit on export path.

--- code/sys/color/src/mod.Ansi.ts
+++ code/sys/color/src/mod.Ansi.ts
@@ -2,7 +2,7 @@
  * @module
  * ANSI Color formatting tools for the terminal.
  */
-import { c, stripAnsi } from '@sys/std/ansi';
+import { c, stripAnsi } from '@sys/std/ansi/server';
 import { Color as rgb } from './m.Rgb/mod.ts';
 
 import type { t } from './common.ts';
