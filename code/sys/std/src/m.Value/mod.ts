f8357b7f12c9ec8c0696e746c7aa1e514335e48d
Phil Cockfield <phil@cockfield.net>
2025-02-06 19:02:08 +1300

    sortKeys helper (@sys/std)

--- code/sys/std/src/m.Value/mod.ts
+++ code/sys/std/src/m.Value/mod.ts
@@ -2,6 +2,7 @@
  * @module
  * Tools for evaluating and manipulating various types of values.
  */
-export { isObject, isRecord, isEmptyRecord } from '../common.ts';
+export { isEmptyRecord, isObject, isRecord } from '../common.ts';
 export { asArray } from '../m.Value.Array/mod.ts';
+export { sortKeys } from '../m.Value.Obj/m.Obj.ts';
 export { Array, Num, Str, Value } from './m.Value.ts';
