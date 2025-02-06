181459d722a5892cb2789aa0fcb7e19cc88bff00
Phil Cockfield <phil@cockfield.net>
2025-02-06 15:20:04 +1300

    rename

--- code/sys.driver/driver-vitepress/src/m.Vitepress.Tmpl/u.file.ts
+++ code/sys.driver/driver-vitepress/src/m.Vitepress.Tmpl/u.file.ts
@@ -10,7 +10,7 @@ export function createFileProcessor(args: t.VitepressTmplCreateArgs): t.TmplProc
 
   const getWorkspace = async () => {
     const ws = await DenoFile.workspace();
-    const deps = (await DenoDeps.fromYaml(Path.join(ws.dir, 'config.yaml'))).data;
+    const deps = (await DenoDeps.fromYaml(Path.join(ws.dir, 'deps.yaml'))).data;
     const modules = Esm.modules([...(deps?.modules.items ?? []), ...ws.modules.items]);
     return { ws, modules };
   };
