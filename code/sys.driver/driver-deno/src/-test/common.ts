1f39b7656bbaa17de007325eefde76b472a90bce
Phil Cockfield <phil@cockfield.net>
2025-02-07 17:23:45 +1300

    Update common.ts

--- code/sys.driver/driver-deno/src/-test/common.ts
+++ code/sys.driver/driver-deno/src/-test/common.ts
@@ -1,2 +1,2 @@
-export { describe, expect, Fs, it, Testing } from '@sys/testing/server';
+export { describe, expect, Fs, it, Testing, c } from '@sys/testing/server';
 export * from '../ns.Runtime/common.ts';
