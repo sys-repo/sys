766b319f2aef96be19a749b35e09570666e7180c
Phil Cockfield <phil@cockfield.net>
2025-02-10 12:53:53 +1300

    prep - copy deps.yaml

--- code/sys.driver/driver-vitepress/-scripts/-prep.ts
+++ code/sys.driver/driver-vitepress/-scripts/-prep.ts
@@ -1,13 +1,22 @@
-import { Fs } from '@sys/fs';
-import { DenoFile } from '@sys/driver-deno/runtime';
+import { DenoDeps, DenoFile } from '@sys/driver-deno/runtime';
+import { Dep } from '@sys/driver-deno/t';
 import { Vitepress } from '@sys/driver-vitepress';
+import { Fs } from '@sys/fs';
 
-const ws = await DenoFile.workspace();
-await Fs.copy(Fs.join(ws.dir, 'deps.yaml'), './src/-tmpl/.sys/deps.yaml');
 /**
- * TODO 🐷 expose from {ws}. Save to file
+ * Save monorepo deps.
  */
+const ws = await DenoFile.workspace();
+const deps: Dep[] = ws.children.modules.items.map((m) => DenoDeps.toDep(m));
+const yaml = DenoDeps.toYaml(deps);
 
+const dir = './src/-tmpl/.sys';
+await Fs.copy(Fs.join(ws.dir, 'deps.yaml'), Fs.join(dir, 'deps.yaml'));
+await Fs.write(Fs.join(dir, 'deps.sys.yaml'), yaml.text);
+
+/**
+ * Bundle files (for code-registry).
+ */
 const Bundle = Vitepress.Tmpl.Bundle;
 await Bundle.toFilemap();
 await Bundle.toFilesystem(); // NB: test output.
