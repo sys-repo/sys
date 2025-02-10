0369d5900d9f30bdf929f657ab4fbb43758a1031
Phil Cockfield <phil@cockfield.net>
2025-02-10 18:16:16 +1300

    imports working: WIP ← /*.css imports still failing.

---
+++ .github/workflows/Video.tsx
@@ -0,0 +1,54 @@
+// @ts-types="@types/react"
+import React from 'react';
+
+// import '@vidstack/react/player/styles/base.css';
+// import '@vidstack/react/player/styles/plyr/theme.css';
+
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+// import { PlyrLayout, plyrLayoutIcons } from '@vidstack/react/player/layouts/plyr';
+
+console.log('MediaPlayer', MediaPlayer);
+
+/**
+ * TODO 🐷 - add workspace/plugin refs for VitePress → see: @sys/driver-vite
+ */
+import { Foo } from '@sys/tmp/ui';
+
+export type VideoProps = {
+  title?: string;
+  src?: string;
+};
+
+/**
+ * Component
+ */
+export const Video: React.FC<VideoProps> = (props: VideoProps) => {
+  const src = props.src || DEFAULTS.src;
+
+  // const elPlayer = (
+  //   <MediaPlayer title={props.title} src={src} playsInline={true}>
+  //     <MediaProvider />
+  //     <PlyrLayout
+  //       // thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt"
+  //       icons={plyrLayoutIcons}
+  //     />
+  //   </MediaPlayer>
+  // );
+
+  // TEMP 🐷
+  return (
+    <div>
+      <div>
+        import: <Foo importSample={true} />
+      </div>
+      {/* {elPlayer} */}
+    </div>
+  );
+};
+
+/**
+ * Constants
+ */
+export const DEFAULTS = {
+  src: 'vimeo/499921561', // Tubes.
+} as const;
