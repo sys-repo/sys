a77ef4ed5a8d2fc5bdc35918f879f90e034d6a4d
Phil Cockfield <phil@cockfield.net>
2025-02-14 11:18:39 +1300

    Update ui.Foo.tsx

--- code/sys.tmp/src/ui/ui.Foo.tsx
+++ code/sys.tmp/src/ui/ui.Foo.tsx
@@ -1,5 +1,7 @@
 // @ts-types="@types/react"
 import React from 'react';
+
+import { css } from '@sys/ui-css';
 import { pkg } from '../pkg.ts';
 
 /** Constants. */
@@ -18,6 +20,18 @@ export type FooProps = {
 export const Foo: React.FC<FooProps> = (props) => {
   const { enabled = true } = props;
   let text = `${pkg.name}@${pkg.version}/ui:<Foo>`;
+
+  const styles = {
+    base: css({
+      display: 'inline-block',
+      backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
+    }),
+  };
+
   if (!enabled) text += ' (disabled)';
-  return <code style={{ backgroundColor: RED }}>{text}</code>;
+  return (
+    <div className={styles.base.class}>
+      <code>{text}</code>
+    </div>
+  );
 };
