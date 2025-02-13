a12435f532e3f3e71b67f099a40577e55e8f288f
Phil Cockfield <phil@cockfield.net>
2025-02-14 12:21:35 +1300

    supressing sanitize flags not necessary
    { sanitizeOps: false, sanitizeResources: false }

--- code/sys.ui/ui-css/src/m.Css.Dom/-.test.ts
+++ code/sys.ui/ui-css/src/m.Css.Dom/-.test.ts
@@ -3,107 +3,100 @@ import { transform } from '../m.Style/mod.ts';
 import { DEFAULTS } from './common.ts';
 import { CssDom } from './mod.ts';
 
-describe(
-  'Style: CSS ClassName',
+describe('Style: CSS ClassName', () => {
+  DomMock.polyfill();
 
-  /** NOTE: leaked timers left around by the "happy-dom" module. */
-  { sanitizeOps: false, sanitizeResources: false },
-
-  () => {
-    DomMock.polyfill();
-
-    describe('create (instance)', () => {
-      it('prefix: default', () => {
-        const a = CssDom.create('');
-        const b = CssDom.create('   ');
-        const c = CssDom.create();
-        expect(a.prefix).to.eql(DEFAULTS.prefix);
-        expect(b.prefix).to.eql(DEFAULTS.prefix);
-        expect(c.prefix).to.eql(DEFAULTS.prefix);
-      });
-
-      it('custom prefix', () => {
-        const test = (prefix: string, expected: string) => {
-          const ns = CssDom.create(prefix);
-          expect(ns.prefix).to.eql(expected);
-        };
-        test('foo', 'foo');
-        test('  foo  ', 'foo');
-        test(' foo- ', 'foo'); // NB: trimmed.
-        test(' foo-- ', 'foo');
-        test('foo123', 'foo123');
-        test('foo-123', 'foo-123');
-      });
-
-      it('pooling (instance reuse keyed on "prefix")', () => {
-        const a = CssDom.create();
-        const b = CssDom.create(DEFAULTS.prefix);
-        const c = CssDom.create('foo');
-        expect(a).to.equal(b);
-        expect(a).to.not.equal(c);
-      });
-
-      it('insert root <style> into DOM (singleton)', () => {
-        const find = () => document.querySelector(`style[data-controller="${pkg.name}"]`);
-        CssDom.create();
-        expect(find()).to.exist;
-        CssDom.create();
-        expect(find()).to.equal(find()); // Singleton.
-      });
-
-      it('throw: invalid prefix', () => {
-        const test = (prefix: string) => {
-          const fn = () => CssDom.create(prefix);
-          expect(fn).to.throw(
-            /String must start with a letter and can contain letters, digits, and hyphens \(hyphen not allowed at the beginning\)/,
-          );
-        };
-        test('123foo'); // NB: Starts with a number.
-        test(' 123-foo ');
-        test('-foo');
-        test('-');
-        test('foo*bar');
-      });
+  describe('create (instance)', () => {
+    it('prefix: default', () => {
+      const a = CssDom.create('');
+      const b = CssDom.create('   ');
+      const c = CssDom.create();
+      expect(a.prefix).to.eql(DEFAULTS.prefix);
+      expect(b.prefix).to.eql(DEFAULTS.prefix);
+      expect(c.prefix).to.eql(DEFAULTS.prefix);
     });
 
-    describe('class/style DOM insertion', () => {
-      const setup = (): t.CssDom => {
-        const prefix = `sample-${slug()}`;
-        return CssDom.create(prefix);
+    it('custom prefix', () => {
+      const test = (prefix: string, expected: string) => {
+        const ns = CssDom.create(prefix);
+        expect(ns.prefix).to.eql(expected);
       };
-
-      it('simple ("hx" not passed)', () => {
-        const dom = setup();
-        const m = transform({ fontSize: 32, display: 'grid', PaddingX: [5, 10] });
-        expect(dom.classes.length).to.eql(0); // NB: no "inserted classes" yet.
-
-        // Baseline: ensure the rule is not yet within the DOM.
-        const className = `${dom.prefix}-${m.hx}`;
-        expect(findCssRule(className)).to.eql(undefined); // NB: nothing inserted yet.
-
-        const a = dom.class(m.style);
-        const b = dom.class(m.style);
-
-        expect(dom.classes.length).to.eql(1); // NB: not added twice.
-        expect(a).to.eql(b);
-        expect(a).to.eql(className);
-
-        // Ensure is inserted within DOM.
-        const rule = findCssRule(className);
-        expect(rule?.cssText).to.eql(`.${className} { ${m.toString()} }`);
-      });
-
-      it('hash passed as parameter', () => {
-        const dom = setup();
-        const m = transform({ fontSize: 32, display: 'grid' });
-
-        const className = `${dom.prefix}-${m.hx}`;
-        expect(findCssRule(className)).to.eql(undefined); // NB: nothing inserted yet.
-
-        dom.class(m.style, m.hx);
-        const rule = findCssRule(className);
-        expect(rule?.cssText).to.eql(`.${className} { ${m.toString()} }`);
-      });
+      test('foo', 'foo');
+      test('  foo  ', 'foo');
+      test(' foo- ', 'foo'); // NB: trimmed.
+      test(' foo-- ', 'foo');
+      test('foo123', 'foo123');
+      test('foo-123', 'foo-123');
     });
-  },
-);
+
+    it('pooling (instance reuse keyed on "prefix")', () => {
+      const a = CssDom.create();
+      const b = CssDom.create(DEFAULTS.prefix);
+      const c = CssDom.create('foo');
+      expect(a).to.equal(b);
+      expect(a).to.not.equal(c);
+    });
+
+    it('insert root <style> into DOM (singleton)', () => {
+      const find = () => document.querySelector(`style[data-controller="${pkg.name}"]`);
+      CssDom.create();
+      expect(find()).to.exist;
+      CssDom.create();
+      expect(find()).to.equal(find()); // Singleton.
+    });
+
+    it('throw: invalid prefix', () => {
+      const test = (prefix: string) => {
+        const fn = () => CssDom.create(prefix);
+        expect(fn).to.throw(
+          /String must start with a letter and can contain letters, digits, and hyphens \(hyphen not allowed at the beginning\)/,
+        );
+      };
+      test('123foo'); // NB: Starts with a number.
+      test(' 123-foo ');
+      test('-foo');
+      test('-');
+      test('foo*bar');
+    });
+  });
+
+  describe('class/style DOM insertion', () => {
+    const setup = (): t.CssDom => {
+      const prefix = `sample-${slug()}`;
+      return CssDom.create(prefix);
+    };
+
+    it('simple ("hx" not passed)', () => {
+      const dom = setup();
+      const m = transform({ fontSize: 32, display: 'grid', PaddingX: [5, 10] });
+      expect(dom.classes.length).to.eql(0); // NB: no "inserted classes" yet.
+
+      // Baseline: ensure the rule is not yet within the DOM.
+      const className = `${dom.prefix}-${m.hx}`;
+      expect(findCssRule(className)).to.eql(undefined); // NB: nothing inserted yet.
+
+      const a = dom.class(m.style);
+      const b = dom.class(m.style);
+
+      expect(dom.classes.length).to.eql(1); // NB: not added twice.
+      expect(a).to.eql(b);
+      expect(a).to.eql(className);
+
+      // Ensure is inserted within DOM.
+      const rule = findCssRule(className);
+      expect(rule?.cssText).to.eql(`.${className} { ${m.toString()} }`);
+    });
+
+    it('hash passed as parameter', () => {
+      const dom = setup();
+      const m = transform({ fontSize: 32, display: 'grid' });
+
+      const className = `${dom.prefix}-${m.hx}`;
+      expect(findCssRule(className)).to.eql(undefined); // NB: nothing inserted yet.
+
+      dom.class(m.style, m.hx);
+      const rule = findCssRule(className);
+      expect(rule?.cssText).to.eql(`.${className} { ${m.toString()} }`);
+    });
+  });
+});
--- code/sys.ui/ui-css/src/m.Style/-transform.test.ts
+++ code/sys.ui/ui-css/src/m.Style/-transform.test.ts
@@ -2,167 +2,160 @@ import { type t, DomMock, describe, expect, findCssRule, it } from '../-test.ts'
 import { toHash } from './common.ts';
 import { Style, css, transform } from './mod.ts';
 
-describe(
-  'Style.css',
+describe('Style.css', () => {
+  DomMock.polyfill();
 
-  /** NOTE: leaked timers left around by the "happy-dom" module. */
-  { sanitizeOps: false, sanitizeResources: false },
+  it('API', () => {
+    expect(Style.transform).to.equal(transform);
+    expect(css).to.equal(transform);
+  });
 
-  () => {
-    DomMock.polyfill();
+  describe('css → { styles }', () => {
+    it('empty', () => {
+      const a = transform();
+      const b = transform([]);
+      const c = transform(...[], false);
+      const d = transform(null, undefined, [], false, ...[]);
 
-    it('API', () => {
-      expect(Style.transform).to.equal(transform);
-      expect(css).to.equal(transform);
+      expect(a.style).to.eql({});
+      expect(b).to.eql(a);
+      expect(c).to.eql(a);
+      expect(d).to.eql(a);
+
+      // Cached instances (on hx).
+      expect(a.style).to.equal(b.style);
+      expect(a.style).to.equal(c.style);
+      expect(a.style).to.equal(d.style);
     });
 
-    describe('css → { styles }', () => {
-      it('empty', () => {
-        const a = transform();
-        const b = transform([]);
-        const c = transform(...[], false);
-        const d = transform(null, undefined, [], false, ...[]);
+    it('plain CSS fields', () => {
+      const a = transform({ fontSize: 30 });
+      const b = transform({ fontSize: 30 });
+      const c = transform({ fontSize: 31 });
 
-        expect(a.style).to.eql({});
-        expect(b).to.eql(a);
-        expect(c).to.eql(a);
-        expect(d).to.eql(a);
+      expect(a.style.fontSize).to.eql(30);
+      expect(b.style.fontSize).to.eql(30);
+      expect(c.style.fontSize).to.eql(31);
 
-        // Cached instances (on hx).
-        expect(a.style).to.equal(b.style);
-        expect(a.style).to.equal(c.style);
-        expect(a.style).to.equal(d.style);
-      });
+      expect(a).to.equal(b);
+      expect(a).to.not.equal(c);
+    });
+  });
 
-      it('plain CSS fields', () => {
-        const a = transform({ fontSize: 30 });
-        const b = transform({ fontSize: 30 });
-        const c = transform({ fontSize: 31 });
-
-        expect(a.style.fontSize).to.eql(30);
-        expect(b.style.fontSize).to.eql(30);
-        expect(c.style.fontSize).to.eql(31);
-
-        expect(a).to.equal(b);
-        expect(a).to.not.equal(c);
-      });
+  describe('{ Template } → {styles} | capitalised known templates.', () => {
+    it('Absolute: 0', () => {
+      const a = transform({ Absolute: 0 });
+      const b = { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 };
+      expect(a.style).to.eql(b);
+      expect(a.hx).to.eql(toHash(b));
+      expect(a.style.Absolute).to.eql(undefined); // NB: clean up on object after transform.
     });
 
-    describe('{ Template } → {styles} | capitalised known templates.', () => {
-      it('Absolute: 0', () => {
-        const a = transform({ Absolute: 0 });
-        const b = { position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 };
-        expect(a.style).to.eql(b);
-        expect(a.hx).to.eql(toHash(b));
-        expect(a.style.Absolute).to.eql(undefined); // NB: clean up on object after transform.
-      });
+    it('PaddingX: [10, 20]', () => {
+      const a = transform({ PaddingX: [10, 20] });
+      const b = { paddingLeft: 10, paddingRight: 20 };
+      expect(a.style).to.eql(b);
+      expect(a.hx).to.eql(toHash(b));
+      expect(a.style.PaddingX).to.eql(undefined); // NB: clean up on object after transform.
+    });
+  });
 
-      it('PaddingX: [10, 20]', () => {
-        const a = transform({ PaddingX: [10, 20] });
-        const b = { paddingLeft: 10, paddingRight: 20 };
-        expect(a.style).to.eql(b);
-        expect(a.hx).to.eql(toHash(b));
-        expect(a.style.PaddingX).to.eql(undefined); // NB: clean up on object after transform.
-      });
+  describe('css → "class-name" (inserted into DOM)', () => {
+    it('simple', () => {
+      const style = { fontSize: 22 };
+      const a = transform(style);
+      const className = `sys-${a.hx}`;
+      expect(findCssRule(className)).to.eql(undefined);
+
+      expect(a.class).to.eql(className);
+      expect(findCssRule(className)?.cssText).to.include(className);
+      expect(findCssRule(className)?.cssText).to.include(a.toString());
+    });
+  });
+
+  describe('toString', () => {
+    it('empty', () => {
+      expect(transform({}).toString()).to.eql('');
     });
 
-    describe('css → "class-name" (inserted into DOM)', () => {
-      it('simple', () => {
-        const style = { fontSize: 22 };
-        const a = transform(style);
-        const className = `sys-${a.hx}`;
-        expect(findCssRule(className)).to.eql(undefined);
+    it('simple', () => {
+      const a = transform({ fontSize: 30, fontFamily: 'sans-serif' });
+      expect(a.toString()).to.eql('font-size: 30px; font-family: sans-serif;');
+    });
+  });
 
-        expect(a.class).to.eql(className);
-        expect(findCssRule(className)?.cssText).to.include(className);
-        expect(findCssRule(className)?.cssText).to.include(a.toString());
-      });
+  describe('merging', () => {
+    it('basic merge', () => {
+      const a = transform({ color: 'red' });
+      const b = transform({ background: 'blue' });
+
+      const res = transform(a, b);
+      expect(res.style).to.include({ color: 'red' });
+      expect(res.style).to.include({ background: 'blue' });
     });
 
-    describe('toString', () => {
-      it('empty', () => {
-        expect(transform({}).toString()).to.eql('');
-      });
-
-      it('simple', () => {
-        const a = transform({ fontSize: 30, fontFamily: 'sans-serif' });
-        expect(a.toString()).to.eql('font-size: 30px; font-family: sans-serif;');
-      });
-    });
-
-    describe('merging', () => {
-      it('basic merge', () => {
-        const a = transform({ color: 'red' });
-        const b = transform({ background: 'blue' });
-
-        const res = transform(a, b);
+    it('deep merge', () => {
+      const assert = (res: t.CssTransformed) => {
         expect(res.style).to.include({ color: 'red' });
         expect(res.style).to.include({ background: 'blue' });
-      });
+      };
 
-      it('deep merge', () => {
-        const assert = (res: t.CssTransformed) => {
-          expect(res.style).to.include({ color: 'red' });
-          expect(res.style).to.include({ background: 'blue' });
-        };
+      const props = { style: { color: 'red' } };
+      const styles = { base: transform({ background: 'blue' }) };
 
-        const props = { style: { color: 'red' } };
-        const styles = { base: transform({ background: 'blue' }) };
-
-        assert(transform(styles.base, props.style));
-        assert(transform(styles.base, css(props.style)));
-        assert(transform(transform(styles.base), transform(transform(props.style))));
-        assert(transform([css(styles.base), transform(transform(props.style))]));
-        assert(
-          transform(
-            transform([transform(styles.base), css(transform(props.style))]),
-            transform([transform(styles.base), [transform(transform([css([[props.style]])]))]]),
-          ),
-        );
-      });
-
-      it('deep merge ← {style} object', () => {
-        const assert = (res: t.CssTransformed) => {
-          expect(res.style).to.include({ color: 'red' });
-          expect(res.style).to.include({ background: 'blue' });
-        };
-
-        const props = { style: { color: 'red' } };
-        const styles = { base: transform({ background: 'blue' }) };
-
-        assert(transform(styles.base, props.style));
-        assert(transform(styles.base, transform(props.style)));
-        assert(transform(transform(styles.base), transform(transform(props.style))));
-        assert(transform([transform(styles.base), transform(transform(props.style))]));
-        assert(
-          transform(
-            transform([transform(styles.base), Style.transform(transform(props.style))]),
-            transform([
-              transform(styles.base),
-              [Style.transform(transform([transform([[props.style]])]))],
-            ]),
-          ),
-        );
-      });
-
-      it('cache name repeats (reused)', () => {
-        const props = { style: { color: 'red' } };
-        const styles = { base: transform({ background: 'blue' }) };
-
-        const a = transform(transform(styles.base), transform(transform(props.style)));
-        const b = transform(transform(styles.base), transform(transform(props.style)));
-        expect(a.hx).to.equal(b.hx);
-      });
-
-      it('wrapped ← equality', () => {
-        const a = transform({ color: 'red' });
-        const b = transform(a);
-        const c = transform([b]);
-        const d = transform([a, c], b);
-        expect(b).to.eql(a);
-        expect(c).to.eql(a);
-        expect(d).to.eql(a);
-      });
+      assert(transform(styles.base, props.style));
+      assert(transform(styles.base, css(props.style)));
+      assert(transform(transform(styles.base), transform(transform(props.style))));
+      assert(transform([css(styles.base), transform(transform(props.style))]));
+      assert(
+        transform(
+          transform([transform(styles.base), css(transform(props.style))]),
+          transform([transform(styles.base), [transform(transform([css([[props.style]])]))]]),
+        ),
+      );
     });
-  },
-);
+
+    it('deep merge ← {style} object', () => {
+      const assert = (res: t.CssTransformed) => {
+        expect(res.style).to.include({ color: 'red' });
+        expect(res.style).to.include({ background: 'blue' });
+      };
+
+      const props = { style: { color: 'red' } };
+      const styles = { base: transform({ background: 'blue' }) };
+
+      assert(transform(styles.base, props.style));
+      assert(transform(styles.base, transform(props.style)));
+      assert(transform(transform(styles.base), transform(transform(props.style))));
+      assert(transform([transform(styles.base), transform(transform(props.style))]));
+      assert(
+        transform(
+          transform([transform(styles.base), Style.transform(transform(props.style))]),
+          transform([
+            transform(styles.base),
+            [Style.transform(transform([transform([[props.style]])]))],
+          ]),
+        ),
+      );
+    });
+
+    it('cache name repeats (reused)', () => {
+      const props = { style: { color: 'red' } };
+      const styles = { base: transform({ background: 'blue' }) };
+
+      const a = transform(transform(styles.base), transform(transform(props.style)));
+      const b = transform(transform(styles.base), transform(transform(props.style)));
+      expect(a.hx).to.equal(b.hx);
+    });
+
+    it('wrapped ← equality', () => {
+      const a = transform({ color: 'red' });
+      const b = transform(a);
+      const c = transform([b]);
+      const d = transform([a, c], b);
+      expect(b).to.eql(a);
+      expect(c).to.eql(a);
+      expect(d).to.eql(a);
+    });
+  });
+});
