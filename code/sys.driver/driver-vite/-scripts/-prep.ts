285feff5929626b3137d04ccbcde2807a2dc1319
Phil Cockfield <phil@cockfield.net>
2025-02-14 15:36:23 +1300

    prep: save deps.yaml

--- code/sys.driver/driver-vite/-scripts/-prep.ts
+++ code/sys.driver/driver-vite/-scripts/-prep.ts
@@ -1,9 +1,23 @@
 import { Vite } from '@sys/driver-vite';
-import { Fs, PATHS, c, pkg } from './common.ts';
+import { type t, DenoDeps, Fs, PATHS, c, pkg, DenoFile } from './common.ts';
 
 const resolve = (...parts: string[]) => Fs.join(import.meta.dirname ?? '', '..', ...parts);
 await Fs.remove(resolve('.tmp'));
 
+/**
+ * Save monorepo deps.
+ */
+const ws = await DenoFile.workspace();
+const deps: t.Dep[] = ws.modules.items.map((m) => DenoDeps.toDep(m));
+const yaml = DenoDeps.toYaml(deps);
+
+const dir = resolve('src/-tmpl/.sys');
+await Fs.copy(Fs.join(ws.dir, 'deps.yaml'), Fs.join(dir, 'deps.yaml'), { force: true });
+await Fs.write(Fs.join(dir, 'deps.sys.yaml'), yaml.text);
+
+/**
+ * Bundle files (for code-registry).
+ */
 const bundle = Vite.Tmpl.Bundle;
 await bundle.toFilemap();
 await bundle.writeToFile(resolve(PATHS.tmpl.tmp)); // NB: test output.
