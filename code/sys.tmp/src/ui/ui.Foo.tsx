a4b072481f9651565e1b82ac93afdcd3da6d0157
Phil Cockfield <phil@cockfield.net>
2025-02-14 16:27:09 +1300

    mouse over demo

--- code/sys.tmp/src/ui/ui.Foo.tsx
+++ code/sys.tmp/src/ui/ui.Foo.tsx
@@ -17,16 +17,19 @@ export const Foo: React.FC<FooProps> = (props) => {
   const { enabled = true } = props;
   let text = `${pkg.name}@${pkg.version}/ui:<Foo>`;
 
+  const [isOver, setOver] = React.useState(false);
+  const over = (isOver: boolean) => () => setOver(isOver);
+
   const styles = {
     base: css({
       display: 'inline-block',
-      backgroundColor: Color.RUBY,
+      backgroundColor: `rgba(255, 0, 0, ${isOver ? 0.9 : 0.3})` /* RED */,
     }),
   };
 
   if (!enabled) text += ' (disabled)';
   return (
-    <div className={styles.base.class}>
+    <div className={styles.base.class} onMouseEnter={over(true)} onMouseLeave={over(false)}>
       <code>{text}</code>
     </div>
   );
