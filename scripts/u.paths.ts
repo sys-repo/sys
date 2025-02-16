ff0a5e91fd3af1483b49ac6082f21f3bdbaef1f9
Phil Cockfield <phil@cockfield.net>
2025-02-17 10:54:47 +1300

    temporarily re-enabled prettier

--- scripts/u.paths.ts
+++ scripts/u.paths.ts
@@ -1,5 +1,5 @@
-import { default as denojson } from "../deno.json" with { type: "json" };
-export { Path } from "@sys/std";
+import { default as denojson } from '../deno.json' with { type: 'json' };
+export { Path } from '@sys/std';
 
 export const Paths = {
   workspace: denojson.workspace,
@@ -7,55 +7,55 @@ export const Paths = {
     /**
      * @sys: standard libs.
      */
-    "code/sys/types",
-    "code/sys/std",
-    "code/sys/color",
+    'code/sys/types',
+    'code/sys/std',
+    'code/sys/color',
 
-    "code/sys/testing",
-    "code/sys/fs",
-    "code/sys/cli",
-    "code/sys/process",
-    "code/sys/crypto",
+    'code/sys/testing',
+    'code/sys/fs',
+    'code/sys/cli',
+    'code/sys/process',
+    'code/sys/crypto',
 
-    "code/sys/http",
-    "code/sys/cmd",
-    "code/sys/text",
-    "code/sys/tmpl",
-    "code/sys/jsr",
+    'code/sys/http',
+    'code/sys/cmd',
+    'code/sys/text',
+    'code/sys/tmpl',
+    'code/sys/jsr',
 
     /**
      * →| dev/null
      */
-    "code/sys.tmp",
+    'code/sys.tmp',
 
     /**
      * UI
      */
-    "code/sys.ui/ui-css",
-    "code/sys.ui/ui-dom",
-    "code/sys.ui/ui-react",
-    "code/sys.ui/ui-react-devharness",
-    "code/sys.ui/ui-react-components",
+    'code/sys.ui/ui-css',
+    'code/sys.ui/ui-dom',
+    'code/sys.ui/ui-react',
+    'code/sys.ui/ui-react-devharness',
+    'code/sys.ui/ui-react-components',
 
     /**
      * Drivers
      */
     // 'code/sys.driver/driver-automerge',
-    "code/sys.driver/driver-deno",
-    "code/sys.driver/driver-immer",
-    "code/sys.driver/driver-vite",
-    "code/sys.driver/driver-vitepress",
+    'code/sys.driver/driver-deno',
+    'code/sys.driver/driver-immer',
+    'code/sys.driver/driver-vite',
+    'code/sys.driver/driver-vitepress',
 
     //
-    "code/sys.driver/driver-obsidian",
-    "code/sys.driver/driver-ollama",
-    "code/sys.driver/driver-orbiter",
-    "code/sys.driver/driver-quilibrium",
+    'code/sys.driver/driver-obsidian',
+    'code/sys.driver/driver-ollama',
+    'code/sys.driver/driver-orbiter',
+    'code/sys.driver/driver-quilibrium',
 
     /**
      * Barrels
      */
-    "code/sys/sys",
-    "code/sys/main",
+    'code/sys/sys',
+    'code/sys/main',
   ],
 } as const;
