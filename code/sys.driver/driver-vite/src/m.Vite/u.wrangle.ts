d010824de94dac3e404ba29837da45d78f823492
Phil Cockfield <phil@cockfield.net>
2025-02-21 11:12:17 +1300

    path normalize hack

--- code/sys.driver/driver-vite/src/m.Vite/u.wrangle.ts
+++ code/sys.driver/driver-vite/src/m.Vite/u.wrangle.ts
@@ -1,4 +1,4 @@
-import { type t, Path } from './common.ts';
+import { type t, Path, ViteConfig } from './common.ts';
 
 /**
  * Helpers
@@ -9,5 +9,37 @@ export const Wrangle = {
     const cmd = `deno run -A --node-modules-dir npm:vite ${arg} --config=${config}`;
     const args = cmd.split(' ').slice(1);
     return { cmd, args } as const;
+  },
+
+  async pathsFromConfigfile(cwd?: t.StringDir) {
+    const rootDir = cwd || Path.cwd();
+    const filename = 'vite.config.ts';
+    const path = Path.join(rootDir, filename);
+
+    const res = await ViteConfig.fromFile(path);
+    let paths = res.module.paths;
+
+    if (!paths) {
+      const err = `Failed to load paths from [${filename}], ensure it exports "paths". Source: ${path}`;
+      throw new Error(err);
+    }
+
+    const delta = Path.relative(paths.cwd, rootDir);
+    if (delta) {
+      /**
+       * HACK:
+       *    There is a delta between the response {paths} current-working-directory (CWD)
+       *    and the actual CWD. Adjust the relevant subpaths to account for this.
+       * NB:
+       *    One scenario where this happens happen when testing within the mono-repo and
+       *    simulating operating in a root CWD, but actually building into ".tmp/sample/"
+       */
+      const entry = Path.normalize(Path.join(delta, paths.app.entry));
+      const outDir = Path.normalize(Path.join(delta, paths.app.outDir));
+      const app = { ...paths.app, entry, outDir };
+      paths = { ...paths, app };
+    }
+
+    return paths;
   },
 } as const;
