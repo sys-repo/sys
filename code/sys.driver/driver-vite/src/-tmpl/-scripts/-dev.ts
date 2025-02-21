cc98de2582be0feb2b5ce3b2903c1704edef2e57
Phil Cockfield <phil@cockfield.net>
2025-02-21 20:08:02 +1300

    clean up scripts

--- code/sys.driver/driver-vite/src/-tmpl/-scripts/-dev.ts
+++
@@ -1,6 +0,0 @@
-import { Vite } from '@sys/driver-vite';
-import { pkg } from '../src/pkg.ts';
-
-const input = './src/-test/index.html';
-const server = await Vite.dev({ pkg, input });
-await server.listen();
