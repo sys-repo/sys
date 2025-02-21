60cebb99d2f00a67a1e49f755c2e8b79a3a141b2
Phil Cockfield <phil@cockfield.net>
2025-02-21 18:48:04 +1300

    Update u.wrangle.ts

--- code/sys.driver/driver-vite/src/m.Vite/u.wrangle.ts
+++ code/sys.driver/driver-vite/src/m.Vite/u.wrangle.ts
@@ -20,9 +20,6 @@ export const Wrangle = {
     let paths = res.module.paths;
 
     if (!paths) {
-      // TEMP 🐷
-      console.log('res', { ...res });
-
       const err = `Failed to load paths from [${filename}], ensure it exports "paths". Source: ${path}`;
       throw new Error(err);
     }
