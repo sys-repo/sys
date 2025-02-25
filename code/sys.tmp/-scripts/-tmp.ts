177e19a073ba2ed5c959d928eba2adfdad9a5e05
Phil Cockfield <phil@cockfield.net>
2025-02-25 14:02:51 +1300

    Update -tmp.ts

--- code/sys.tmp/-scripts/-tmp.ts
+++ code/sys.tmp/-scripts/-tmp.ts
@@ -1,10 +1,1 @@
-import { build, emptyDir } from '@deno/dnt';
-// console.log('tmp', tmp);
-
-/**
- * https://jsr.io/@deno/dnt
- */
-
-console.log('build:', build);
-
-await emptyDir('./npm');
+console.log('tmp', '🐷');
