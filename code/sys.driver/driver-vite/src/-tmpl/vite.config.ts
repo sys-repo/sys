a9f3609393d162d42451ab1bdd1d363d00134fbd
Phil Cockfield <phil@cockfield.net>
2025-02-28 12:51:48 +1300

    prep

--- code/sys.driver/driver-vite/src/-tmpl/vite.config.ts
+++ code/sys.driver/driver-vite/src/-tmpl/vite.config.ts
@@ -1,9 +1,9 @@
 import { Vite } from '@sys/driver-vite';
 import { defineConfig } from 'vite';
 
-export const paths = Vite.Config.paths({ app: { entry: './src/-test/index.html' } });
-
 export default defineConfig(() => {
+  const entry = './src/-test/index.html';
+  const paths = Vite.Config.paths({ app: { entry } });
   return Vite.Config.app({
     paths,
     chunks(e) {
