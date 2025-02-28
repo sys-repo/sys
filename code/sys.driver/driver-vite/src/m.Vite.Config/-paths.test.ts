40dec55560d03a0ebb55f96912cda7bd80339add
Phil Cockfield <phil@cockfield.net>
2025-02-28 20:09:01 +1300

    test: refactor files

--- code/sys.driver/driver-vite/src/m.Vite.Config/-paths.test.ts
+++ code/sys.driver/driver-vite/src/m.Vite.Config/-paths.test.ts
@@ -1,11 +1,10 @@
-import { type t, c, describe, expect, it, Path, SAMPLE } from '../-test.ts';
-import { Vite } from '../mod.ts';
+import { c, describe, expect, it, Path } from '../-test.ts';
 import { ViteConfig } from './mod.ts';
 
-describe('ViteConfig: paths', () => {
+describe('ViteConfig.paths', () => {
   const { brightCyan: cyan, bold } = c;
 
-  describe('ViteConfig.paths', () => {
+  describe('[ViteConfigPaths]: data structure', () => {
     it('default paths (empty params)', () => {
       const a = ViteConfig.paths();
       const b = ViteConfig.paths();
@@ -63,54 +62,6 @@ describe('ViteConfig: paths', () => {
       expect(b.cwd).to.eql(Path.dirname(Path.fromFileUrl(import.meta.url)));
       expect(c.cwd).to.eql('/foo/bar');
       expect(d.cwd).to.eql(b.cwd);
-    });
-  });
-
-  describe('ViteConfig.fromFile', () => {
-    it('from "/<root-dir>/"', async () => {
-      const rootDir = SAMPLE.Dirs.sample2;
-      const res = await ViteConfig.fromFile(rootDir);
-      expect(res.exists).to.eql(true);
-      expect(res.error).to.eql(undefined);
-
-      expect(res.paths?.cwd).to.eql(Path.resolve(rootDir));
-      expect(res.paths?.app.entry).to.eql('src/-entry/index.html');
-      expect(res.paths?.app.base).to.eql('./');
-      expect(res.paths?.app.outDir).to.eql('dist');
-    });
-
-    it('from "/<root-dir>/vite.config.ts" (with config filename)', async () => {
-      const rootDir = SAMPLE.Dirs.sample2;
-      const resA = await ViteConfig.fromFile(Path.join(rootDir, 'vite.config.ts'));
-      const resB = await ViteConfig.fromFile(rootDir);
-      expect(resA.exists).to.eql(true);
-      expect(resA.error).to.eql(undefined);
-      expect(resA).to.eql(resB);
-    });
-
-    it('no params: load from implicit {CWD}', async () => {
-      const res = await ViteConfig.fromFile();
-      expect(res.paths?.cwd).to.eql(Path.cwd());
-      expect(res.exists).to.eql(true);
-      expect(res.error).to.eql(undefined);
-    });
-
-    it('loads main samples', async () => {
-      const test = async (path: t.StringPath) => {
-        const res = await Vite.Config.fromFile(path);
-        expect(res.error).to.eql(undefined);
-        expect(ViteConfig.Is.paths(res.paths)).to.be.true;
-      };
-
-      await test('src/-test/vite.sample-config/simple/vite.config.ts');
-      await test('src/-test/vite.sample-config/custom/vite.config.ts');
-    });
-
-    it('fail: not found', async () => {
-      const res = await ViteConfig.fromFile('/foo/404/vite.config.ts');
-      expect(res.exists).to.eql(false);
-      expect(res.error?.message).to.include('A config file could not be found in directory');
-      expect(res.error?.message).to.include(': /foo/404');
     });
   });
 });
