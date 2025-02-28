834c0d58fcaa9f903f19f3566212c37c6987c4ef
Phil Cockfield <phil@cockfield.net>
2025-02-28 19:28:09 +1300

    re-export

--- code/sys.tmp/src/ui/mod.ts
+++ code/sys.tmp/src/ui/mod.ts
@@ -3,4 +3,4 @@
  * Sample UI modules and components.
  */
 export { Foo } from './ui.Foo.tsx';
-// export { VideoPlayer } from './ui.VideoPlayer.tsx';
+export { VideoPlayer } from './ui.VideoPlayer.tsx';
