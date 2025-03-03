b5a7c8a9731bab7d80bf6c112c540769857e8ea3
Phil Cockfield <phil@cockfield.net>
2025-03-03 13:27:44 +1300

    Update -reset.ts

--- code/sys.driver/driver-vite/-scripts/-reset.ts
+++ code/sys.driver/driver-vite/-scripts/-reset.ts
@@ -3,4 +3,5 @@ const remove = (path: string) => Fs.remove(Fs.resolve(path), { log: true });
 
 await remove('./dist');
 await remove('./.tmp');
+await remove('./.swc');
 await remove('./node_modules');
