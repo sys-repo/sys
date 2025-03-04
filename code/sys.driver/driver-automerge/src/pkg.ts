5138a61c6e590d0e1867cef075618dc41ba8ed95
Phil Cockfield <phil@cockfield.net>
2025-03-04 16:30:49 +1300

    update {pkg} meta-data across modulees

--- code/sys.driver/driver-automerge/src/pkg.ts
+++ code/sys.driver/driver-automerge/src/pkg.ts
@@ -2,5 +2,15 @@ import type { Pkg } from '@sys/types';
 
 /**
  * Package meta-data.
+ *
+ * AUTO-GENERATED:
+ *    This file is generated via the `prep` command across the
+ *    @system monorepo. See command:
+ *
+ *        cd ./<system-repo-root>
+ *        deno task prep
+ *
+ *    - DO check this file in to source-control.
+ *    - Do NOT manually alter the file (as your work will be lost).
  */
 export const pkg: Pkg = { name: '@sys/driver-automerge', version: '0.0.90' };
