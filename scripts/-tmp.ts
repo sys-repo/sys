c1e000b62572acfd20f4a08a6cf06356536173d5
Phil Cockfield <phil@cockfield.net>
2025-03-05 12:39:54 +1300

    tmp: script to copy source docs (md) to .tmp VitePress sample

--- scripts/-tmp.ts
+++ scripts/-tmp.ts
@@ -1,17 +1,36 @@
-import { Cli } from '@sys/cli';
-import { Paths } from './u.paths.ts';
+import { c, Cli } from '@sys/cli';
+import { Fs } from '@sys/fs';
 
-const options = Paths.modules.map((path) => {
-  return { name: path, value: path };
-});
+export async function copyDocs() {
+  const dir = {
+    source: '/Users/phil/Documents/Notes/tdb/slc/current',
+    target: Fs.resolve('code/sys.driver/driver-vitepress/.tmp/sample'),
+  } as const;
 
-const res = await Cli.Prompt.Checkbox.prompt({
-  message: '(Workflow) JSR Publish Modules:',
-  options,
-});
-console.log('res', res);
+  const table = Cli.table([]);
+  const push = (label: string, value: string) => table.push([c.gray(label), value]);
+
+  const copy = async (from: string, to: string) => {
+    from = Fs.join(dir.source, from);
+    to = Fs.join(dir.target, to);
+    await Fs.copy(from, to, { force: true });
+    push('From', from);
+    push('To', to);
+    table.push([]);
+  };
+
+  await copy('docs', 'docs');
+  await copy('src', 'src');
+
+  // Ouput
+  console.info();
+  console.info(c.bold(c.brightGreen('Copy')));
+  console.info(table.toString().trim());
+  console.info();
+}
 
 /**
- * Finish up.
+ * Run
  */
+await copyDocs();
 Deno.exit(0);
