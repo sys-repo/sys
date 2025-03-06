038f670fcf9e06a87f7e1f489b3ac74a5f42e69f
Phil Cockfield <phil@cockfield.net>
2025-03-07 12:16:08 +1300

    docs

--- code/sys.driver/driver-vite/-scripts/-main.ts
+++ code/sys.driver/driver-vite/-scripts/-main.ts
@@ -2,7 +2,7 @@
  * Sample used to pass "deno task" commands to the entry point.
  *
  * @example
- * Invoked via deno run command:
+ * Invoked via deno run command, eg:
  * ```bash
  *  deno run jsr:@sys/http/server/start --port=1234
  * ```
