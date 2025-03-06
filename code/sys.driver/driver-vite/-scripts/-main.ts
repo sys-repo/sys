482c3710052629f64cf1b12e99aee80f8fdc07f4
Phil Cockfield <phil@cockfield.net>
2025-03-07 12:17:34 +1300

    docs

--- code/sys.driver/driver-vite/-scripts/-main.ts
+++ code/sys.driver/driver-vite/-scripts/-main.ts
@@ -1,10 +1,5 @@
 /**
- * Sample used to pass "deno task" commands to the entry point.
- *
- * @example
- * Invoked via deno run command, eg:
- * ```bash
- *  deno run jsr:@sys/http/server/start --port=1234
- * ```
+ * Sample used to pass "deno task" commands to the entry point
+ * locally within the mono-repo rather than calling out to JSR.
  */
 import '../src/-entry/-main.ts';
