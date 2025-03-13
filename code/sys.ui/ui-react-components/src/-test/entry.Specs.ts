6299df04bb62f800b5c0e05ba9d6856290384282
Phil Cockfield <phil@cockfield.net>
2025-03-14 10:53:26 +1300

    Update entry.Specs.ts

--- code/sys.ui/ui-react-components/src/-test/entry.Specs.ts
+++ code/sys.ui/ui-react-components/src/-test/entry.Specs.ts
@@ -6,6 +6,9 @@ import type { t } from './common.ts';
 
 export const Specs = {
   'sys.ui.react.component.Panel': () => import('../ui/Panel/-SPEC.tsx'),
+  'sys.ui.react.component.Button': () => import('../ui/Button/-SPEC.tsx'),
+  'sys.ui.react.component.Icon': () => import('../ui/Icon/-SPEC.tsx'),
+
   'sys.ui.react.component.Player.Video': () => import('../ui/Player.Video/-SPEC.tsx'),
   'sys.ui.react.component.Player.Concept': () => import('../ui/Player.Concept/-SPEC.tsx'),
   'sys.ui.react.component.Player.Thumbnails': () => import('../ui/Player.Thumbnails/-SPEC.tsx'),
