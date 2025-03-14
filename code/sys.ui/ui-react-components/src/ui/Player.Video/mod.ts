0d5472e8167aea83f5e88a2bb36679eb01b2871c
Phil Cockfield <phil@cockfield.net>
2025-03-14 15:04:36 +1300

    delete old event hooks (for player)

--- code/sys.ui/ui-react-components/src/ui/Player.Video/mod.ts
+++ code/sys.ui/ui-react-components/src/ui/Player.Video/mod.ts
@@ -1,4 +1,2 @@
-export { VideoPlayer } from './ui.tsx';
-
 export { playerSignalsFactory } from './api.Signals.ts';
-export { usePlayerEvents } from './use.PlayerEvents.ts';
+export { VideoPlayer } from './ui.tsx';
