af7a0681e5d71bb8e3a5953803b8078c212a8cea
Phil Cockfield <phil@cockfield.net>
2025-03-18 15:35:09 +1300

    clean

--- code/sys.ui/ui-react-components/src/ui/Button/-SPEC.Debug.tsx
+++ code/sys.ui/ui-react-components/src/ui/Button/-SPEC.Debug.tsx
@@ -20,10 +20,7 @@ export const Debug: React.FC<P> = (props) => {
    */
   const theme = Color.theme(props.theme);
   const styles = {
-    base: css({
-      backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
-      color: theme.fg,
-    }),
+    base: css({ color: theme.fg }),
   };
 
   return (
