eac6b06d14081a3e8c0f639849be4709e119f8a3
Phil Cockfield <phil@cockfield.net>
2025-03-18 18:32:29 +1300

    Update mod.ts

--- code/sys.ui/ui-css/src/m.Css.Dom/mod.ts
+++ code/sys.ui/ui-css/src/m.Css.Dom/mod.ts
@@ -2,7 +2,7 @@
  * @module
  * Tools for programatically managing CSS stylesheets within the browser DOM.
  */
-import { type t, DEFAULT } from './common.ts';
+import { type t } from './common.ts';
 import { create } from './u.create.ts';
 
 export const CssDom: t.CssDomLib = {
