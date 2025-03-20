c07c07e5d9fadc2873a4b6c62cbc13cd749a2665
Phil Cockfield <phil@cockfield.net>
2025-03-21 10:38:05 +1300

    refactor

--- code/sys.ui/ui-react-components/src/common/t.ts
+++ code/sys.ui/ui-react-components/src/common/t.ts
@@ -2,6 +2,6 @@ export type * from '@sys/types';
 
 export type { SpecImports } from '@sys/testing/t';
 export type { CssEdgesInput, CssInput, CssMarginInput, CssValue } from '@sys/ui-css/t';
-export type { ReadonlySignal, Signal } from '@sys/ui-react/t';
+export type { ReadonlySignal, Signal, ExtractSignalValue } from '@sys/ui-react/t';
 
 export type * from '../types.ts';
