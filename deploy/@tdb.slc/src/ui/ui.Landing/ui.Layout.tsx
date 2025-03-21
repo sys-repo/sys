ce194379aca75760ab3408fd28b54ba867874d70
Phil Cockfield <phil@cockfield.net>
2025-03-21 20:17:15 +1300

    animated layout (WIP)

--- deploy/@tdb.slc/src/ui/ui.Landing/ui.Layout.tsx
+++ deploy/@tdb.slc/src/ui/ui.Landing/ui.Layout.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { type t, Color, css, Signal, DEFAULTS, rx, Motion } from './common.ts';
+import React from 'react';
+import { type t, Color, css, DEFAULTS, Motion } from './common.ts';
 
 export type LayoutProps = {
-  canvas: {
-    element?: JSX.Element;
-    position?: t.LandingCanvasPosition;
-  };
+  canvas: { element?: JSX.Element; position?: t.LandingCanvasPosition };
+  video: { element?: JSX.Element };
   theme?: t.CommonTheme;
   style?: t.CssInput;
 };
@@ -16,8 +14,8 @@ type P = LayoutProps;
  * Component:
  */
 export const Layout: React.FC<P> = (props) => {
-  const { canvas } = props;
-  const canvasPosition = canvas.position ?? 'Center:Bottom';
+  const { canvas, video } = props;
+  const position = canvas.position ?? DEFAULTS.canvasPosition;
 
   /**
    * Render:
@@ -31,12 +29,47 @@ export const Layout: React.FC<P> = (props) => {
       left: '50%',
       transform: 'translate(-50%, -50%) scale(1)',
     }),
+
+    video: css({
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: '85%',
+      aspectRatio: '16/9',
+      backgroundColor: 'white',
+      borderRadius: 15,
+      overflow: 'hidden',
+      display: position === 'Center:Bottom' ? 'grid' : 'none',
+    }),
   };
 
+  /**
+   * Animation state:
+   */
   const canvasState =
-    canvasPosition === 'Center:Bottom'
+    position === 'Center:Bottom'
       ? { top: 'calc(100% - 10px)', transform: 'translate(-50%, -100%) scale(0.65)' }
       : { top: '50%', transform: 'translate(-50%, -50%) scale(1.1)' };
+
+  const backgroundAnimate =
+    position === 'Center:Bottom'
+      ? { opacity: 1, top: '50%', transform: 'translate(-50%, calc(-50% - 80px))' }
+      : { opacity: 0, top: '50%', transform: 'translate(-50%, calc(-50% - 80px))' };
+
+  /**
+   * Elements:
+   */
+  const elVideo = (
+    <Motion.div
+      className={styles.video.class}
+      initial={false} // Prevent initial animation on mount.
+      animate={backgroundAnimate}
+      transition={{ type: 'spring', stiffness: 200, damping: 15, mass: 0.8 }}
+    >
+      {video.element}
+    </Motion.div>
+  );
 
   const elCanvas = canvas.element && (
     <Motion.div
@@ -49,5 +82,10 @@ export const Layout: React.FC<P> = (props) => {
     </Motion.div>
   );
 
-  return <div className={css(styles.base, props.style).class}>{elCanvas}</div>;
+  return (
+    <div className={css(styles.base, props.style).class}>
+      {elVideo}
+      {elCanvas}
+    </div>
+  );
 };
