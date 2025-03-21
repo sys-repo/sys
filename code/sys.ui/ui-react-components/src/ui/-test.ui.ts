619ad134c31c8600440870aa75e67ea932ec42a1
Phil Cockfield <phil@cockfield.net>
2025-03-21 19:11:58 +1300

    VIMEO constants

--- code/sys.ui/ui-react-components/src/ui/-test.ui.ts
+++ code/sys.ui/ui-react-components/src/ui/-test.ui.ts
@@ -5,3 +5,12 @@
 export { expect } from '@sys/std/testing';
 export { Dev, Spec } from '@sys/ui-react-devharness';
 export * from '../common.ts';
+
+/**
+ * Constants:
+ */
+export const VIMEO = {
+  'app/tubes': 499921561,
+  'stock/running': 287903693, // https://vimeo.com/stock/clip-287903693-silhouette-woman-running-on-beach-waves-splashing-female-athlete-runner-exercising-sprinting-intense-workout-on-rough-ocean-seas
+  'public/helvetica': 73809723,
+};
