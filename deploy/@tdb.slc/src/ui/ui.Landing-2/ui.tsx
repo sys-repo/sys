56440f804322d641ee4746c335acfbc3d378a756
Phil Cockfield <phil@cockfield.net>
2025-03-25 19:57:42 +1300

    resizing with @container

--- deploy/@tdb.slc/src/ui/ui.Landing-2/ui.tsx
+++ deploy/@tdb.slc/src/ui/ui.Landing-2/ui.tsx
@@ -4,9 +4,12 @@ import { type t, Color, css } from './common.ts';
 import { CanvasMini } from '../ui.Canvas.Mini/mod.ts';
 import { Logo } from '../ui.Logo/mod.ts';
 import { VideoBackgroundTubes } from '../ui.Video.Background.Tubes/mod.ts';
+import { useKeyboard } from '../ui.Landing-1/use.Keyboard.ts';
 
 export const Landing: React.FC<t.Landing2Props> = (props) => {
   const { debug = false, backgroundVideo = 0 } = props;
+
+  useKeyboard();
 
   /**
    * Render:
@@ -15,35 +18,41 @@ export const Landing: React.FC<t.Landing2Props> = (props) => {
   const styles = {
     base: css({
       position: 'relative',
-      containerType: 'inline-size',
+      containerType: 'size',
       color: theme.fg,
+      backgroundColor: theme.bg,
     }),
     background: css({ Absolute: 0 }),
     body: css({ Absolute: 0, overflow: 'hidden', display: 'grid', placeItems: 'center' }),
 
     canvas: css({
-      backgroundColor: 'rgba(255, 0, 0, 0.2)' /* RED */,
-      aspectRatio: '1.93/1',
       width: 400,
-    }).container('max-width: 550px', {
-      // Mobile screen:
-      width: 200,
-    }).done,
+      transition: 'width 200ms',
+    })
+      .container('max-width: 550px', { width: 350 })
+      .container('max-width: 450px', { width: 280 }).done,
+
+    logo: css({ Absolute: [null, 15, 12, null] })
+      .container('max-height: 470px', { Absolute: null })
+      .container('max-height: 470px', { display: 'none' }).done,
   };
 
   const elBackground = backgroundVideo !== undefined && (
     <VideoBackgroundTubes opacity={backgroundVideo} style={styles.background} />
   );
 
-  const elCanvas = <div className={styles.canvas.class}>canvas</div>;
-  // <CanvasMini theme={theme.name} />
+  const elCanvas = (
+    <div className={styles.canvas.class}>
+      <CanvasMini theme={theme.name} />
+    </div>
+  );
 
   return (
     <div className={css(styles.base, props.style).class}>
       {elBackground}
       <div className={styles.body.class}>
         {elCanvas}
-        <Logo theme={theme.name} />
+        <Logo theme={theme.name} style={styles.logo} />
       </div>
     </div>
   );
