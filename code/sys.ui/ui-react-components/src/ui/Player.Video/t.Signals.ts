bae4290f10d012d897a935b03ed1a1b8933248a9
Phil Cockfield <phil@cockfield.net>
2025-03-26 14:07:57 +1300

    refactor types

---
+++ code/sys.ui/ui-react-components/src/ui/Player.Video/t.Signals.ts
@@ -0,0 +1,64 @@
+import { RefObject } from 'react';
+import type { MediaPlayerProps, MediaPlayerInstance } from '@vidstack/react';
+import type { t } from './common.ts';
+
+/**
+ * Signals:
+ */
+export type PlayerSignalsFactory = (
+  defaults?: PlayerSignalsFactoryDefaults,
+) => t.VideoPlayerSignals;
+
+/** Defaults passed to the signals API factory. */
+export type PlayerSignalsFactoryDefaults = {
+  src?: t.StringVideoAddress;
+  loop?: boolean;
+  showControls?: boolean;
+  showFullscreenButton?: boolean;
+  cornerRadius?: number;
+  aspectRatio?: string;
+  autoPlay?: boolean;
+  muted?: boolean;
+  background?: boolean;
+};
+
+/**
+ * Signals API for dynamic control of the <VideoPlayer>.
+ */
+export type VideoPlayerSignals = {
+  props: {
+    ready: t.Signal<boolean>;
+    src: t.Signal<t.StringVideoAddress>;
+
+    /**
+     * Media.
+     */
+    playing: t.Signal<boolean>;
+    currentTime: t.Signal<t.Secs>;
+    loop: t.Signal<boolean>;
+    autoPlay: t.Signal<boolean>;
+    muted: t.Signal<boolean>;
+
+    /**
+     * Appearance.
+     */
+    showControls: t.Signal<boolean>;
+    showFullscreenButton: t.Signal<boolean>;
+    /** A background video, covers the container running silently (and generally auto-plays). */
+    background: t.Signal<boolean>;
+    aspectRatio: t.Signal<string>;
+    cornerRadius: t.Signal<number>;
+
+    /**
+     * Commands.
+     */
+    jumpTo: t.Signal<t.VideoPlayerJumpTo | undefined>;
+  };
+  jumpTo(second: t.Secs, options?: { play?: boolean }): VideoPlayerSignals;
+  play(): VideoPlayerSignals;
+  pause(): VideoPlayerSignals;
+  toggle(playing?: boolean): VideoPlayerSignals;
+};
+
+/** Structure representing a jump-to ("seek") location */
+export type VideoPlayerJumpTo = { second: t.Secs; play: boolean };
--- code/sys.ui/ui-react-components/src/ui/Player.Video/t.ts
+++ code/sys.ui/ui-react-components/src/ui/Player.Video/t.ts
@@ -1,65 +1,14 @@
-import type { MediaPlayerProps } from '@vidstack/react';
+import type { MediaPlayerInstance, MediaPlayerProps } from '@vidstack/react';
+import type { RefObject } from 'react';
 import type { t } from './common.ts';
 
+export type * from './t.Signals.ts';
+
+/** The address of a video (eg. "vimeo/499921561"). */
 export type StringVideoAddress = string;
 
-/**
- * Signals:
- */
-export type PlayerSignalsFactory = (
-  defaults?: PlayerSignalsFactoryDefaults,
-) => t.VideoPlayerSignals;
-
-/** Defaults passed to the signals API factory. */
-export type PlayerSignalsFactoryDefaults = {
-  src?: t.StringVideoAddress;
-  loop?: boolean;
-  showControls?: boolean;
-  showFullscreenButton?: boolean;
-  cornerRadius?: number;
-  aspectRatio?: string;
-  autoPlay?: boolean;
-  muted?: boolean;
-  background?: boolean;
-};
-
-/**
- * Signals API for dynamic control of the <VideoPlayer>.
- */
-export type VideoPlayerSignals = {
-  props: {
-    ready: t.Signal<boolean>;
-    src: t.Signal<t.StringVideoAddress>;
-
-    /**
-     * Media.
-     */
-    playing: t.Signal<boolean>;
-    currentTime: t.Signal<t.Secs>;
-    loop: t.Signal<boolean>;
-    autoPlay: t.Signal<boolean>;
-    muted: t.Signal<boolean>;
-
-    /**
-     * Appearance.
-     */
-    showControls: t.Signal<boolean>;
-    showFullscreenButton: t.Signal<boolean>;
-    /** A background video, covers the container running silently (and generally auto-plays). */
-    background: t.Signal<boolean>;
-    aspectRatio: t.Signal<string>;
-    cornerRadius: t.Signal<number>;
-
-    /**
-     * Commands.
-     */
-    jumpTo: t.Signal<t.VideoPlayerJumpTo | undefined>;
-  };
-  jumpTo(second: t.Secs, options?: { play?: boolean }): VideoPlayerSignals;
-  play(): VideoPlayerSignals;
-  pause(): VideoPlayerSignals;
-  toggle(playing?: boolean): VideoPlayerSignals;
-};
+/** A React reference to the MediaPlayer instance. */
+export type VideoPlayerRef = RefObject<MediaPlayerInstance>;
 
 /** Structure representing a jump-to ("seek") location */
 export type VideoPlayerJumpTo = { second: t.Secs; play: boolean };
