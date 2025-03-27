e2861be46cda5458011d462ff886e05a0f572237
Phil Cockfield <phil@cockfield.net>
2025-03-27 16:44:28 +1300

    Mobile Layout

---
+++ deploy/@tdb.slc/src/ui/ui.Layout.Mobile/-SPEC.Debug.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { type t, Button, Color, css, Signal } from './common.ts';
+
+/**
+ * Types:
+ */
+export type DebugProps = { debug: DebugSignals; style?: t.CssInput };
+export type DebugSignals = ReturnType<typeof createDebugSignals>;
+
+/**
+ * Signals:
+ */
+export function createDebugSignals(init?: (e: DebugSignals) => void) {
+  type P = t.MyComponentProps;
+  const s = Signal.create;
+  const props = { theme: s<P['theme']>('Light') };
+  const api = { props };
+  init?.(api);
+  return api;
+}
+
+/**
+ * Component:
+ */
+export const Debug: React.FC<DebugProps> = (props) => {
+  const { debug } = props;
+  const p = debug.props;
+
+  Signal.useRedrawEffect(() => {
+    p.theme.value;
+  });
+
+  /**
+   * Render:
+   */
+  const theme = Color.theme(p.theme.value);
+  const styles = {
+    base: css({ color: theme.fg }),
+  };
+
+  return (
+    <div className={css(styles.base, props.style).class}>
+      <Button
+        block
+        label={`theme: ${p.theme}`}
+        onClick={() => Signal.cycle<t.CommonTheme>(p.theme, ['Light', 'Dark'])}
+      />
+
+      <hr />
+    </div>
+  );
+};
