89ada31e5de417f3222505268969c098b92d47d5
Phil Cockfield <phil@cockfield.net>
2025-03-28 10:13:19 +1300

    rename folder

--- deploy/@tdb.slc/src/types.ts
+++ deploy/@tdb.slc/src/types.ts
@@ -6,14 +6,15 @@ export type * from './ui/m.Signals/t.ts';
 export type * from './ui/use/t.ts';
 
 export type * from './ui/ui.Canvas.Mini/t.ts';
-export type * from './ui/ui.Layout.Mobile/t.ts';
 export type * from './ui/ui.Logo/t.ts';
 export type * from './ui/ui.Video.Background.Tubes/t.ts';
+
+export type * from './ui/ui.Layout.Mobile/t.ts';
+export type * from './ui/ui.Layout/t.ts';
 
 export type * from './ui/ui.Landing-1/t.ts';
 export type * from './ui/ui.Landing-2/t.ts';
 export type * from './ui/ui.Landing-3/t.ts';
-export type * from './ui/ui.Landing/t.ts';
 
 export type * from './-sample/-ui.Videos/t.ts';
 
--- deploy/@tdb.slc/src/ui/ui.Landing-3/common.ts
+++ deploy/@tdb.slc/src/ui/ui.Landing-3/common.ts
@@ -4,7 +4,7 @@ export * from '../common.ts';
  * Libs:
  */
 export { createSignals } from '../m.Signals/mod.ts';
-export { LayoutBreakpoint } from '../ui.Landing/mod.ts';
+export { LayoutBreakpoint } from '../ui.Layout/mod.ts';
 export { MobileLayout } from '../ui.Layout.Mobile/mod.ts';
 export { VideoBackgroundTubes } from '../ui.Video.Background.Tubes/mod.ts';
 export { useDist, useKeyboard } from '../use/mod.ts';
