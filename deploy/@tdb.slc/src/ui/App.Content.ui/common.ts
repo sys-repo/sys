d2cef23232de9080ed3e009e4283ff5123565acc
Phil Cockfield <phil@cockfield.net>
2025-03-29 22:21:46 +1300

    Update common.ts

--- deploy/@tdb.slc/src/ui/App.Content.ui/common.ts
+++ deploy/@tdb.slc/src/ui/App.Content.ui/common.ts
@@ -1,14 +1,22 @@
+import { type t } from '../common.ts';
+
 export { AppContent } from '../App.Content/mod.ts';
 export { VIDEO } from '../App.Content/VIDEOS.ts';
 
 export { CanvasMini } from '../ui.Canvas.Mini/mod.ts';
 export { Layout } from '../ui.Layout/mod.ts';
 export { Logo } from '../ui.Logo/mod.ts';
-export { Sheet } from '../ui.Sheet/mod.ts';
+export { Sheet as BaseSheet } from '../ui.Sheet/mod.ts';
 
 export * from '../common.ts';
 
 /**
  * Constants
  */
-export const DEFAULTS = { baseSheetOffset: 15 };
+
+const sheetTheme: t.CommonTheme = 'Light';
+
+export const DEFAULTS = {
+  sheetTheme,
+  baseSheetOffset: 40,
+};
