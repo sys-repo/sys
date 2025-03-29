e37b3b984bcb0fc798bc59aa1661b35659c4241d
Phil Cockfield <phil@cockfield.net>
2025-03-30 11:04:39 +1300

    useKeyboard hook initiated at entry

--- deploy/@tdb.slc/src/-test/entry.tsx
+++ deploy/@tdb.slc/src/-test/entry.tsx
@@ -1,13 +1,27 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { pkg } from '../common.ts';
+
+import type { t } from '../common.ts';
+import { pkg } from '../pkg.ts';
+import { useKeyboard } from '../ui/use/use.Keyboard.ts';
 
 /**
  * Render UI.
  */
-globalThis.document.title = pkg.name;
-console.info('🐷 ./entry.tsx → Pkg:💦', pkg);
+const document = globalThis.document;
+if (document) {
+  document.title = pkg.name;
+  document.body.style.overflow = 'hidden'; // NB: suppress rubber-band effect.
+}
 
+const Root = (props: { state: t.AppSignals; children?: t.ReactNode }) => {
+  useKeyboard(props.state);
+  return props.children;
+};
+
+/**
+ * MAIN entry.
+ */
 export async function main() {
   const params = new URL(location.href).searchParams;
   const isDev = params.has('dev') || params.has('d');
@@ -17,21 +31,31 @@ export async function main() {
     /**
      * DevHarness:
      */
+    const { App } = await import('../ui/App.Signals/mod.ts');
     const { render } = await import('@sys/ui-react-devharness');
     const { Specs } = await import('./entry.Specs.ts');
+
+    const app = App.signals();
     const el = await render(pkg, Specs, { hrDepth: 3, style: { Absolute: 0 } });
-    root.render(<StrictMode>{el}</StrictMode>);
+
+    root.render(
+      <StrictMode>
+        <Root state={app}>{el}</Root>
+      </StrictMode>,
+    );
   } else {
     /**
      * Landing (entry):
      */
     const { Landing, App, AppContent } = await import('../ui/ui.Landing-3/mod.ts');
-    const state = App.signals();
-    state.stack.push(await AppContent.find('Trailer'));
+    const app = App.signals();
+    app.stack.push(await AppContent.find('Entry'));
 
     root.render(
       <StrictMode>
-        <Landing state={state} style={{ Absolute: 0 }} />
+        <Root state={app}>
+          <Landing state={app} style={{ Absolute: 0 }} />
+        </Root>
       </StrictMode>,
     );
   }
