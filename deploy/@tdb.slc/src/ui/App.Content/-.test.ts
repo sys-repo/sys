bda0817dee2d36a180a7591df909740629b1ec21
Phil Cockfield <phil@cockfield.net>
2025-03-30 15:31:28 +1300

    Player.find: unit test

--- deploy/@tdb.slc/src/ui/App.Content/-.test.ts
+++ deploy/@tdb.slc/src/ui/App.Content/-.test.ts
@@ -1,29 +1,58 @@
-import { describe, expect, it } from '../../-test.ts';
+import { type t, describe, expect, it } from '../../-test.ts';
 import { AppContent, VIDEO } from './mod.ts';
+import { App } from '../App.Signals/mod.ts';
 
-describe('Content', () => {
-  it('Entry', async () => {
-    const res = await AppContent.find('Entry');
-    expect(res?.id).to.eql('Entry');
+describe('AppContent', () => {
+  describe('.find: (factory)', () => {
+    it('Entry', async () => {
+      const res = await AppContent.find('Entry');
+      expect(res?.id).to.eql('Entry');
+    });
+
+    it('Trailer', async () => {
+      const res = await AppContent.find('Trailer');
+      expect(res?.video?.src).to.eql(VIDEO.Trailer.src);
+    });
+
+    it('Overview', async () => {
+      const res = await AppContent.find('Overview');
+      expect(res?.video?.src).to.eql(VIDEO.Overview.src);
+    });
+
+    it('Programme', async () => {
+      const res = await AppContent.find('Programme');
+      expect(res?.id).to.eql('Programme');
+    });
+
+    it('Not Found', async () => {
+      const res = await AppContent.find('Foobar' as any);
+      expect(res).to.eql(undefined);
+    });
   });
 
-  it('Trailer', async () => {
-    const res = await AppContent.find('Trailer');
-    expect(res?.video?.src).to.eql(VIDEO.Trailer.src);
-  });
+  describe('.Player.find', () => {
+    type T = t.Content;
+    const a: T = { id: 'foo' };
+    const b: T = { id: 'foo.bar', video: { src: 'vimeo/1234' } };
 
-  it('Overview', async () => {
-    const res = await AppContent.find('Overview');
-    expect(res?.video?.src).to.eql(VIDEO.Overview.src);
-  });
+    const test = (layer: t.StringId | T, index: number, expected: boolean) => {
+      const app = App.signals();
+      app.stack.push(a);
+      app.stack.push(b);
+      expect(Object.keys(app.props.players).length).to.eql(1);
 
-  it('Programme', async () => {
-    const res = await AppContent.find('Programme');
-    expect(res?.id).to.eql('Programme');
-  });
+      const player = AppContent.Player.find(app, layer, index);
+      expect(typeof player?.play === 'function').to.eql(expected);
+    };
 
-  it('Not Found', async () => {
-    const res = await AppContent.find('Foobar' as any);
-    expect(res).to.eql(undefined);
+    it('success', () => {
+      test(b, 1, true);
+      test(b.id, 1, true);
+    });
+
+    it('no-match', () => {
+      test(a, 0, false);
+      test(a, 1, false);
+    });
   });
 });
--- deploy/@tdb.slc/src/ui/App.Content/m.Content.tsx
+++ deploy/@tdb.slc/src/ui/App.Content/m.Content.tsx
@@ -1,7 +1,28 @@
+import type { t } from './common.ts';
 import { find } from './m.Content.find.tsx';
 import { render } from './m.Content.render.tsx';
 
 export const AppContent = {
   find,
   render,
+
+  /**
+   * Find the video-player controller for the given layer
+   */
+  Player: {
+    find(app: t.AppSignals, layer: t.StringId | t.Content, index: number) {
+      const layerId = wrangle.layerId(layer);
+      const key = `${layerId}.${index}`;
+      return app.props.players[key];
+    },
+  },
+} as const;
+
+/**
+ * Helpers
+ */
+const wrangle = {
+  layerId(input: t.StringId | t.Content): t.StringId {
+    return typeof input === 'string' ? input : input.id;
+  },
 } as const;
