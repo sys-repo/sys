e5b00d525357459744fb456fd52609a7301f65fc
Phil Cockfield <phil@cockfield.net>
2025-03-30 13:22:54 +1300

    exports

--- deploy/@tdb.slc/src/mod.ts
+++ deploy/@tdb.slc/src/mod.ts
@@ -6,3 +6,11 @@ export { pkg } from './pkg.ts';
 
 /** Module types. */
 export type * as t from './types.ts';
+
+/**
+ * Library
+ */
+export { App } from './ui/App.Signals/mod.ts';
+export { AppContent } from './ui/App.Content/mod.ts';
+
+import { App } from '@tdb/slc';
