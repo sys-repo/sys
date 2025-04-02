154a5ff6233cc8d12425c0d929c0aef392f53bf1
Phil Cockfield <phil@cockfield.net>
2025-04-02 14:50:22 +1300

    bump

--- code/sys/text/src/pkg.ts
+++ code/sys/text/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source-control.
  *    - Do NOT manually alter the file (as your work will be lost).
  */
-export const pkg: Pkg = { name: '@sys/text', version: '0.0.82' };
+export const pkg: Pkg = { name: '@sys/text', version: '0.0.83' };
