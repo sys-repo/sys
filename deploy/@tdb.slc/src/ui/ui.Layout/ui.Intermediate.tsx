ac614425d18c423313f826adaecbf29cf1123785
Phil Cockfield <phil@cockfield.net>
2025-04-12 15:00:29 +1200

    enhance size warning display (with icon)

--- deploy/@tdb.slc/src/ui/ui.Layout/ui.Intermediate.tsx
+++ deploy/@tdb.slc/src/ui/ui.Layout/ui.Intermediate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { type t, Color, Cropmarks, css } from './common.ts';
+import { type t, Color, Cropmarks, css, Icons } from './common.ts';
 
 type P = t.LayoutIntermediateProps;
 
@@ -15,13 +15,30 @@ export const LayoutIntermediate: React.FC<P> = (props) => {
    * Render:
    */
   const styles = {
-    base: css({ display: 'grid' }),
+    base: css({
+      position: 'relative',
+      userSelect: 'none',
+      display: 'grid',
+    }),
+    body: css({
+      Padding: [20, 40],
+      lineHeight: 1.5,
+      display: 'grid',
+      placeItems: 'center',
+    }),
   };
 
   return (
     <div className={css(styles.base, props.style).class}>
       <Cropmarks theme={'Dark'} borderOpacity={0.05}>
-        <div>{`🐷 Layout:Intermediate`}</div>
+        <div className={styles.body.class}>
+          <Icons.ProjectorScreen size={50} />
+          <p>
+            Please make your window bigger, or
+            <br />
+            move over to your mobile device.
+          </p>
+        </div>
       </Cropmarks>
     </div>
   );
