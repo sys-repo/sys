13032aad1a11d911b273ad5914ac90d53a00bb4e
Phil Cockfield <phil@cockfield.net>
2025-04-22 10:42:35 +1200

    add video reference

--- deploy/@tdb.slc/src/ui.Content/VIDEO.ts
+++ deploy/@tdb.slc/src/ui.Content/VIDEO.ts
@@ -8,4 +8,9 @@ export const VIDEO = {
   GroupScale: vimeo(727951677),
   Trailer: vimeo(1068502644),
   Overview: vimeo(1068653222),
+  Programme: {
+    Intro: {
+      About: vimeo(577856505),
+    },
+  },
 } as const;
