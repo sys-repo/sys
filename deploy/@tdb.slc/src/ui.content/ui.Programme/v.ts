a74901379de243cc5f6f61d3a91ca4eec0c955b4
Phil Cockfield <phil@cockfield.net>
2025-04-24 15:59:56 +1200

    folder rename

--- deploy/@tdb.slc/src/ui.content/ui.Programme/v.ts
+++ deploy/@tdb.slc/src/ui.content/ui.Programme/v.ts
@@ -1,18 +1,1 @@
-import type { t } from './common.ts';
-import { conculsion } from './v.conclusion.ts';
-import { metrics } from './v.metrics.ts';
-import { model } from './v.model.ts';
-import { start } from './v.start.ts';
-
-export { conculsion, metrics, model, start };
-
-export const children: t.VideoMediaContent[] = [
-  start,
-  model.customer,
-  model.impact,
-  model.econ,
-  metrics,
-  conculsion,
-];
-
-export const Programme = { children } as const;
+export * from './v/m.ts';
