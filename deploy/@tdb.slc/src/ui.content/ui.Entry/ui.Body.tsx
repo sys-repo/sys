7c4c6a1ea45aced9bfcce52444055f89a1eba53e
Phil Cockfield <phil@cockfield.net>
2025-04-24 19:16:55 +1200

    wait for ready on SVG imports

--- deploy/@tdb.slc/src/ui.content/ui.Entry/ui.Body.tsx
+++ deploy/@tdb.slc/src/ui.content/ui.Entry/ui.Body.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+
 import { type t, Color, css, LogoCanvas, LogoWordmark } from './common.ts';
 import { IntroButtons, StartProgrammeButton } from './ui.Buttons.tsx';
 
@@ -11,11 +12,24 @@ export type BodyProps = {
 const heartRateBPM = 72;
 const delay = 60_000 / heartRateBPM; // NB: 60_000 ms in a minute.
 
+type Part = 'Canvas' | 'Workmark';
+const PARTS: Part[] = ['Canvas', 'Workmark'] as const;
+
 /**
  * Component:
  */
 export const Body: React.FC<BodyProps> = (props) => {
   const { state } = props;
+
+  const readyRef = useRef(new Set<Part>());
+  const [, setRender] = useState(0);
+  const redraw = () => setRender((n) => n + 1);
+  const registerReady = (component: Part) => {
+    readyRef.current.add(component);
+    redraw();
+  };
+
+  const isReady: boolean = PARTS.every((part) => readyRef.current.has(part));
 
   /**
    * Render:
@@ -26,6 +40,8 @@ export const Body: React.FC<BodyProps> = (props) => {
       color: theme.fg,
       display: 'grid',
       placeItems: 'center',
+      opacity: isReady ? 0 : 1,
+      transition: 'opacity 200ms',
     }),
     brand: {
       base: css({ display: 'grid', placeItems: 'center', rowGap: '35px' }),
@@ -42,8 +58,13 @@ export const Body: React.FC<BodyProps> = (props) => {
           style={styles.brand.canvas}
           // selected={'purpose'}
           selectionAnimation={{ delay, loop: true }}
+          onReady={() => registerReady('Canvas')}
         />
-        <LogoWordmark theme={theme.name} style={styles.brand.wordmark} />
+        <LogoWordmark
+          theme={theme.name}
+          style={styles.brand.wordmark}
+          onReady={() => registerReady('Workmark')}
+        />
         <IntroButtons theme={theme.name} state={state} style={{ marginTop: 150 }} />
         <div>
           <StartProgrammeButton state={state} theme={theme.name} />
