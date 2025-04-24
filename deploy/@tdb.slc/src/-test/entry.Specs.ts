3cee1af111618593a7a4d8e2e1e4c5547887135d
Phil Cockfield <phil@cockfield.net>
2025-04-25 11:24:34 +1200

    Update entry.Specs.ts

--- deploy/@tdb.slc/src/-test/entry.Specs.ts
+++ deploy/@tdb.slc/src/-test/entry.Specs.ts
@@ -28,7 +28,7 @@ export const SpecsComponents = {
   'tdb.slc.content.ui.Playlist': () => import('../ui.content/ui/ui.Playlist/-SPEC.tsx'),
   'tdb.slc.content.ui.MenuList': () => import('../ui.content/ui/ui.MenuList/-SPEC.tsx'),
 
-  'tmp.🐷': () => import('../ui.tmp/-SPEC.tsx'),
+  'tmp.🐷 (mandelbrot)': () => import('../ui.tmp/-SPEC.tsx'),
 } as t.SpecImports;
 
 /**
