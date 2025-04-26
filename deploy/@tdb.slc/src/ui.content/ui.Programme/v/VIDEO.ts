d4c669fc6b2e3c33878ad77dbb07c5881a397cd3
Phil Cockfield <phil@cockfield.net>
2025-04-26 18:09:30 +1200

    video references

--- deploy/@tdb.slc/src/ui.content/ui.Programme/v/VIDEO.ts
+++ deploy/@tdb.slc/src/ui.content/ui.Programme/v/VIDEO.ts
@@ -5,11 +5,38 @@ import { vimeo } from './common.ts';
  */
 export const VIDEO = {
   Intro: {
+    Entry: vimeo(851209192),
     About: vimeo(577856505),
+    HowToUse: vimeo(851212258),
+    WhenFinished: vimeo(851212258),
+    Purpose: vimeo(577945255),
   },
   Model: {
     Customer: {
-      Customers: vimeo(577933592),
+      Entry: vimeo(577933592),
+      Customers: vimeo(577928799),
+      CustomerSegments: vimeo(577928799),
+      EarlyAdopter: vimeo(577929214),
+      JobsToBeDone: vimeo(577933180),
+      ExistingAlternatives: vimeo(577933435),
+      UVP: vimeo(577937937),
+      Solution: vimeo(577937777),
+    },
+    Impact: {
+      Entry: vimeo(577955565),
+      Context: vimeo(577955697),
+      Disclaimer: vimeo(577955864),
+      Issue: vimeo(577955305),
+      Participants: vimeo(577955976),
+      Activities: vimeo(577943744),
+      Outcomes: {
+        ShortTerm: vimeo(577956096),
+        MediumTerm: vimeo(577955482),
+        LongTerm: vimeo(577955408),
+      },
+      Impact: vimeo(577955192),
+    },
+    Economic: {
     },
   },
 };
