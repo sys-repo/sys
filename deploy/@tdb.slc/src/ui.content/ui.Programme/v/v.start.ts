dcc05f9b72432724d6f71a504a0c96bd4444a451
Phil Cockfield <phil@cockfield.net>
2025-04-27 12:46:36 +1200

    remove: "When Finished"

--- deploy/@tdb.slc/src/ui.content/ui.Programme/v/v.start.ts
+++ deploy/@tdb.slc/src/ui.content/ui.Programme/v/v.start.ts
@@ -6,17 +6,11 @@ const Intro = VIDEO.Intro;
 export const start: t.VideoMediaContent = {
   id: 'start',
   title: 'Getting Started',
-  video: v(Intro.Entry.src),
+  video: v(Intro.About.src),
   timestamps: {},
   children: [
     { id: 'about', title: `About this Programme`, video: v(Intro.About.src), timestamps: {} },
     { id: 'how', title: `How to use the Canvas`, video: v(Intro.HowToUse.src), timestamps: {} },
-    {
-      id: 'finished',
-      title: `When You're Finished`,
-      video: v(Intro.WhenFinished.src),
-      timestamps: {},
-    },
     { id: 'purpose', title: `Purpose`, video: v(Intro.Purpose.src), timestamps: {} },
   ],
 };
