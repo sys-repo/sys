7fbd7e559bb4463be95bbd50b6dc8694fc04466d
Phil Cockfield <phil@cockfield.net>
2025-04-28 17:35:12 +1200

    econ-model

--- deploy/@tdb.slc/src/ui.content/ui.Programme/v/v.model.econ.tsx
+++ deploy/@tdb.slc/src/ui.content/ui.Programme/v/v.model.econ.tsx
@@ -1,34 +1,54 @@
 import { type t, VIDEO } from './common.ts';
-import { v } from './u.tsx';
+import { Dir, image, v } from './u.tsx';
 
 const Economic = VIDEO.Model.Economic;
+const dir = Dir.programme.dir('model.econ');
 
 export const econ: t.VideoMediaContent = {
   id: 'model.econ',
   title: 'Economic Model',
   video: v(Economic.Entry.src),
-  timestamps: {},
+  timestamps: {
+    '00:00:00.000': { main: () => image(dir.path('economic-model-1.png')) },
+    '00:00:18.000': { main: () => image(dir.path('economic-model-2.png')) },
+  },
+
   children: [
-    { id: 'channels', title: `Channels`, video: v(Economic.Channels.src), timestamps: {} },
+    {
+      id: 'channels',
+      title: `Channels`,
+      video: v(Economic.Channels.src),
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('channels.png')) } },
+    },
     {
       id: 'financial',
       title: `Financial Model`,
       video: v(Economic.Financial.src),
-      timestamps: {},
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('financial-model.png')) } },
     },
-    { id: 'revenue', title: `Revenue`, video: v(Economic.Revenue.src), timestamps: {} },
-    { id: 'costs', title: `Costs`, video: v(Economic.Costs.src), timestamps: {} },
+    {
+      id: 'revenue',
+      title: `Revenue`,
+      video: v(Economic.Revenue.src),
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('revenue.png')) } },
+    },
+    {
+      id: 'costs',
+      title: `Costs`,
+      video: v(Economic.Costs.src),
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('costs.png')) } },
+    },
     {
       id: 'advantage.leverage',
       title: `Advantage: Leverage`,
       video: v(Economic.Advantage.Leverage.src),
-      timestamps: {},
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('advantage.png')) } },
     },
     {
       id: 'advantage.innovate',
       title: `Advantage: Innovate`,
       video: v(Economic.Advantage.Innovate.src),
-      timestamps: {},
+      timestamps: { '00:00:00.000': { main: () => image(dir.path('advantage.png')) } },
     },
     // { id: 'example', title: `Example`, video: v(Economic.Example.src), timestamps: {} },
   ],
