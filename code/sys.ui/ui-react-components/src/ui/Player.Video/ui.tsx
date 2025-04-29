f44c07388784f583d54c387a7855c1146e633f72
Phil Cockfield <phil@cockfield.net>
2025-04-30 10:31:11 +1200

    move debug label

--- code/sys.ui/ui-react-components/src/ui/Player.Video/ui.tsx
+++ code/sys.ui/ui-react-components/src/ui/Player.Video/ui.tsx
@@ -105,7 +105,7 @@ export const VideoPlayer: React.FC<P> = (props) => {
       lineHeight: 0, // NB: ensure no "baseline" gap below the <MediaPlayer>.
     }),
     debug: css({
-      Absolute: [-20, null, null, 6],
+      Absolute: [6, null, null, 6],
       color: theme.fg,
       fontSize: 11,
       opacity: 0.4,
