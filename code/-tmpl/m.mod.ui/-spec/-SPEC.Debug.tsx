a32845f7acbbb0374b7a75fd8dda7d1a10d317b4
Phil Cockfield <phil@cockfield.net>
2025-04-30 18:50:26 +1200

    Update -SPEC.Debug.tsx

--- code/-tmpl/m.mod.ui/-spec/-SPEC.Debug.tsx
+++ code/-tmpl/m.mod.ui/-spec/-SPEC.Debug.tsx
@@ -71,7 +71,7 @@ export const Debug: React.FC<DebugProps> = (props) => {
       />
 
       <hr />
-      <ObjectView name={'debug'} data={Signal.toObject(debug)} expand={1} />
+      <ObjectView name={'debug'} data={Signal.toObject(debug)} expand={['$']} />
     </div>
   );
 };
