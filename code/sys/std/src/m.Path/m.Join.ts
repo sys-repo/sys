81b1eac848b532789abb521bcd5fb90f2f2bb057
Phil Cockfield <phil@cockfield.net>
2025-05-05 16:52:44 +1200

    copy in common

--- code/sys/std/src/m.Path/m.Join.ts
+++ code/sys/std/src/m.Path/m.Join.ts
@@ -1,7 +1,7 @@
 import { join as auto } from '@std/path/join';
 import { join as posix } from '@std/path/posix/join';
 import { join as windows } from '@std/path/windows/join';
-import { type t } from '../common.ts';
+import { type t } from './common.ts';
 
 export const Join: t.PathJoinLib = {
   auto,
