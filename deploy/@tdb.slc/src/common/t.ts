e5b74fd4c2c84b2ff247ade88b874d8505942381
Phil Cockfield <phil@cockfield.net>
2025-05-06 19:22:41 +1200

    Update t.ts

--- deploy/@tdb.slc/src/common/t.ts
+++ deploy/@tdb.slc/src/common/t.ts
@@ -12,8 +12,8 @@ export type { ColorTheme } from '@sys/color/t';
 export type { ExtractSignalValue, Signal } from '@sys/std/t';
 export type { SpecImports } from '@sys/testing/t';
 export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
-
 export type { KeyboardModifierFlags } from '@sys/ui-dom/t';
+
 export type {
   SheetMarginInput,
   SheetOrientation,
