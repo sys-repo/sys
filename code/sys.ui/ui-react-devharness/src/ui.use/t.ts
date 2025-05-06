6441b39d69f77e2064dec5c87672bee4466c4919
Phil Cockfield <phil@cockfield.net>
2025-05-07 09:33:39 +1200

    consolidate keyboard controller actions

--- code/sys.ui/ui-react-devharness/src/ui.use/t.ts
+++ code/sys.ui/ui-react-devharness/src/ui.use/t.ts
@@ -1,4 +1,14 @@
+import type { t } from './common.ts';
+
 /**
  * Hook: DevHarness Keyboard controller.
  */
-export type UseKeyboardFactory = () => void;
+export type UseDevKeyboard = (options?: UseDevKeyboardOptions) => void;
+
+/** Options passed to the dev keyboard hook. */
+export type UseDevKeyboardOptions = {
+  clearConsole?: boolean;
+  cancelSave?: boolean;
+  cancelPrint?: boolean;
+  dispose$?: t.UntilInput;
+};
