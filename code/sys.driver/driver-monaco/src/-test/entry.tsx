e5840b505d161d6429d27424a5d413885799ed50
Phil Cockfield <phil@cockfield.net>
2025-05-06 19:12:14 +1200

    initialize with dev-harness

--- code/sys.driver/driver-monaco/src/-test/entry.tsx
+++ code/sys.driver/driver-monaco/src/-test/entry.tsx
@@ -1,7 +1,45 @@
-import { pkg } from '../common.ts';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import type { t } from '../common.ts';
+import { pkg } from '../pkg.ts';
 
 /**
  * Render UI.
  */
-globalThis.document.title = pkg.name;
-console.log('🐷 entry.tsx', pkg);
+const document = globalThis.document;
+if (document) {
+  document.title = pkg.name;
+  document.body.style.overflow = 'hidden'; // NB: suppress rubber-band effect.
+}
+
+/**
+ * Setup mounter:
+ */
+const Root = (props: { children?: t.ReactNode }) => {
+  return props.children;
+};
+
+/**
+ * MAIN entry.
+ */
+export async function main() {
+  const params = new URL(location.href).searchParams;
+  const isDev = params.has('dev') || params.has('d');
+  const root = createRoot(document.getElementById('root')!);
+
+  /**
+   * DevHarness:
+   */
+  const { render } = await import('@sys/ui-react-devharness');
+  const { Specs } = await import('./entry.Specs.ts');
+  const el = await render(pkg, Specs, { hrDepth: 3, keyboard: true, style: { Absolute: 0 } });
+
+  root.render(
+    <React.StrictMode>
+      <Root>{el}</Root>
+    </React.StrictMode>,
+  );
+}
+
+main().catch((err) => console.error(`Failed to render DevHarness`, err));
