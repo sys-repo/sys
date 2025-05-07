fd78af2805c39938bd02b718a30d62a7afd6c664
Phil Cockfield <phil@cockfield.net>
2025-05-08 11:23:17 +1200

    test

--- code/sys.ui/ui-react-components/src/ui/Media/-.test.ts
+++ code/sys.ui/ui-react-components/src/ui/Media/-.test.ts
@@ -3,7 +3,9 @@ import { Video } from '../Media.Video/mod.ts';
 import { Media } from './mod.ts';
 
 describe('Media', () => {
-  it('API', () => {
+  it('API', async () => {
+    const { Media: RootImport } = await import('@sys/ui-react-components');
+    expect(Media).to.equal(RootImport);
     expect(Media.Video).to.equal(Video);
   });
 });
