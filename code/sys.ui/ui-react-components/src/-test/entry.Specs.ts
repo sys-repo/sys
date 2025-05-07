71962d43f7450447e17470ed2ba635e5607f8d6b
Phil Cockfield <phil@cockfield.net>
2025-05-07 18:38:26 +1200

    entry

--- code/sys.ui/ui-react-components/src/-test/entry.Specs.ts
+++ code/sys.ui/ui-react-components/src/-test/entry.Specs.ts
@@ -22,6 +22,7 @@ export const SpecsComponents = {
   [`${ns}: Sheet`]: () => import('../ui/Sheet/-SPEC.tsx'),
   [`${ns}: Spinners.Bar`]: () => import('../ui/Spinners.Bar/-SPEC.tsx'),
   [`${ns}: FadeElement`]: () => import('../ui/FadeElement/-SPEC.tsx'),
+  [`${ns}: Media.Recorder`]: () => import('../ui/Media.Recorder/-spec/-SPEC.tsx'),
 
   [`${ns}: Player.Video`]: () => import('../ui/Player.Video/-SPEC.tsx'),
   [`${ns}: Player.Concept`]: () => import('../ui/Player.Concept/-SPEC.tsx'),
