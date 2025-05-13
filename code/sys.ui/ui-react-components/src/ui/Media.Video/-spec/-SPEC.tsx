607ac48731180191b2008ca5aa4e3229591a8198
Phil Cockfield <phil@cockfield.net>
2025-05-13 14:15:52 +1200

    Update -SPEC.tsx

--- code/sys.ui/ui-react-components/src/ui/Media.Video/-spec/-SPEC.tsx
+++ code/sys.ui/ui-react-components/src/ui/Media.Video/-spec/-SPEC.tsx
@@ -1,7 +1,7 @@
 import { Dev, Signal, Spec } from '../../-test.ui.ts';
+import { D } from '../common.ts';
 import { MediaVideo } from '../mod.ts';
 import { Debug, createDebugSignals } from './-SPEC.Debug.tsx';
-import { D } from '../common.ts';
 
 export default Spec.describe('Media.Video', (e) => {
   const debug = createDebugSignals();
@@ -25,17 +25,20 @@ export default Spec.describe('Media.Video', (e) => {
     });
 
     ctx.subject
+      .size([460, 259])
       .display('grid')
-      .render(() => (
-        <MediaVideo
-          debug={p.debug.value}
-          theme={p.theme.value}
-          fit={p.fit.value}
-          borderRadius={p.borderRadius.value}
-          aspectRatio={p.aspectRatio.value}
-          onReady={(e) => console.info(`⚡️ onReady:`, e)}
-        />
-      ));
+      .render(() => {
+        return (
+          <MediaVideo
+            debug={p.debug.value}
+            theme={p.theme.value}
+            fit={p.fit.value}
+            borderRadius={p.borderRadius.value}
+            aspectRatio={p.aspectRatio.value}
+            onReady={(e) => console.info(`⚡️ onReady:`, e)}
+          />
+        );
+      });
 
     /**
      * Initial state:
