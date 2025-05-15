a139a7693a1387aeba98853f4b1af11a3ef6bfa3
Phil Cockfield <phil@cockfield.net>
2025-05-15 16:43:23 +1200

    refactor

--- deploy/@tdb.fs/-scripts/-build.ts
+++ deploy/@tdb.fs/-scripts/-build.ts
@@ -1,9 +1,7 @@
-import { type t, pkg } from './common.ts';
-import { Cli, c } from '@sys/cli';
-import { Process } from '@sys/process';
-import { Fs, Path, Pkg } from '@sys/fs';
-import { DenoFile } from '@sys/driver-deno/runtime';
-import { Tmpl, create } from '@sys/tmpl/fs';
+import { Fs, Pkg } from '@sys/fs';
+import { Tmpl } from '@sys/tmpl/fs';
+import { buildAndCopy } from './-build.u.ts';
+import { pkg } from './common.ts';
 
 /**
  * Ensure dist.
@@ -11,39 +9,12 @@ import { Tmpl, create } from '@sys/tmpl/fs';
 await Fs.ensureDir('./dist');
 
 /**
- * Build project(s).
+ * Pull in modules.
  */
-async function buildAndCopy(moduleDir: t.StringDir, targetDir: t.StringRelativeDir) {
-  const path = Fs.resolve(moduleDir);
-  const silent = true;
-  const sh = Process.sh({ path, silent });
-
-  const denofile = (await DenoFile.load(path)).data;
-  if (!denofile) {
-    throw new Error(`Failed to load deno.json from: ${path}`);
-  }
-
-  const pkg: t.Pkg = { name: denofile.name ?? 'Unnamed', version: denofile.version ?? '0.0.0' };
-  let label = c.gray(`building: ${c.green(pkg.name)} → ${c.cyan(`/${targetDir}`)}`);
-  const spinner = Cli.spinner(label);
-  const res = await sh.run('deno task test && deno task build');
-  spinner.stop();
-
-  if (!res.success) {
-    console.info(res.text.stderr);
-    console.info();
-    console.error(`Failed while building: ${path}`);
-  }
-
-  /**
-   * Copy build to local /dist.
-   */
-  await Fs.copy(Path.join(path, 'dist'), Path.resolve('dist', targetDir));
-}
-
 console.info();
-await buildAndCopy('../../code/sys.ui/ui-react-components', 'sys/ui');
-await buildAndCopy('../../code/sys.driver/driver-monaco', 'sys/ui.driver.monaco');
+const build = true;
+await buildAndCopy('../../code/sys.ui/ui-react-components', 'sys/ui', { build });
+await buildAndCopy('../../code/sys.driver/driver-monaco', 'sys/ui.driver.monaco', { build });
 
 // Write entry HTML.
 const tmpl = Tmpl.create('src/-tmpl');
@@ -53,7 +24,7 @@ await tmpl.write('dist');
  * Calculate [PkgDist].
  */
 await Fs.remove('dist/dist.json');
-await Pkg.Dist.compute({ dir: 'dist', pkg, save: true });
+await Pkg.Dist.compute({ dir: 'dist', pkg, save: true, builder: pkg });
 
 // Finish.
 Deno.exit(0);
---
+++ deploy/@tdb.fs/-scripts/-build.u.ts
@@ -0,0 +1,50 @@
+import { Cli, c } from '@sys/cli';
+import { DenoFile } from '@sys/driver-deno/runtime';
+import { Fs, Path } from '@sys/fs';
+import { Process } from '@sys/process';
+import { type t } from './common.ts';
+
+/**
+ * Ensure dist.
+ */
+await Fs.ensureDir('./dist');
+
+/**
+ * Build project(s).
+ */
+export async function buildAndCopy(
+  moduleDir: t.StringDir,
+  targetDir: t.StringRelativeDir,
+  options: { build?: boolean } = {},
+) {
+  const path = Fs.resolve(moduleDir);
+  const silent = true;
+  const sh = Process.sh({ path, silent });
+
+  const denofile = (await DenoFile.load(path)).data;
+  if (!denofile) {
+    throw new Error(`Failed to load deno.json from: ${path}`);
+  }
+
+  /**
+   * Build
+   */
+  if (options.build ?? true) {
+    const pkg: t.Pkg = { name: denofile.name ?? 'Unnamed', version: denofile.version ?? '0.0.0' };
+    let label = c.gray(`building: ${c.green(pkg.name)} → ${c.cyan(`/${targetDir}`)}`);
+    const spinner = Cli.spinner(label);
+    const res = await sh.run('deno task test && deno task build');
+    spinner.stop();
+
+    if (!res.success) {
+      console.info(res.text.stderr);
+      console.info();
+      console.error(`Failed while building: ${path}`);
+    }
+  }
+
+  /**
+   * Copy build to local /dist.
+   */
+  await Fs.copy(Path.join(path, 'dist'), Path.resolve('dist', targetDir));
+}
