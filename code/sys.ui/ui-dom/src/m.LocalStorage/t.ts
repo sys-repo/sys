11ba2da8ee574d8e39ccfc8f4de9a5f2cb96c521
Phil Cockfield <phil@cockfield.net>
2025-05-16 14:50:25 +1200

    Update t.ts

--- code/sys.ui/ui-dom/src/m.LocalStorage/t.ts
+++ code/sys.ui/ui-dom/src/m.LocalStorage/t.ts
@@ -11,33 +11,21 @@ export type LocalStorageFactory = <T extends t.JsonMapU>(prefix: string) => Loca
  * A strongly typed wrapper around the browser's local-storage.
  */
 export type LocalStorage<T extends t.JsonMapU> = {
-  /**
-   * A prefix for keys in the local-storage.
-   */
-  prefix: string;
+  /** A prefix for keys in the local-storage. */
+  readonly prefix: string;
 
-  /**
-   * Retrieves a value from local-storage by key, or returns a default value if not present.
-   */
+  /** Retrieves a value from local-storage by key, or returns a default value if not present. */
   get<K extends keyof T>(key: K, defaultValue: T[K]): T[K];
 
-  /**
-   * Stores a value in the local-storage for the specified key.
-   */
+  /** Stores a value in the local-storage for the specified key. */
   put<K extends keyof T>(key: K, value: T[K]): T[K];
 
-  /**
-   * Deletes an entry from the local-storage by key.
-   */
+  /** Deletes an entry from the local-storage by key. */
   delete<K extends keyof T>(key: K): void;
 
-  /**
-   * Clears all entries from the local-storage.
-   */
+  /** Clears all entries from the local-storage. */
   clear(): void;
 
-  /**
-   * Returns an object representing the initial values in local-storage.
-   */
+  /** Returns an object representing the initial values in local-storage. */
   object(initial: T): T;
 };
