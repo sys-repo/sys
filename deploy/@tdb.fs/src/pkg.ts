e725be472e5d20dc9729232ff03d23addf430558
Phil Cockfield <phil@cockfield.net>
2025-05-16 18:34:18 +1200

    bump

--- deploy/@tdb.fs/src/pkg.ts
+++ deploy/@tdb.fs/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source-control.
  *    - Do NOT manually alter the file (as your work will be lost).
  */
-export const pkg: Pkg = { name: '@tdb/public-fs', version: '0.0.2' };
+export const pkg: Pkg = { name: '@tdb/public-fs', version: '0.0.3' };
