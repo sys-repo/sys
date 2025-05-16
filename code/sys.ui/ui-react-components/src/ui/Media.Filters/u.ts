9f481401afb45d32860f33115ac83bb45fa4452c
Phil Cockfield <phil@cockfield.net>
2025-05-16 12:19:07 +1200

    update call site

--- code/sys.ui/ui-react-components/src/ui/Media.Filters/u.ts
+++ code/sys.ui/ui-react-components/src/ui/Media.Filters/u.ts
@@ -1,4 +1,4 @@
-import { type t, D, Obj } from './common.ts';
+import { type t, D, Num, Obj } from './common.ts';
 
 export const values: t.MediaFiltersLib['values'] = (filters) => {
   const res: Partial<t.MediaFilterValueMap> = {};
@@ -9,16 +9,7 @@ export const values: t.MediaFiltersLib['values'] = (filters) => {
 export const toString: t.MediaFiltersLib['toString'] = (values = {}) => {
   return Obj.keys(D.config)
     .filter((name) => values[name] !== undefined)
-    .map((name) => ({ name, unit: D.config[name].unit, value: formatNumber(values[name]) }))
+    .map((name) => ({ name, unit: D.config[name].unit, value: Num.toString(values[name]) }))
     .map((e) => `${e.name}(${e.value}${e.unit})`)
     .join(' ');
 };
-
-/**
- * Helpers:
- */
-const formatNumber = (value: number = 0, maxDecimals = 2) =>
-  new Intl.NumberFormat(undefined, {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: maxDecimals,
-  }).format(value);
