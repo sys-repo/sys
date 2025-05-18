0caf989ede9441e93441dd03ac840660806eb798
Phil Cockfield <phil@cockfield.net>
2025-05-19 11:40:58 +1200

    stub in: @sample/deno-llm

---
+++ code/-sample/@sample.deno.llm/src/-test/entry.Specs.ts
@@ -0,0 +1,12 @@
+/**
+ * @module
+ * DevHarness visual specs.
+ */
+import type { t } from './common.ts';
+
+/**
+ * Specs:
+ */
+export const Specs = {
+  // 'namespace.component': () => import('../ui/component/-spec/-SPEC.tsx'),
+} as t.SpecImports;
