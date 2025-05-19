c3988ac65974458ed320185342ddfbb7fa641d0e
Phil Cockfield <phil@cockfield.net>
2025-05-19 13:21:07 +1200

    deps prep: add support for sub-paths in import definition

--- code/sys.driver/driver-deno/src/ns.Runtime/m.DenoDeps/u.toDep.ts
+++ code/sys.driver/driver-deno/src/ns.Runtime/m.DenoDeps/u.toDep.ts
@@ -2,9 +2,10 @@ import { type t, Esm } from './common.ts';
 
 export const toDep: t.DepsLib['toDep'] = (input, options = {}) => {
   const { dev, wildcard } = options;
+  const subpaths = wrangle.subpaths(options.subpaths);
   const target = wrangle.target(options.target).toSorted();
   const module = typeof input === 'string' ? Esm.parse(input) : Esm.parse(input.toString());
-  const res: t.Dep = { module, target, dev, wildcard };
+  const res: t.Dep = { module, target, dev, wildcard, subpaths };
   return res;
 };
 
@@ -16,5 +17,14 @@ const wrangle = {
     if (!input) return ['deno.json'];
     if (typeof input === 'string') return [input];
     return input;
+  },
+
+  subpaths(input?: t.StringDir[]): string[] | undefined {
+    if (!Array.isArray(input)) return;
+    const subpaths = input
+      .map((p) => String(p).trim())
+      .filter(Boolean)
+      .map((p) => p.replace(/^\/+/, '').replace(/\/+$/, ''));
+    return subpaths.length === 0 ? undefined : subpaths;
   },
 } as const;
