4ec5a0e1de50dae5154964c2effe0799cb110023
Phil Cockfield <phil@cockfield.net>
2025-05-22 10:27:05 +1200

    setup structure - and simple automerge init test

---
+++ code/sys.driver/driver-automerge/src/crdt.web/-.test.ts
@@ -0,0 +1,5 @@
+import { type t, describe, it, expect, Testing } from '../-test.ts';
+
+describe('CRDT: web (browser)', () => {
+  it.skip('🐷', async () => {});
+});
---
+++ code/sys.driver/driver-automerge/src/crdt/-.test.ts
@@ -0,0 +1,5 @@
+import { type t, describe, it, expect, Testing } from '../-test.ts';
+
+describe('CRDT: common (runtime platform agnostic)', () => {
+  describe('Automerge', () => {});
+});
