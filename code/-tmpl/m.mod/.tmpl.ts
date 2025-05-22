c282e2f4cb3b00807f4932a28ae7b0bd8623032e
Phil Cockfield <phil@cockfield.net>
2025-05-22 20:34:23 +1200

    refactor

--- code/-tmpl/m.mod/.tmpl.ts
+++ code/-tmpl/m.mod/.tmpl.ts
@@ -5,5 +5,5 @@ import type { t } from '../common.ts';
 
 export const dir = import.meta.dirname;
 export default async function setup(e: t.TmplWriteHandlerArgs) {
-  console.log('🐷 setup template:', dir);
+  console.log('🐷 m.mod: setup template:', dir);
 }
