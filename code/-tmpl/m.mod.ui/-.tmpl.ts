829aac2d7a12b8e26303db9ed0ef415d0307a840
Phil Cockfield <phil@cockfield.net>
2025-05-22 19:31:09 +1200

    tmpl: ui.mod → update "MyComponent" to specified name

--- code/-tmpl/m.mod.ui/-.tmpl.ts
+++ code/-tmpl/m.mod.ui/-.tmpl.ts
@@ -1,11 +1,20 @@
-import { type t, Fs } from '../common.ts';
+import { type t, Cli, Fs, Str } from '../common.ts';
 
 /**
  * Setup the template:
  */
 export const dir = import.meta.dirname;
 export default async function setup(e: t.TmplWriteHandlerArgs) {
-  console.log(`-------------------------------------------`);
-  console.log('setup', dir);
-  console.log('Fs', Fs);
+  const name = await Cli.Prompt.Input.prompt({ message: 'Component Name:' });
+
+  const dir = Fs.glob(e.dir.target.absolute);
+  const paths = await dir.find('**/*.{ts,tsx}', { includeDirs: false });
+
+  for (const { path } of paths) {
+    const text = (await Fs.readText(path)).data;
+    if (text) {
+      const res = Str.replaceAll(text, 'MyComponent', name);
+      if (res.changed) await Fs.write(path, res.after, { force: true });
+    }
+  }
 }
