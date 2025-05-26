147ee7b8537a18d8e81769dfc3691d2ea9600718
Phil Cockfield <phil@cockfield.net>
2025-05-26 13:07:48 +1200

    stub in sample

---
+++ code/-sample/@sample.mcp/.tmpl.ts
@@ -0,0 +1,9 @@
+/**
+ * Setup the template:
+ */
+import type { t } from '../common.ts';
+
+export const dir = import.meta.dirname;
+export default async function setup(e: t.TmplWriteHandlerArgs) {
+  console.log('🐷 pkg.deno: setup template:', dir);
+}
---
+++ code/-sample/@sample.mcp/src/common/t.ts
@@ -0,0 +1,21 @@
+/**
+ * @external
+ */
+export type { ReactElement, MouseEventHandler as ReactMouseEventHandler, ReactNode } from 'react';
+
+/**
+ * @system
+ */
+export type * from '@sys/types';
+
+export type { ColorTheme } from '@sys/color/t';
+export type { ExtractSignalValue, Signal } from '@sys/std/t';
+export type { SpecImports } from '@sys/testing/t';
+export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
+export type { KeyboardModifierFlags } from '@sys/ui-dom/t';
+export type { DevCtx } from '@sys/ui-react-devharness/t';
+
+/**
+ * @local
+ */
+export type * from '../types.ts';
