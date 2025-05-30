ebac9f66bcf2321a2b4454775e5c1663587fe4e3
Phil Cockfield <phil@cockfield.net>
2025-05-30 16:34:26 +1200

    Update -m.mod.ui.test.ts

--- code/-tmpl/-tests/-m.mod.ui.test.ts
+++ code/-tmpl/-tests/-m.mod.ui.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, Fs, it, Templates, Testing, Tmpl, tmplFilter } from '
 describe('Template: m.mod.ui', () => {
   const getDir = async () => Testing.dir('m.mod.ui').create();
 
-  it('setup: updates <root types> and <spec entry>', async () => {
+  it('setup: updates <component name>, <root types> and <spec entry>', async () => {
     const fs = await getDir();
     const def = {
       pkg: await Templates['pkg.deno'](),
@@ -16,30 +16,26 @@ describe('Template: m.mod.ui', () => {
 
     const path = 'src/ui/ui.FooBar';
     const name = 'MyFooBar';
-
     await tmpl.pkg.write(fs.dir);
     await tmpl.ui.write(fs.join(path), { onAfter: (e) => def.ui.default(e, { name }) });
 
     expect((await Fs.readText(fs.join('src/ui/ui.FooBar/mod.ts'))).data).to.include(name);
 
-    const ls = await fs.ls(true);
-    const paths = [
-      'src/ui/ui.FooBar/-spec/-SPEC.Debug.tsx',
-      'src/ui/ui.FooBar/-spec/-SPEC.tsx',
-      'src/ui/ui.FooBar/common.ts',
-      'src/ui/ui.FooBar/mod.ts',
-      'src/ui/ui.FooBar/t.ts',
-      'src/ui/ui.FooBar/ui.tsx',
-    ];
+    const glob = await Fs.glob(def.ui.dir!).find('**', { trimCwd: true, includeDirs: false });
+    const paths = glob
+      .map((m) => m.path.slice('src/m.mod.ui/'.length))
+      .map((p) => `${path}/${p}`)
+      .filter((p) => !p.endsWith('/.tmpl.ts'));
 
+    const ls = await fs.ls(true);
     for (const path of paths) {
-      expect(ls.includes(path)).to.eql(true);
+      expect(ls.includes(path)).to.eql(true, path);
     }
 
     const typesFile = (await Fs.readText(fs.join('src/types.ts'))).data!;
-    const specsFile = (await Fs.readText(fs.join('src/-test/entry.Specs.ts'))).data!;
     expect(typesFile).to.include(`export type * from './ui/ui.FooBar/t.ts';`);
 
+    const specsFile = (await Fs.readText(fs.join('src/-test/entry.Specs.ts'))).data!;
     expect(specsFile).to.include('[`${ns}: ui.FooBar`]:');
     expect(specsFile).to.include(`() => import('../ui/ui.FooBar/-spec/-SPEC.tsx'),`);
   });
