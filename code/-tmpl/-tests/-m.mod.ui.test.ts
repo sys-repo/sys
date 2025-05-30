4566e3687c27e82854be6c6f2d5df00aebb3d755
Phil Cockfield <phil@cockfield.net>
2025-05-30 13:31:41 +1200

    update root types

--- code/-tmpl/-tests/-m.mod.ui.test.ts
+++ code/-tmpl/-tests/-m.mod.ui.test.ts
@@ -1,10 +1,43 @@
-import { type t, describe, it, expect, Testing, Tmpl } from '../src/-test.ts';
+import { describe, expect, Fs, it, Templates, Testing, Tmpl, tmplFilter } from '../src/-test.ts';
 
 describe('Template: m.mod.ui', () => {
   const getDir = async () => Testing.dir('m.mod.ui').create();
 
-  it('setup', async () => {
+  it('setup: updates root types', async () => {
     const fs = await getDir();
-    // 🐷 WIP
+    const def = {
+      pkg: await Templates['pkg.deno'](),
+      ui: await Templates['m.mod.ui'](),
+    };
+    const tmpl = {
+      pkg: Tmpl.create(def.pkg.dir!).filter(tmplFilter),
+      ui: Tmpl.create(def.ui.dir!).filter(tmplFilter),
+    };
+
+    const path = 'src/ui/ui.FooBar';
+    const name = 'MyFooBar';
+
+    await tmpl.pkg.write(fs.dir);
+    await tmpl.ui.write(fs.join(path), { onAfter: (e) => def.ui.default(e, { name }) });
+
+    expect((await Fs.readText(fs.join('src/ui/ui.FooBar/mod.ts'))).data).to.include(name);
+    const ls = await fs.ls(true);
+    const paths = [
+      'src/ui/ui.FooBar/-spec/-SPEC.Debug.tsx',
+      'src/ui/ui.FooBar/-spec/-SPEC.tsx',
+      'src/ui/ui.FooBar/common.ts',
+      'src/ui/ui.FooBar/mod.ts',
+      'src/ui/ui.FooBar/t.ts',
+      'src/ui/ui.FooBar/ui.tsx',
+    ];
+
+    for (const path of paths) {
+      expect(ls.includes(path)).to.eql(true);
+    }
+
+    const typesFile = (await Fs.readText(fs.join('src/types.ts'))).data;
+    const specsFile = (await Fs.readText(fs.join('src/-test/entry.Specs.ts'))).data;
+    expect(typesFile).to.include(`export type * from './ui/ui.FooBar/t.ts';`);
+
   });
 });
--- code/-tmpl/src/m.mod.ui/.tmpl.ts
+++ code/-tmpl/src/m.mod.ui/.tmpl.ts
@@ -1,20 +1,38 @@
-import { type t, Cli, Fs, Str } from '../common.ts';
+import { Tmpl } from '@sys/tmpl/fs';
+import { type t, Cli, DenoFile, Fs, Str } from '../common.ts';
 
 /**
  * Setup the template:
  */
 export const dir = import.meta.dirname;
-export default async function setup(e: t.TmplWriteHandlerArgs) {
-  const name = await Cli.Prompt.Input.prompt({ message: 'Component Name:' });
+export default async function setup(e: t.TmplWriteHandlerArgs, options: { name?: string } = {}) {
+  const name = options.name ?? (await Cli.Prompt.Input.prompt({ message: 'Component Name:' }));
+  const dir = e.dir.target.absolute;
+  const denofile = await DenoFile.Path.nearest(dir);
+  const pkgDir = denofile ? Fs.dirname(denofile) : undefined;
 
-  const dir = Fs.glob(e.dir.target.absolute);
-  const paths = await dir.find('**/*.{ts,tsx}', { includeDirs: false });
-  for (const { path } of paths) {
-    const text = (await Fs.readText(path)).data;
-    if (text) {
-      const res = Str.replaceAll(text, 'MyComponent', name);
-      if (res.changed) await Fs.write(path, res.after, { force: true });
-    }
+  const glob = Fs.glob(dir);
+  const paths = (await glob.find('**/*.{ts,tsx}', { includeDirs: false })).map((m) => m.path);
+
+  // Update component name(s):
+  await Tmpl.File.update(paths, (line) => {
+    const res = Str.replaceAll(line.text, 'MyComponent', name);
+    if (res.changed) line.modify(res.after);
+  });
+
+  if (pkgDir) {
+    // Update types files:
+    const moduleDir = dir.slice((pkgDir + 'src/').length + 1);
+    const typesPath = Fs.join(pkgDir, 'src/types.ts');
+    const specsPath = Fs.join(pkgDir, 'src/-test/entry.Specs.ts');
+
+    await Tmpl.File.update(typesPath, (line) => {
+      if (line.is.last) {
+        const path = Fs.join(moduleDir, 't.ts');
+        const text = `export type * from './${path}';`;
+        line.modify(text);
+      }
+    });
   }
 
   /**
