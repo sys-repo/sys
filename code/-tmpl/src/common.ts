21d1a3fec1bddb7462af7312fea542a572202d5d
Phil Cockfield <phil@cockfield.net>
2025-06-01 11:23:55 +1200

    Update common.ts

--- code/-tmpl/src/common.ts
+++ code/-tmpl/src/common.ts
@@ -8,11 +8,16 @@ export { t };
 export * from './pkg.deno/src/common.ts';
 
 /**
- * UI Refs:
+ * Server refs:
  */
 export { c, Cli } from '@sys/cli';
 export { DenoFile } from '@sys/driver-deno/runtime';
 export { Fs, Path } from '@sys/fs';
+export { Tmpl } from '@sys/tmpl/fs';
+
+/**
+ * UI Refs:
+ */
 export { Color, css } from '@sys/ui-css';
 export { Signal } from '@sys/ui-react';
 export { Button, ObjectView } from '@sys/ui-react-components';
