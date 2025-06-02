2f256ea3575eafc05aec8e701adc189b6dd100f7
Phil Cockfield <phil@cockfield.net>
2025-06-02 18:25:03 +1200

    Create sw.js

---
+++ code/sys.ui/ui-react-components/public/sw.js
@@ -0,0 +1,8 @@
+/**
+ * 🐷
+ * Dev: Stub (Service Worker)
+ * - Build to see prod service working operating.
+ * - ref: `/src/worker.service/mod.ts`
+ */
+
+console.info('🐷 [dev-sw] dummy service worker in Vite (dev mode).');
