0e0590bcd9cb5ba4e0dec969a9fe625027e951b3
Phil Cockfield <phil@cockfield.net>
2025-06-03 12:26:05 +1200

    Update t.ts

--- code/sys.ui/ui-react-components/src/ui/Player/t.ts
+++ code/sys.ui/ui-react-components/src/ui/Player/t.ts
@@ -8,7 +8,7 @@ export type PlayerLib = {
   Video: {
     signals: t.PlayerSignalsFactory;
     View: React.FC<t.VideoPlayerProps>;
-    Element: React.FC<t.VideoElementProps
+    Element: React.FC<t.VideoElementProps>;
   };
   Timestamp: {
     Thumbnails: { View: React.FC<t.ThumbnailsProps> };
