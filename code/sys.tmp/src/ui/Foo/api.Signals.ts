65788c77cb7b45fc143c87e3bd59dec2162543f8
Phil Cockfield <phil@cockfield.net>
2025-06-05 12:40:42 +1200

    delete module

--- code/sys.tmp/src/ui/Foo/api.Signals.ts
+++
@@ -1,2 +0,0 @@
-import { Signal } from '@sys/ui-react';
-export const count = Signal.create(0);
--- code/sys.tmp/src/ui/mod.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @module
- * Sample UI modules and components.
- */
-export { Foo } from './Foo/mod.ts';
