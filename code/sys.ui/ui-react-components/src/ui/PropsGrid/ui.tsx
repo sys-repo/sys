8e7f5dca00e64f39934f76c140842d1e934ff729
Phil Cockfield <phil@cockfield.net>
2025-06-06 19:21:33 +1200

    sample

--- code/sys.ui/ui-react-components/src/ui/PropsGrid/ui.tsx
+++ code/sys.ui/ui-react-components/src/ui/PropsGrid/ui.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { type t, Color, css, D } from './common.ts';
 
+import { ObjectView } from '../ObjectView/mod.ts';
+
 export const PropsGrid: React.FC<t.PropsGridProps> = (props) => {
   const { debug = false } = props;
 
@@ -18,6 +20,7 @@ export const PropsGrid: React.FC<t.PropsGridProps> = (props) => {
   return (
     <div className={css(styles.base, props.style).class}>
       <div>{`🐷`}</div>
+      <ObjectView name={'data.rows'} data={props.data} expand={5} theme={theme.name} />
     </div>
   );
 };
