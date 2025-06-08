771f43e50297bb5b2b29a9192e4971b43fe0fd0d
Phil Cockfield <phil@cockfield.net>
2025-06-09 11:13:33 +1200

    Update libs.ts

--- code/-tmpl/src/pkg.deno/src/ui/common/libs.ts
+++ code/-tmpl/src/pkg.deno/src/ui/common/libs.ts
@@ -1,5 +1,6 @@
 export { Color, css, Style } from '@sys/ui-css';
 export { Keyboard } from '@sys/ui-dom';
 
+export { LocalStorage } from '@sys/ui-dom';
 export { useSizeObserver } from '@sys/ui-react';
 export { Button, ObjectView } from '@sys/ui-react-components';
