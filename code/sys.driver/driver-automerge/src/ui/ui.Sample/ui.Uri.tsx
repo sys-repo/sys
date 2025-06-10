9b9a81e131799143ae79d9d08f8ec120e53b37c4
Phil Cockfield <phil@cockfield.net>
2025-06-11 10:15:36 +1200

    refactor

---
+++ code/sys.driver/driver-automerge/src/ui/ui.Sample/ui.Uri.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { type t, css } from './common.ts';
+
+export type UriProps = {
+  text?: string;
+  style?: t.CssInput;
+};
+
+/**
+ * Component:
+ */
+export const Uri: React.FC<UriProps> = (props) => {
+  const text = wrangle.text(props.text);
+
+  /**
+   * Render:
+   */
+  const styles = {
+    base: css({
+      display: 'grid',
+      gridAutoFlow: 'column',
+      gridAutoColumns: 'auto',
+      columnGap: 3,
+    }),
+  };
+
+  if (!text) return null;
+
+  return (
+    <div className={css(styles.base, props.style).class}>
+      {text.parts.map((part, i, items) => {
+        const isFirst = i === 0;
+        const isLast = i === items.length - 1;
+        const opacity = isLast ? 1 : 0.3;
+        return (
+          <React.Fragment key={i}>
+            {!isFirst && <span>{':'}</span>}
+            <span style={{ opacity }}>{part}</span>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+/**
+ * Helpers:
+ */
+const wrangle = {
+  text(text: string = '') {
+    text = text.trim();
+    if (!text) return;
+    return { parts: text.split(':'), toString: () => text };
+  },
+} as const;
--- code/sys.driver/driver-automerge/src/ui/ui.Sample/ui.tsx
+++ code/sys.driver/driver-automerge/src/ui/ui.Sample/ui.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { type t, Color, css, ObjectView } from './common.ts';
+import { Uri } from './ui.Uri.tsx';
 
 export const Sample: React.FC<t.SampleProps> = (props) => {
   const { debug = false, repo } = props;
-  const peerId = wrangle.peerId(repo?.id.peer);
+  const peerId = repo?.id.peer;
 
   /**
    * Render:
@@ -11,31 +12,10 @@ export const Sample: React.FC<t.SampleProps> = (props) => {
   const theme = Color.theme(props.theme);
   const styles = {
     base: css({ position: 'relative', color: theme.fg, padding: 10 }),
-    peerId: css({
-      Absolute: [null, 10, -23, null],
-      fontSize: 11,
-      display: 'grid',
-      gridAutoFlow: 'column',
-      gridAutoColumns: 'auto',
-      columnGap: 3,
-    }),
+    peerId: css({ Absolute: [null, 10, -23, null], fontSize: 11 }),
   };
 
-  const elPeer = peerId && (
-    <div className={styles.peerId.class}>
-      {peerId.parts.map((part, i) => {
-        const isFirst = i === 0;
-        const isLast = i === peerId.parts.length - 1;
-        const opacity = isLast ? 1 : 0.3;
-        return (
-          <React.Fragment key={i}>
-            {!isFirst && <span>{':'}</span>}
-            <span style={{ opacity }}>{part}</span>
-          </React.Fragment>
-        );
-      })}
-    </div>
-  );
+  const elPeer = <Uri text={peerId} style={styles.peerId} />;
 
   return (
     <div className={css(styles.base, props.style).class}>
@@ -50,17 +30,3 @@ export const Sample: React.FC<t.SampleProps> = (props) => {
     </div>
   );
 };
-
-/**
- * Helpers:
- */
-const wrangle = {
-  peerId(text: string = '') {
-    text = text.trim();
-    if (!text) return;
-    return {
-      parts: text.split(':'),
-      toString: () => text,
-    };
-  },
-} as const;
