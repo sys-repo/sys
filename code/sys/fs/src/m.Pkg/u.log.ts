59409a064a716f16caec756198f04d8ab9ac7e57
Phil Cockfield <phil@cockfield.net>
2025-06-13 09:23:40 +1200

    cli output

--- code/sys/fs/src/m.Pkg/u.log.ts
+++ code/sys/fs/src/m.Pkg/u.log.ts
@@ -1,2 +1,7 @@
 import { type t, c } from './common.ts';
-export const toModuleString = (pkg: t.Pkg) => c.gray(`${c.white(pkg.name)}@${c.cyan(pkg.version)}`);
+
+export const toModuleString = (pkg: t.Pkg, hash?: string) => {
+  const version = `${c.white(pkg.name)}@${c.cyan(pkg.version)}`;
+  let hx = hash ? c.gray(c.dim(`.${hash.trim()}`)) : '';
+  return `${version}${hx}`;
+};
