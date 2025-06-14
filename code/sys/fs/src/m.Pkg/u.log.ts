4621bbcc1e7e95dad2958ed6bd8360b236b915f6
Phil Cockfield <phil@cockfield.net>
2025-06-15 10:13:27 +1200

    Update u.log.ts

--- code/sys/fs/src/m.Pkg/u.log.ts
+++ code/sys/fs/src/m.Pkg/u.log.ts
@@ -2,6 +2,6 @@ import { type t, c } from './common.ts';
 
 export const toModuleString = (pkg: t.Pkg, hash?: string) => {
   const version = `${c.white(pkg.name)}@${c.cyan(pkg.version)}`;
-  let hx = hash ? c.gray(c.dim(`.${hash.trim()}`)) : '';
-  return `${version}${hx}`;
+  let hx = hash ? c.dim(`.${hash.trim()}`) : '';
+  return c.gray(`${version}${hx}`);
 };
