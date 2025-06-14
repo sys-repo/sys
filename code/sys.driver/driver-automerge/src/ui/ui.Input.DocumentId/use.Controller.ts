72ac4e6efc6dee20e0e05b763809345049dad65d
Phil Cockfield <phil@cockfield.net>
2025-06-14 14:37:58 +1200

    controller logic

--- code/sys.driver/driver-automerge/src/ui/ui.Input.DocumentId/use.Controller.ts
+++ code/sys.driver/driver-automerge/src/ui/ui.Input.DocumentId/use.Controller.ts
@@ -1,45 +1,149 @@
-import { useRef } from 'react';
-import { type t, Is, slug } from './common.ts';
+import { useCallback, useRef } from 'react';
+import { type t, CrdtIs, Is, Signal, slug } from './common.ts';
 
-type P = t.DocumentIdInputProps;
-type H = t.DocumentIdHook;
+type Args = t.UseDocumentIdHookArgs;
+type Hook = t.DocumentIdHook;
+type P = t.DocumentIdHookSignals;
 
-export const useController: t.UseDocumentIdHook = (input = {}) => {
+/**
+ * Hook: controller (or passthough).
+ */
+export const useController: t.UseDocumentIdHook = (input: Hook | Args = {}) => {
+  return isHook(input)
+    ? input //                ← Controlled.
+    : useInternal(input); //  ← Uncontrolled.
+};
+
+/**
+ * Internal (always runs full hook list):
+ */
+function useInternal(args: Args = {}): Hook {
+  const instance = useRef(slug()).current;
+  const repo = args.repo;
+  const repoId = repo?.id.instance;
+
   /**
-   * Hooks:
+   * Refs:
    */
-  const instanceRef = useRef(slug());
-
-  // Exit if an existing hook intance was passed.
-  if (Is.record<H>(input) && Is.string(input.instance)) return input;
+  const signalsRef = useRef<t.DocumentIdHookSignals>({
+    id: args.signals?.id ?? Signal.create<string>(),
+    doc: args.signals?.doc ?? Signal.create<t.CrdtRef>(),
+    spinning: args.signals?.spinning ?? Signal.create(false),
+  });
 
   /**
-   * Setup:
+   * Effect: (hook into redraw listeners).
    */
-  const args = input as t.UseDocumentIdHookArgs;
-  const { repo } = args ?? {};
-
-  // console.log('repo', repo);
-  const action: t.DocumentIdInputAction = 'Create'; // TODO 🐷
+  Signal.useRedrawEffect(() => {
+    const p = signalsRef.current;
+    p.id.value;
+    p.doc.value;
+    p.spinning.value;
+  });
 
   /**
    * Handlers:
    */
-  const onActionClick: t.DocumentIdInputActionHandler = (e) => {
-  };
+  const invoke = useCallback(
+    async (action: t.DocumentIdInputAction) => {
+      if (!repo) return;
+      const p = signalsRef.current;
+      const props = wrangle.props(p, repo);
+      if (!props.is.enabled.action) return;
 
-  const onValueChange: t.TextInputChangeHandler = (e) => {
-  };
+      if (action === 'Load' && props.id) {
+        p.spinning.value = true;
+        p.doc.value = await repo.get(props.id);
+        p.spinning.value = false;
+      }
+
+      if (action === 'Create') {
+        const doc = repo.create(args.initial ?? {});
+        p.id.value = doc.id;
+        p.doc.value = doc;
+      }
+    },
+    [repoId],
+  );
+
+  const onTextChange = useCallback<t.TextInputChangeHandler>(
+    (e) => {
+      const p = signalsRef.current;
+      const doc = p.doc.value;
+      if (doc && doc.id !== e.value) p.doc.value = undefined;
+      p.id.value = e.value;
+    },
+    [repoId],
+  );
+
+  const onKeyDown = useCallback<t.TextInputKeyHandler>(
+    (e) => {
+      if (e.key === 'Enter') {
+        const props = wrangle.props(signalsRef.current, repo);
+        invoke(props.action);
+      }
+    },
+    [repoId],
+  );
+
+  const onActionClick = useCallback<t.DocumentIdInputActionHandler>(
+    (e) => invoke(e.action),
+    [repoId],
+  );
 
   /**
    * API:
    */
+  const signals = signalsRef.current;
   return {
-    instance: instanceRef.current,
-    action,
-    handlers: {
-      onActionClick,
-      onValueChange,
-    },
+    instance,
+    signals,
+    props: wrangle.props(signals, repo),
+    handlers: { onActionClick, onTextChange, onKeyDown },
   };
-};
+}
+
+/**
+ * Helpers:
+ */
+function isHook(input: unknown): input is Hook {
+  return !!input && Is.string((input as any).instance);
+}
+
+const wrangle = {
+  props(p: P, repo?: t.CrdtRepo): t.DocumentIdHookProps {
+    const id = wrangle.id(p);
+    const is = wrangle.is(p, repo);
+    return { id, is, action: wrangle.action(p) };
+  },
+
+  id(p: P) {
+    return (p.id.value ?? '').trim();
+  },
+
+  is(p: P, repo?: t.CrdtRepo): t.DocumentIdHookProps['is'] {
+    const id = wrangle.id(p);
+    const doc = p.doc.value;
+    const valid = CrdtIs.id(id);
+
+    let action = true;
+    if (!repo) action = false;
+    if (id && !valid) action = false;
+    if (id && doc?.id === id) action = false;
+
+    let input = true;
+    if (!repo) input = false;
+
+    return {
+      valid,
+      enabled: { action, input },
+      spinning: p.spinning.value,
+    };
+  },
+
+  action(p: P): t.DocumentIdInputAction {
+    const id = wrangle.id(p);
+    if (!id) return 'Create';
+    return 'Load';
+  },
+} as const;
