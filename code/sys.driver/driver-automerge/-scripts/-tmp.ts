574fca270a45683f34e5b52b391ff0ef43de4e3d
Phil Cockfield <phil@cockfield.net>
2025-06-15 20:17:45 +1200

    sample: loading synced data on mac filesystem

--- code/sys.driver/driver-automerge/-scripts/-tmp.ts
+++ code/sys.driver/driver-automerge/-scripts/-tmp.ts
@@ -1,2 +1,18 @@
+import { c, Cli } from '@sys/cli';
 import { Crdt } from '@sys/driver-automerge/fs';
-console.log('Crdt', Crdt);
+
+const path = '.tmp/sync.crdt';
+const repo = Crdt.repo(path);
+
+const id = '3YxRXmu8TcBs3SZmy43vRVNibqte';
+const doc = await repo.get(id);
+
+const table = Cli.table([]);
+table.push([c.gray('path:'), c.gray(path)]);
+table.push([c.gray('doc.id:'), c.green(id)]);
+table.push([c.gray('doc:')]);
+
+console.info('\n🌳');
+console.info(table.toString().trim());
+console.info(doc?.current);
+console.info();
