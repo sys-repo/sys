f5355663f4d2f6c80fe8157f0a9a798bcda90aa5
Phil Cockfield <phil@cockfield.net>
2025-06-15 14:35:49 +1200

    Update t.ts

--- code/sys.driver/driver-automerge/src/common/t.ts
+++ code/sys.driver/driver-automerge/src/common/t.ts
@@ -21,13 +21,14 @@ export type { ColorTheme } from '@sys/color/t';
 export type { ExtractSignalValue, Signal } from '@sys/std/t';
 export type { SpecImports } from '@sys/testing/t';
 export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
-export type { KeyboardModifierFlags } from '@sys/ui-dom/t';
+export type { KeyboardModifierFlags, LocalStorageImmutable } from '@sys/ui-dom/t';
 export type {
   TextInputChangeHandler,
   TextInputKeyHandler,
   TextInputProps,
 } from '@sys/ui-react-components/t';
 export type { DevCtx } from '@sys/ui-react-devharness/t';
+export type { PointerEventsHandler } from '@sys/ui-react/t';
 
 /**
  * @local
