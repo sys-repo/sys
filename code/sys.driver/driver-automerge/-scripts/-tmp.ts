acca7f87b78d0788deb56f28317063a8cf308995
Phil Cockfield <phil@cockfield.net>
2025-06-16 18:32:44 +1200

    refactor

--- code/sys.driver/driver-automerge/-scripts/-tmp.ts
+++ code/sys.driver/driver-automerge/-scripts/-tmp.ts
@@ -1,6 +1,21 @@
 import { c, Cli } from '@sys/cli';
 import { Crdt } from '@sys/driver-automerge/fs';
 
+const print = () => {
+  console.clear();
+
+  const table = Cli.table([]);
+  table.push([c.gray('  path:'), c.gray(dir)]);
+  table.push([c.gray('  doc.id:'), c.green(id)]);
+  table.push([c.gray('  doc:')]);
+
+  console.info('🌳');
+  console.info(table.toString().trim());
+  console.info();
+  console.info(doc?.current);
+  console.info();
+};
+
 /**
  * Pull document:
  */
@@ -10,25 +25,5 @@ const repo = Crdt.repo({ dir, network: { ws: 'sync.db.team' } });
 const id = '6EYQPyrnUw8BYR4vffMcotnrcAn';
 const doc = await repo.get(id);
 
-/**
- * Listen and print:
- */
-const print = (options: { clear?: boolean } = {}) => {
-  const { clear = true } = options;
-  if (clear) console.clear();
-
-  const table = Cli.table([]);
-  table.push([c.gray('  path:'), c.gray(dir)]);
-  table.push([c.gray('  doc.id:'), c.green(id)]);
-  table.push([c.gray('  doc:')]);
-
-  if (!clear) console.info();
-  console.info('🌳');
-  console.info(table.toString().trim());
-  console.info();
-  console.info(doc?.current);
-  console.info();
-};
-
-doc?.events().changed$.subscribe(() => print());
+doc?.events().changed$.subscribe(print);
 print();
