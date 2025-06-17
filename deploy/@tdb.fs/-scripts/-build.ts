58627fdc4b1627e139e4480f8230f29c544e6f59
Phil Cockfield <phil@cockfield.net>
2025-06-18 11:23:47 +1200

    new path

--- deploy/@tdb.fs/-scripts/-build.ts
+++ deploy/@tdb.fs/-scripts/-build.ts
@@ -10,7 +10,7 @@ console.info();
 const build = true;
 await buildAndCopy('../../code/sys.ui/ui-react-components', 'sys/ui', { build });
 await buildAndCopy('../../code/sys.driver/driver-monaco', 'sys/ui.driver.monaco', { build });
-await buildAndCopy('../../code/sys.driver/driver-automerge', 'sys/ui.driver.automerge', { build });
+await buildAndCopy('../../code/sys.driver/driver-automerge', 'sys/driver.automerge', { build });
 await copyPublic('public', 'dist');
 
 // Write entry HTML.
