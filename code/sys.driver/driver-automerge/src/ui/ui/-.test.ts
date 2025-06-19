88a4f8ba20a020fc07b599d44cad2890466b4d8c
Phil Cockfield <phil@cockfield.net>
2025-06-20 11:23:16 +1200

    Update -.test.ts

--- code/sys.driver/driver-automerge/src/ui/ui/-.test.ts
+++ code/sys.driver/driver-automerge/src/ui/ui/-.test.ts
@@ -1,9 +1,14 @@
 import { describe, expect, it } from '../../-test.ts';
-import { DocumentId } from '../ui.DocumentId/mod.ts';
 import { Crdt } from './mod.ts';
+
+import { Card } from '../ui.Card/mod.ts';
+import { DocumentId } from '../ui.DocumentId/mod.ts';
+import { TextEditor } from '../ui.TextEditor/mod.ts';
 
 describe('Input UI', () => {
   it('API', () => {
+    expect(Crdt.UI.Card).to.equal(Card);
     expect(Crdt.UI.DocumentId).to.equal(DocumentId);
+    expect(Crdt.UI.TextEditor).to.equal(TextEditor);
   });
 });
