b5b2a0c40dcb157d0950d2b6e4a0b2bc5f14b466
Phil Cockfield <phil@cockfield.net>
2025-06-23 20:02:02 +1200

    refactor

--- code/sys.driver/driver-automerge/src/ui/m.Crdt/use.RedrawEffect.ts
+++ code/sys.driver/driver-automerge/src/ui/m.Crdt/use.RedrawEffect.ts
@@ -5,22 +5,26 @@ import { type t } from './common.ts';
  * Triggers redraw on Doc<T> changes.
  */
 export const useRedrawEffect: t.UseRedrawEffect = (doc, onRedraw) => {
-  const [, setRender] = React.useState(0);
-  const redraw = () => {
-    setRender((n) => n + 1);
-    if (doc) onRedraw?.(doc);
-  };
+  const [count, setRender] = React.useState(0);
+  const redraw = () => setRender((n) => n + 1);
 
+  /**
+   * Effect:
+   */
   React.useEffect(() => {
     if (!doc) return;
-    const events = doc.events();
 
-
+    const events = doc?.events();
     events.$.subscribe((e) => {
+      if (doc) onRedraw?.(doc);
       redraw();
     });
+
     return events.dispose;
   }, [doc?.id]);
 
-  return { doc };
+  /**
+   * API:
+   */
+  return { count, doc };
 };
