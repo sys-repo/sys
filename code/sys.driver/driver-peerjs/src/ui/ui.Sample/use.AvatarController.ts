229d66d9e72d9f668f43b8e4b511458f3544ebac
Phil Cockfield <phil@cockfield.net>
2025-06-28 14:57:10 +1200

    tidy

--- code/sys.driver/driver-peerjs/src/ui/ui.Sample/use.AvatarController.ts
+++ code/sys.driver/driver-peerjs/src/ui/ui.Sample/use.AvatarController.ts
@@ -28,9 +28,10 @@ export function useAvatarController(args: {
   const onReady: t.MediaVideoStreamReadyHandler = (e) => {
     setStream(e.stream.raw);
 
-    console.info(`⚡️ [${name}].MediaStream.onReady:`, e);
-    Media.Log.tracks(' • stream.raw:', e.stream.raw);
-    Media.Log.tracks(' • stream.filtered:', e.stream.filtered);
+    const prefix = `⚡️ [${name}].MediaStream`;
+    console.info(`${prefix}.onReady:`, e);
+    Media.Log.tracks(`${prefix}.raw:`, e.stream.raw);
+    Media.Log.tracks(`${prefix}.filtered:`, e.stream.filtered);
   };
 
   const onSelect: t.AvatarSelectHandler = (e) => {
