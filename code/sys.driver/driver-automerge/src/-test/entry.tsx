5d9ad1e72c4063bec08cd51765243eb378edf792
Phil Cockfield <phil@cockfield.net>
2025-06-28 15:07:25 +1200

    automerge - stub in service worker

--- code/sys.driver/driver-automerge/src/-test/entry.tsx
+++ code/sys.driver/driver-automerge/src/-test/entry.tsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { pkg } from '../pkg.ts';
 
 /**
+ * Service Worker:
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const devmode = import.meta.env.DEV;
+    const prefix = devmode ? `[main:dev]` : `[main]`;
+    const title = devmode ? 'ServiceWorker-Sample' : 'ServiceWorker';
+    navigator.serviceWorker
+      .register('sw.js', { type: 'module' })
+      .then((reg) => console.info(`🌳 ${prefix} ${title} registered with scope: ${reg.scope}`))
+      .catch((err) => console.error(`💥 ${prefix} ${title} registration failed:`, err));
+  });
+}
+
+/**
  * Render UI:
  */
 console.info('🐷 ./entry.tsx → Pkg:💦', pkg);
@@ -12,9 +27,6 @@ if (document) {
   document.body.style.overflow = 'hidden'; // NB: suppress rubber-band effect.
 }
 
-/**
- * MAIN entry:
- */
 export async function main() {
   const params = new URL(location.href).searchParams;
   const isDev = params.has('dev') || params.has('d');
