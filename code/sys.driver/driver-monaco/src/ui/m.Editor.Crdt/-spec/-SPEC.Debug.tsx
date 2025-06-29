eb3481d65fc8d845dbf46172498fdf691689f1d7
Phil Cockfield <phil@cockfield.net>
2025-06-30 10:26:41 +1200

    docs

--- code/sys.driver/driver-monaco/src/ui/m.Editor.Crdt/-spec/-SPEC.Debug.tsx
+++ code/sys.driver/driver-monaco/src/ui/m.Editor.Crdt/-spec/-SPEC.Debug.tsx
@@ -93,7 +93,7 @@ export const Debug: React.FC<DebugProps> = (props) => {
     <div className={css(styles.base, props.style).class}>
       <div className={Styles.title.class}>
         <div>{D.name}</div>
-        <div>{'Binding'}</div>
+        <div>{'Data Binding'}</div>
       </div>
 
       <Button
