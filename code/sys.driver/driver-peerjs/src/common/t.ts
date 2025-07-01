6764619ba857e11b32bd6e99892e3bfc095362b0
Phil Cockfield <phil@cockfield.net>
2025-07-01 12:02:52 +1200

    tidy

--- code/sys.driver/driver-peerjs/src/common/t.ts
+++ code/sys.driver/driver-peerjs/src/common/t.ts
@@ -25,10 +25,9 @@ export type { Crdt, DocumentIdProps } from '@sys/driver-automerge/t';
 /**
  * PeerJS (WebRTC):
  */
-import type { MediaConnection as _MediaConnection, Peer as _Peer } from 'peerjs';
 export namespace PeerJS {
-  export type Peer = _Peer;
-  export type MediaConnection = _MediaConnection;
+  export type Peer = import('peerjs').Peer;
+  export type MediaConnection = import('peerjs').MediaConnection;
 }
 
 /**
