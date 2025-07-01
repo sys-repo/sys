3086ef8a350ffb8684da609bb3ff479490718383
Phil Cockfield <phil@cockfield.net>
2025-07-01 15:44:39 +1200

    clean up redraw - and allow paths to be filtered on

--- code/sys.driver/driver-automerge/src/ui.Crdt/t.ts
+++ code/sys.driver/driver-automerge/src/ui.Crdt/t.ts
@@ -23,9 +23,19 @@ export type CrdtUiLib = t.CrdtBrowserLib & {
 /** Hook that causes a redraw on doc changes. */
 export type UseRedrawEffect = <T extends O = O>(
   doc?: t.CrdtRef<T>,
-  onRedraw?: (e: CrdtRedrawEvent<T>) => void,
+  options?:
+    | t.UseRedrawEffectOptions<T>
+    | t.CrdtRedrawEventHandler<T>
+    | (t.ObjectPath | t.ObjectPath[]),
 ) => CrdtRedrawHook;
 
+export type UseRedrawEffectOptions<T extends O> = {
+  onRedraw?: t.CrdtRedrawEventHandler<T>;
+  path?: t.ObjectPath | t.ObjectPath[];
+};
+
+/** Callback for when a redraw is triggered. */
+export type CrdtRedrawEventHandler<T extends O> = (e: CrdtRedrawEvent<T>) => void;
 /** Event fired when the redraw hook redraws. */
 export type CrdtRedrawEvent<T extends O> = {
   readonly doc: t.CrdtRef<T>;
--- code/sys.driver/driver-automerge/src/ui.Crdt/use.RedrawEffect.ts
+++ code/sys.driver/driver-automerge/src/ui.Crdt/use.RedrawEffect.ts
@@ -1,5 +1,5 @@
-import React from 'react';
-import { type t } from './common.ts';
+import { useEffect, useState } from 'react';
+import { type t, Is } from './common.ts';
 
 type O = Record<string, unknown>;
 
@@ -8,31 +8,61 @@ type O = Record<string, unknown>;
  */
 export function useRedrawEffect<T extends O = O>(
   doc?: t.CrdtRef<T>,
-  onRedraw?: (e: t.CrdtRedrawEvent<T>) => void,
+  opt?: t.UseRedrawEffectOptions<T> | t.CrdtRedrawEventHandler<T> | t.ObjectPath | t.ObjectPath[],
 ): t.CrdtRedrawHook {
+  const options = wrangle.options<T>(opt);
+  const { onRedraw } = options;
+  const path = wrangle.path(options.path) ?? [];
+
   /**
    * Hooks:
    */
-  const [count, setRender] = React.useState(0);
+  const [count, setRender] = useState(0);
   const redraw = () => setRender((n) => n + 1);
 
   /**
    * Effect:
    */
-  React.useEffect(() => {
+  useEffect(() => {
     if (!doc) return;
 
     const events = doc?.events();
-    events.$.subscribe((change) => {
+    const $ = path.length === 0 ? events.$ : events.path(path).$;
+
+    $.subscribe((change) => {
       onRedraw?.({ doc, change });
       redraw();
     });
 
     return events.dispose;
-  }, [doc?.id]);
+  }, [doc?.id, path.join()]);
 
   /**
    * API:
    */
   return { count, doc };
 }
+
+/**
+ * Helpers:
+ */
+const wrangle = {
+  options<T extends O>(
+    input?:
+      | t.UseRedrawEffectOptions<T>
+      | t.CrdtRedrawEventHandler<T>
+      | t.ObjectPath
+      | t.ObjectPath[],
+  ): t.UseRedrawEffectOptions<T> {
+    if (!input) return {};
+    if (Is.func(input)) return { onRedraw: input };
+    if (Array.isArray(input)) return { path: input };
+    return input;
+  },
+
+  path(input: t.ObjectPath | t.ObjectPath[] = []): t.ObjectPath[] {
+    const done = (res: t.ObjectPath[]) => res.filter((a) => a.length > 0);
+    if (Array.isArray(input[0])) return done(input as t.ObjectPath[]);
+    return done([input as t.ObjectPath]);
+  },
+} as const;
