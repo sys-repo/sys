eb10f53d98324c8719a77289461193662be058bc
Phil Cockfield <phil@cockfield.net>
2025-07-01 14:52:48 +1200

    WIP: syncing on changes to YAML/Code-Editor

--- deploy/@tdb.slc/src/ui/ui.Canvas.Project/-spec/-SPEC.Debug.tsx
+++ deploy/@tdb.slc/src/ui/ui.Canvas.Project/-spec/-SPEC.Debug.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { parse } from 'yaml';
 import {
   type t,
-  Obj,
+  Is,
+  Arr,
   Button,
   Crdt,
   css,
   D,
-  CanvasPanel,
   LocalStorage,
+  Obj,
   ObjectView,
+  rx,
   Signal,
 } from '../common.ts';
 import { EditorPanel } from './-ui.EditorPanel.tsx';
@@ -16,6 +19,12 @@ import { EditorPanel } from './-ui.EditorPanel.tsx';
 type Doc = { count: number };
 type P = t.CanvasProjectProps;
 type Storage = Pick<P, 'theme' | 'debug'> & { showEditorPanel?: boolean; showCanvas?: boolean };
+
+export const PATHS = {
+  DEV: ['.dev', D.displayName],
+  YAML: ['project', 'config', 'yaml'],
+  YAML_PARSED: ['project', 'config', 'yaml.parsed'],
+};
 
 /**
  * Types:
@@ -35,7 +44,7 @@ export function createDebugSignals() {
     showEditorPanel: true,
     showCanvas: false,
   };
-  const store = LocalStorage.immutable<Storage>(`dev:${D.name}`, defaults);
+  const store = LocalStorage.immutable<Storage>(`dev:${D.displayName}`, defaults);
   const snap = store.current;
 
   const repo = Crdt.repo({
@@ -44,11 +53,13 @@ export function createDebugSignals() {
   });
 
   const props = {
+    redraw: s(0),
     debug: s(snap.debug),
     theme: s(snap.theme),
     showEditorPanel: s(snap.showEditorPanel),
     showCanvas: s(snap.showCanvas),
     doc: s<t.Crdt.Ref<Doc>>(),
+    video: s<t.SampleVideo>(),
   };
   const p = props;
   const api = {
@@ -62,12 +73,64 @@ export function createDebugSignals() {
   };
 
   Signal.effect(() => {
-    store.change((d) => {
+    const change = (d: Storage) => {
       d.theme = p.theme.value;
       d.debug = p.debug.value;
-      d.showEditorPanel = p.showEditorPanel.value;
       d.showCanvas = p.showCanvas.value;
+      d.showEditorPanel = p.showEditorPanel.value;
+    };
+
+    store.change(change);
+    p.doc.value?.change((d) => {
+      const ns = Obj.Path.Mutate.ensure<Storage>(d, PATHS.DEV, {});
+      change(ns);
     });
+  });
+
+
+  /**
+   * Sync: DevHarness
+   */
+  let _events: t.Crdt.Events<Doc> | undefined;
+  Signal.effect(() => {
+    const doc = p.doc.value;
+    _events?.dispose();
+    _events = doc?.events();
+
+    });
+
+    _events
+      ?.path(PATHS.YAML)
+      .$.pipe(
+        rx.debounceTime(300),
+        rx.map((e) => Obj.Path.get<string>(doc?.current, PATHS.YAML) ?? ''),
+      )
+      .subscribe((text) => {
+        let obj = {} as any;
+        try {
+          obj = parse(text);
+        } catch (error: any) {
+          obj = {};
+        }
+        doc?.change((d) => Obj.Path.mutate(d, PATHS.YAML_PARSED, obj));
+      });
+
+    _events
+      ?.path(PATHS.YAML_PARSED)
+      .$.pipe(
+        rx.map((e) => Obj.Path.get(doc?.current, PATHS.YAML_PARSED)),
+        rx.distinctWhile((p, n) => Obj.eql(p, n)),
+        rx.debounceTime(300),
+      )
+      .subscribe((e) => {
+        const obj = Obj.Path.get(doc?.current, PATHS.YAML_PARSED) as any;
+        if (Is.record(obj.video)) {
+          const video = obj.video as t.SampleVideo;
+          if (!Obj.eql(p.video.value, video)) p.video.value = video;
+        } else {
+          p.video.value = undefined;
+        }
+      });
   });
 
   return api;
@@ -109,6 +172,7 @@ export const Debug: React.FC<DebugProps> = (props) => {
         onClick={() => Signal.cycle<t.CommonTheme>(p.theme, ['Light', 'Dark'])}
       />
 
+      <hr />
       <Button
         block
         label={() => `show editor: ${p.showEditorPanel.value}`}
@@ -123,7 +187,7 @@ export const Debug: React.FC<DebugProps> = (props) => {
       <hr />
       <Button
         block
-        label={() => `count: increment`}
+        label={() => `count: increment (${p.doc.value?.current.count})`}
         onClick={() => {
           const doc = p.doc.value;
           doc?.change((d) => d.count++);
@@ -155,22 +219,8 @@ export const Debug: React.FC<DebugProps> = (props) => {
           const doc = p.doc.value;
           if (doc) {
             doc.change((d) => {
-              const path = ['project', 'panels'];
-              Obj.Path.Mutate.ensure(d, path, {});
-
-              const o = d as any;
-              const panels = Obj.Path.get<any>(d, path)!;
-
-              CanvasPanel.all.forEach((panel) => {
-                const from = o.project[panel];
-
-                if (from) {
-                  const text = from.text;
-                  panels[panel] = { text };
-                }
-
-                delete o.project[panel];
-              });
+              // const path = ['project', 'config'];
+              // const text = Obj.Path.get(d, [...path, 'code']);
             });
           }
         }}
