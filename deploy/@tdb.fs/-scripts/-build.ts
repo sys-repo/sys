828f9384fe8305891aa41f6dde34cf58a2ea65f6
Phil Cockfield <phil@cockfield.net>
2025-07-06 11:57:57 +1200

    add prosemirror to fs deployment bundle

--- deploy/@tdb.fs/-scripts/-build.ts
+++ deploy/@tdb.fs/-scripts/-build.ts
@@ -11,9 +11,10 @@ const build = true;
 
 await buildAndCopyAll([
   ['../../code/sys.ui/ui-react-components', 'sys/ui', { build }],
-  ['../../code/sys.driver/driver-monaco', 'sys/driver.monaco', { build }],
   ['../../code/sys.driver/driver-automerge', 'sys/driver.automerge', { build }],
+  ['../../code/sys.driver/driver-monaco', 'sys/driver.monaco', { build }],
   ['../../code/sys.driver/driver-peerjs', 'sys/driver.peerjs', { build }],
+  ['../../code/sys.driver/driver-prosemirror', 'sys/driver.prosemirror', { build }],
 ]);
 await copyPublic('public', 'dist');
 
