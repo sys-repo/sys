55568acb18679f3d92eaadf980a55d8b13449d45
Phil Cockfield <phil@cockfield.net>
2025-07-06 11:48:53 +1200

    fix test

--- code/sys.driver/driver-prosemirror/src/ui/ui.TextEditor/.test.ts
+++ code/sys.driver/driver-prosemirror/src/ui/ui.TextEditor/.test.ts
@@ -3,7 +3,7 @@ import { TextEditor } from './mod.ts';
 
 describe('UI: Rich TextEditor', () => {
   it('API import', async () => {
-    const m = await import('@sys/driver-automerge/ui');
+    const m = await import('@sys/driver-prosemirror');
     expect(m.TextEditor).to.equal(TextEditor);
   });
 });
