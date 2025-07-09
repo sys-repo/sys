973245ba297deeb2c9c3d460886259be6466c263
Phil Cockfield <phil@cockfield.net>
2025-07-09 14:15:49 +1200

    Update libs.ts

--- code/sys.driver/driver-peerjs/src/ui/common/libs.ts
+++ code/sys.driver/driver-peerjs/src/ui/common/libs.ts
@@ -5,4 +5,4 @@ export { Motion as M, Motion };
 export { Color, css, Style } from '@sys/ui-css';
 export { Kbd, LocalStorage } from '@sys/ui-dom';
 export { usePointer, useSizeObserver } from '@sys/ui-react';
-export { Button, Buttons, Cropmarks, ObjectView } from '@sys/ui-react-components';
+export { Bullet, Button, Buttons, Cropmarks, ObjectView } from '@sys/ui-react-components';
