6bd6fa213bbb351a359bd9524ee11f8b53025f87
Phil Cockfield <phil@cockfield.net>
2025-07-27 20:18:15 +1200

    update call-sites

--- code/sys.driver/driver-monaco/src/ui/-sample.schema/u.factory.update.ts
+++ code/sys.driver/driver-monaco/src/ui/-sample.schema/u.factory.update.ts
@@ -1,17 +1,6 @@
 import { SampleSchema } from './-schemas/mod.ts';
 import { type t, Obj, Schema, Signal } from './common.ts';
 
-import { AssertError } from '@sinclair/typebox/value';
-const attempt = <T>(fn: () => T | undefined) => {
-  try {
-    return { ok: true, value: fn() };
-  } catch (err) {
-    if (err instanceof AssertError) return { ok: false, error: err.error };
-    console.warn('unhandled error:', err);
-    return { ok: false };
-  }
-};
-
 /**
  * When the document has changed:
  *   1. Parse the meta-data to determine the main <View> to display.
@@ -30,16 +19,17 @@ export function factoryUpdate(signals: t.SampleSignals) {
    * 1. Read meta from YAML:
    */
   const _meta = Obj.Path.get(root, path.meta, {});
-  const meta = attempt(() => Schema.Value.Parse(SampleSchema.Meta, _meta));
+  const meta = Schema.try(() => Schema.Value.Parse(SampleSchema.Meta, _meta));
 
   /**
    * 2. Read /main UI props and factory lookup-id from YAML:
    */
   const main = meta.value?.main;
-  signals.main.value = undefined;
+  signals.main.value = undefined; // ← (reset).
   if (main?.props && main.component) {
     const p = Obj.Path.curry(main.props.split('/'));
-    const video = attempt(() => Schema.Value.Parse(SampleSchema.Video, p.get(root)));
+    const video = Schema.try(() => Schema.Value.Parse(SampleSchema.Video, p.get(root)));
+
     if (video.value) {
       const component = main.component;
       const props = video.value;
