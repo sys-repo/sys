2fad0d10f2c117171d7f342c14f45a52deba7ac9
Phil Cockfield <phil@cockfield.net>
2025-07-31 13:25:30 +1200

    Update libs.ts

--- code/sys.driver/driver-monaco/src/common/libs.ts
+++ code/sys.driver/driver-monaco/src/common/libs.ts
@@ -1,4 +1,6 @@
-export { Arr, Err, Immutable, Is, Obj, Pkg, R, rx, Str, Time, Url } from '@sys/std';
 export { Schema, Type } from '@sys/schema';
+export { Arr, Err, Immutable, Is, Obj, Pkg, R, rx, Str, Time, Url } from '@sys/std';
 export { Yaml } from '@sys/std/yaml';
 export { Signal } from '@sys/ui-react';
+
+export { A, Crdt } from '@sys/driver-automerge/ui';
