0e7adb4f15599668e547275aa7d8280f90ca8400
Phil Cockfield <phil@cockfield.net>
2025-08-01 12:59:23 +1200

    Update libs.ts

--- code/sys.driver/driver-monaco/src/ui/common/libs.ts
+++ code/sys.driver/driver-monaco/src/ui/common/libs.ts
@@ -1,11 +1,12 @@
 export { Color, css, Style } from '@sys/ui-css';
 export { Keyboard, LocalStorage } from '@sys/ui-dom';
+export { UserAgent } from '@sys/ui-dom/user-agent';
 export { useSizeObserver } from '@sys/ui-react';
 export {
   Bullet,
   Button,
+  Cropmarks,
   ObjectView,
   PathView,
   Spinners,
-  Cropmarks,
 } from '@sys/ui-react-components';
