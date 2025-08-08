ec298f09c629c0b7e9837171ae75e7018ec1c3f4
Phil Cockfield <phil@cockfield.net>
2025-08-09 10:02:28 +1200

    rename folder

--- code/sys.driver/driver-monaco/src/-test/-specs.ts
+++ code/sys.driver/driver-monaco/src/-test/-specs.ts
@@ -11,6 +11,6 @@ export const ns = 'driver.monaco';
 export const Specs = {
   [`${ns}: ui.MonacoEditor`]: () => import('../ui/ui.Editor.Monaco/-spec/-SPEC.tsx'),
   [`${ns}: ui.MonacoEditor ← (CRDT binding)`]: () => import('../ui/m.Crdt/-spec/-SPEC.tsx'),
-  [`${ns}: ui.YamlEditor`]: () => import('../ui/ui.Editor.Yaml/-spec.editor/-SPEC.tsx'),
+  [`${ns}: ui.YamlEditor`]: () => import('../ui/ui.Editor.Yaml/-spec/-SPEC.tsx'),
   [`${ns}: Sample`]: () => import('../ui/-sample/-spec/-SPEC.tsx'),
 } as t.SpecImports;
