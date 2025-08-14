3a21eed598f215033a27ae500477538dc1227aa7
Phil Cockfield <phil@cockfield.net>
2025-08-15 09:50:12 +1200

    tidy

---
+++ code/sys.driver/driver-monaco/src/ui/m.Crdt/t.binding.ts
@@ -0,0 +1,53 @@
+import type { t } from './common.ts';
+
+/**
+ * Method: setup binding.
+ */
+export type EditorCrdtBind = (
+  editor: t.Monaco.Editor,
+  doc: t.Crdt.Ref,
+  path: t.ObjectPath,
+  until?: t.UntilInput,
+) => Promise<t.EditorCrdtBinding>;
+
+/**
+ * A live binding between a Monaco code-editor and
+ * an immutable CRDT document interface.
+ */
+export type EditorCrdtBinding = t.Lifecycle & {
+  readonly doc: t.Crdt.Ref;
+  readonly path: t.ObjectPath;
+  readonly model: t.Monaco.TextModel;
+  readonly $: t.Observable<t.EditorCrdtLocalChange>;
+};
+
+/**
+ * Hook: to setup and tear-down a Monaco-Crdt two-way data binding.
+ */
+export type UseEditorCrdtBinding = (
+  args: t.UseEditorCrdtBindingArgs,
+  onReady?: (e: { binding: t.EditorCrdtBinding; dispose$: t.Observable<void> }) => void,
+) => EditorCrdtBindingHook | undefined;
+
+/** Arguments passed to the CRDT `useBinding` hook. */
+export type UseEditorCrdtBindingArgs = {
+  editor?: t.Monaco.Editor;
+  doc?: t.Crdt.Ref;
+  path?: t.ObjectPath;
+  /** Sync CRDT fold marks with Monaco. (default = off) */
+  foldMarks?: boolean;
+};
+
+/** An instance of the `useBinding` Monaco-Crdt two-way data binding. */
+export type EditorCrdtBindingHook = Omit<t.EditorCrdtBinding, 'dispose'>;
+
+/**
+ * Events:
+ */
+
+/** Local CRDT editor change event. */
+export type EditorCrdtLocalChange = {
+  readonly trigger: 'editor' | 'crdt';
+  readonly path: t.ObjectPath;
+  readonly change: { readonly before: string; readonly after: string };
+};
--- code/sys.driver/driver-monaco/src/ui/m.Crdt/t.ts
+++ code/sys.driver/driver-monaco/src/ui/m.Crdt/t.ts
@@ -1,57 +1,11 @@
 import type { t } from './common.ts';
+export type * from './t.binding.ts';
 
 /**
  * Tools for binding between a Monaco editor and
  * an immutable CRDT document interface.
  */
 export type EditorCrdtLib = Readonly<{
-  bind(
-    editor: t.Monaco.Editor,
-    doc: t.Crdt.Ref,
-    path: t.ObjectPath,
-    until?: t.UntilInput,
-  ): Promise<t.EditorCrdtBinding>;
+  bind: t.EditorCrdtBind;
   useBinding: t.UseEditorCrdtBinding;
 }>;
-
-/**
- * A live binding between a Monaco code-editor and
- * an immutable CRDT document interface.
- */
-export type EditorCrdtBinding = t.Lifecycle & {
-  readonly doc: t.Crdt.Ref;
-  readonly path: t.ObjectPath;
-  readonly model: t.Monaco.TextModel;
-  readonly $: t.Observable<t.EditorCrdtLocalChange>;
-};
-
-/**
- * Hook: to setup and tear-down a Monaco-Crdt two-way data binding.
- */
-export type UseEditorCrdtBinding = (
-  args: t.UseEditorCrdtBindingArgs,
-  onReady?: (e: { binding: t.EditorCrdtBinding; dispose$: t.Observable<void> }) => void,
-) => EditorCrdtBindingHook | undefined;
-
-/** Arguments passed to the CRDT `useBinding` hook. */
-export type UseEditorCrdtBindingArgs = {
-  editor?: t.Monaco.Editor;
-  doc?: t.Crdt.Ref;
-  path?: t.ObjectPath;
-  /** Sync CRDT fold marks with Monaco. (default = off) */
-  foldMarks?: boolean;
-};
-
-/** An instance of the `useBinding` Monaco-Crdt two-way data binding. */
-export type EditorCrdtBindingHook = Omit<t.EditorCrdtBinding, 'dispose'>;
-
-/**
- * Events:
- */
-
-/** Local CRDT editor change event. */
-export type EditorCrdtLocalChange = {
-  readonly trigger: 'editor' | 'crdt';
-  readonly path: t.ObjectPath;
-  readonly change: { readonly before: string; readonly after: string };
-};
