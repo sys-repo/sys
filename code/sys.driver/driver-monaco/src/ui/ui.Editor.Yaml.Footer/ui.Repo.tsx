334fdf211592b71022a41b985467aeb397da7886
Phil Cockfield <phil@cockfield.net>
2025-08-15 18:40:09 +1200

    tidy up footer icons

--- code/sys.driver/driver-monaco/src/ui/ui.Editor.Yaml.Footer/ui.Repo.tsx
+++ code/sys.driver/driver-monaco/src/ui/ui.Editor.Yaml.Footer/ui.Repo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { type t, Color, Crdt, css, Icons, usePointer } from './common.ts';
+import { type t, Color, Crdt, css, Icons, Str, usePointer } from './common.ts';
 
 export type RepoProps = {
   crdt?: t.YamlEditorFooterCrdt;
@@ -7,10 +7,12 @@ export type RepoProps = {
   style?: t.CssInput;
 };
 
+type P = RepoProps;
+
 /**
  * Component:
  */
-export const Repo: React.FC<RepoProps> = (props) => {
+export const Repo: React.FC<P> = (props) => {
   const { crdt = {} } = props;
   const { repo, visible = true } = crdt;
   if (!repo || !visible) return null;
@@ -38,14 +40,16 @@ export const Repo: React.FC<RepoProps> = (props) => {
       color: theme.fg,
       display: 'grid',
       gridTemplateColumns: 'auto auto',
-      alignItems: 'center',
-      columnGap: 5,
+      placeItems: 'center',
+      columnGap: 8,
     }),
     icon: css({
-      opacity: enabled ? 1 : 0.4,
+      opacity: enabled ? 1 : 0.35,
       transition: 'opacity 200ms ease',
       display: 'grid',
+      gridTemplateColumns: 'auto auto',
       placeItems: 'center',
+      columnGap: 3,
     }),
   };
 
@@ -59,14 +63,43 @@ export const Repo: React.FC<RepoProps> = (props) => {
     />
   );
 
-  const Icon = enabled ? Icons.Network.On : Icons.Network.Off;
+  const tooltip = wrangle.tooltips(repo, enabled);
+  const elIcon = (
+    <div className={styles.icon.class}>
+      <Icons.Person size={14} tooltip={tooltip.peer} />
+      <Icons.Network.Antenna size={14} tooltip={tooltip.network} />
+    </div>
+  );
 
   return (
     <div className={css(styles.base, props.style).class} {...pointer.handlers}>
-      <div className={styles.icon.class}>
-        <Icon size={17} />
-      </div>
       {elRepo}
+      {elIcon}
     </div>
   );
 };
+
+/**
+ * Helpers:
+ */
+const wrangle = {
+  tooltips(repo: t.Crdt.Repo, enabled: boolean) {
+    return {
+      peer: enabled ? wrangle.tooltip.peer(repo, enabled) : 'Offline',
+      network: enabled ? 'Online' : 'Offline',
+    } as const;
+  },
+
+  tooltip: {
+    peer(repo: t.Crdt.Repo, enabled: boolean) {
+      const peers = repo.sync.peers;
+      const title = `Sync Peers${Str.plural(peers.length, '', '(s)')}: `;
+      let names = peers
+        .map((n) => `• ${n}`)
+        .join('\n')
+        .trimEnd();
+      if (peers.length > 1) names = `\n${names}`;
+      return `${title}: ${names}`;
+    },
+  },
+} as const;
