1e391eaba9b3e86d72de79c95a5f3d252bdbb101
Phil Cockfield <phil@cockfield.net>
2025-08-19 19:11:56 +1200

    tidy

--- code/sys.driver/driver-monaco/src/ui/ui.Editor.Yaml.Footer/ui.Repo.tsx
+++ code/sys.driver/driver-monaco/src/ui/ui.Editor.Yaml.Footer/ui.Repo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { type t, Color, Crdt, css, Icons, Str, usePointer } from './common.ts';
+import { type t, Color, Crdt, css } from './common.ts';
 
 export type RepoProps = {
   crdt?: t.YamlEditorFooterCrdt;
@@ -21,31 +21,17 @@ export const Repo: React.FC<P> = (props) => {
    * Hooks:
    */
   const [enabled, setEnabled] = useState(repo.sync.enabled);
-  const pointer = usePointer({
-    onDown(e) {
-      e.cancel();
-
-      const next = !repo.sync.enabled;
-      repo.sync.enabled = next;
-      setEnabled(next);
-    },
-  });
 
   /**
    * Render:
    */
   const theme = Color.theme(props.theme);
   const styles = {
-    base: css({
-      color: theme.fg,
-      display: 'grid',
-      gridTemplateColumns: 'auto auto',
-      placeItems: 'center',
-      columnGap: 8,
-    }),
+    base: css({ color: theme.fg, display: 'grid' }),
     icon: css({
       opacity: enabled ? 1 : 0.35,
       transition: 'opacity 200ms ease',
+
       display: 'grid',
       gridTemplateColumns: 'auto auto',
       placeItems: 'center',
@@ -53,18 +39,8 @@ export const Repo: React.FC<P> = (props) => {
     }),
   };
 
-  const elRepo = (
-    <Crdt.UI.Repo.SyncEnabledSwitch
-      theme={theme.name}
-      repo={repo}
-      localstorage={crdt.localstorage}
-      mode={'switch-only'}
-      onChange={(e) => setEnabled(e.enabled)}
-    />
-  );
-
   return (
-    <div className={css(styles.base, props.style).class} {...pointer.handlers}>
+    <div className={css(styles.base, props.style).class}>
       <Crdt.UI.Repo.SyncEnabledSwitch
         theme={theme.name}
         repo={repo}
