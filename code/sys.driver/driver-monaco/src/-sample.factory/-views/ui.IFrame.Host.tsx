425fb4beecb0be30376c8062fd11c4dbe992115d
Phil Cockfield <phil@cockfield.net>
2025-08-21 13:55:15 +1200

    host - iframe showing loaded URL name

--- code/sys.driver/driver-monaco/src/-sample.factory/-views/ui.IFrame.Host.tsx
+++ code/sys.driver/driver-monaco/src/-sample.factory/-views/ui.IFrame.Host.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { type t, Color, Cropmarks, css } from '../common.ts';
+import { type t, Button, Color, Cropmarks, css, Time } from '../common.ts';
 
 import { IFrame as IFrameView } from '@sys/ui-react-components';
 import type { IFrameSchema } from '../-schemas/mod.ts';
@@ -20,12 +20,31 @@ export const IFrameHost: React.FC<IFrameHostProps> = (props) => {
   const sandbox = data?.sandbox ?? true;
 
   /**
+   * Hooks:
+   */
+  const [url, setUrl] = React.useState<t.StringUrl>('url');
+  const [hrefButton, setHrefButton] = React.useState<t.ButtonFlags>();
+  const [copied, setCopied] = React.useState(false);
+  const [iframe, setIframe] = React.useState<React.RefObject<HTMLIFrameElement | null>>();
+
+  /**
    * Render:
    */
   const theme = Color.theme(props.theme);
   const styles = {
     base: css({ color: theme.fg, display: 'grid' }),
-    body: css({ backgroundColor: Color.WHITE, display: 'grid' }),
+    body: css({
+      position: 'relative',
+      backgroundColor: Color.WHITE,
+      display: 'grid',
+    }),
+    debug: {
+      url: css({ Absolute: [-25, null, null, 0] }),
+      label: css({ fontSize: 10, display: 'grid', gridAutoFlow: 'column', gap: 4 }),
+      key: css({ opacity: 1 }),
+      value: css({ opacity: hrefButton?.over ? 1 : 0.5 }),
+      copied: css({ color: Color.GREEN }),
+    },
   };
 
   const elBody = (
@@ -33,8 +52,32 @@ export const IFrameHost: React.FC<IFrameHostProps> = (props) => {
       <IFrameView
         {...data}
         sandbox={sandbox}
-        onLoad={(e) => console.info(`⚡️ IFrame.onLoad:`, e)}
+        onLoad={(e) => {
+          console.info(`⚡️ IFrame.onLoad:`, e);
+          setUrl(e.href);
+          setIframe(e.ref);
+        }}
       />
+    </div>
+  );
+
+  const elUrl = url && (
+    <div className={styles.debug.url.class}>
+      <div className={styles.debug.label.class}>
+        <span className={styles.debug.key.class}>href:</span>{' '}
+        <Button
+          theme={theme.name}
+          onMouse={(e) => setHrefButton(e.is)}
+          onClick={(e) => {
+            navigator.clipboard.writeText(url ?? '<no url>');
+            setCopied(true);
+            Time.delay(800, () => setCopied(false));
+          }}
+        >
+          <span className={styles.debug.value.class}>{url ?? '<none>'}</span>
+        </Button>
+        {copied && <span className={styles.debug.copied.class}>copied</span>}
+      </div>
     </div>
   );
 
@@ -42,6 +85,7 @@ export const IFrameHost: React.FC<IFrameHostProps> = (props) => {
     <div className={css(styles.base, props.style).class}>
       <Cropmarks theme={theme.name} borderOpacity={0.04} size={{ mode: 'fill', margin: 60 }}>
         {elBody}
+        {elUrl}
       </Cropmarks>
     </div>
   );
