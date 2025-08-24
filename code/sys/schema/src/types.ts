1f64e55b32ce25d896b95196ca3dc82056135819
Phil Cockfield <phil@cockfield.net>
2025-08-24 12:53:17 +1200

    refs

--- code/sys/schema/src/types.ts
+++ code/sys/schema/src/types.ts
@@ -4,5 +4,9 @@
  */
 export type * from './m.Schema/t.ts';
 
+/**
+ * Export from upstream library ("Typebox"):
+ */
 export type { Static, TLiteral, TSchema, TUnion } from '@sinclair/typebox';
+export type { ValueError } from '@sinclair/typebox/errors';
 export type { AssertError } from '@sinclair/typebox/value';
