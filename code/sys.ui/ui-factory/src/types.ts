02f5db2b74c2890315967fb8322317c9d8eba32d
Phil Cockfield <phil@cockfield.net>
2025-08-27 17:27:47 +1200

    export namespace

--- code/sys.ui/ui-factory/src/types.ts
+++ code/sys.ui/ui-factory/src/types.ts
@@ -2,14 +2,24 @@
  * Module types.
  * @module
  */
+import type { t } from './common.ts';
+
 export type * from './m.core/t.ts';
 export type * from './m.react/t.ts';
 export type * from './m.schema/t.ts';
 export type * from './m.tmpl/t.ts';
 
 /**
- * @ui
+ * User Interface:
  */
 export type * from './ui/-sample.react/t.ts';
 export type * from './ui/-sample.tmpl/t.ts';
 export type * from './ui/ui.Error.Validation/t.ts';
+
+/**
+ * Namespace:
+ */
+export namespace UiFactory {
+  /** Type-level inference helper (Schema → Static type). */
+  export type Infer<S extends t.TSchema> = t.Infer<S>;
+}
