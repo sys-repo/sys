63ec51bdbf3a800b649b3d51933c3a285aa5b2b0
Phil Cockfield <phil@cockfield.net>
2025-08-29 11:41:08 +1200

    stub in module

---
+++ code/sys.dev/src/ui.catalogs/concept-player/common/t.ts
@@ -0,0 +1,17 @@
+/**
+ * @system
+ */
+export type * from '@sys/types';
+
+/** User Interface: */
+export type { ColorTheme } from '@sys/color/t';
+export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
+
+/** Type Schema: */
+export type { TBoolean, TObject, TOptional, TString } from '@sys/schema/t';
+export type { Factory, Infer, Plan, ReactRegistration } from '@sys/ui-factory/t';
+
+/**
+ * @local
+ */
+export type * from '../t.ts';
