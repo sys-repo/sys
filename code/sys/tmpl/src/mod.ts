1112148767a7554ffcdd2b16a3b7eef494e82c1b
Phil Cockfield <phil@cockfield.net>
2025-08-29 22:03:47 +1200

    CLI holding pattern

--- code/sys/tmpl/src/mod.ts
+++ code/sys/tmpl/src/mod.ts
@@ -1,6 +1,24 @@
 /**
  * @module
  */
-export { pkg } from './pkg.ts';
+import { c } from '@sys/color/ansi';
+import { pkg } from './pkg.ts';
+
 /** Type library (barrel file). */
 export type * as t from './types.ts';
+
+/**
+ * Exports:
+ */
+export { pkg };
+
+/**
+ * Command-line:
+ */
+if (import.meta.main) {
+  const msg = `🐷 coming soon...command-line template generator (library)`;
+  console.info();
+  console.info(c.gray(c.italic(msg)));
+  console.info(pkg);
+  console.info();
+}
