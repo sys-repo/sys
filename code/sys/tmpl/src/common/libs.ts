30cb2930565fc4b1ab8593d3e2dd5be90a099308
Phil Cockfield <phil@cockfield.net>
2025-08-30 11:56:42 +1200

    remove UI refs

--- code/sys/tmpl/src/common/libs.ts
+++ code/sys/tmpl/src/common/libs.ts
@@ -1,20 +1,12 @@
 /**
- * Standard refs:
+ * @standard refs:
  */
 export { Arr, Err, Is, Num, Obj, Pkg, rx, Str, Time } from '@sys/std';
 
 /**
- * Server refs:
+ * @server refs:
  */
 export { c, Cli } from '@sys/cli';
 export { DenoFile } from '@sys/driver-deno/runtime';
 export { Fs, Path } from '@sys/fs';
 export { Tmpl } from '@sys/tmpl-engine/fs';
-
-/**
- * UI refs:
- */
-export { Color, css } from '@sys/ui-css';
-export { LocalStorage } from '@sys/ui-dom';
-export { Signal } from '@sys/ui-react';
-export { Button, ObjectView } from '@sys/ui-react-components';
