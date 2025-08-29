6dd0873396c7fe9f645bf41773e6e1b2d8031daf
Phil Cockfield <phil@cockfield.net>
2025-08-29 21:19:19 +1200

    entry file rename

--- code/sys.dev/src/-test/entry.tsx
+++ code/sys.dev/src/-test/entry.tsx
@@ -51,7 +51,7 @@ export async function main() {
     /**
      * Harness Catalog:
      */
-    const { makeRoot } = await import('./-catalog.tsx');
+    const { makeRoot } = await import('./-catalog.harness.tsx');
     root.render(makeRoot());
   }
 }
