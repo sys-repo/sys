67ffed952c454146e901be4e07fa791609ef4c89
Phil Cockfield <phil@cockfield.net>
2025-08-30 12:01:30 +1200

    refs (not in src)

--- code/sys/tmpl/-tmpl/common.ts
+++ code/sys/tmpl/-tmpl/common.ts
@@ -6,3 +6,11 @@ import type * as t from './common.t.ts';
 export { t };
 
 export * from '../src/common.ts';
+
+/**
+ * @ui refs:
+ */
+export { Color, css } from '@sys/ui-css';
+export { LocalStorage } from '@sys/ui-dom';
+export { Signal } from '@sys/ui-react';
+export { Button, ObjectView } from '@sys/ui-react-components';
