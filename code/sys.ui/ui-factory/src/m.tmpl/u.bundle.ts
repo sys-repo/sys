24289d26eaafe8c1f71a93edc4237a0712f6cc03
Phil Cockfield <phil@cockfield.net>
2025-09-01 11:06:07 +1200

    tidy

--- code/sys.ui/ui-factory/src/m.tmpl/u.bundle.ts
+++ code/sys.ui/ui-factory/src/m.tmpl/u.bundle.ts
@@ -1,6 +1,6 @@
-import { PATHS, TmplEngine } from './common.ts';
+import { c, Fs, PATHS, TmplEngine } from './common.ts';
 
 if (import.meta.main) {
   const result = await TmplEngine.FileMap.bundle(PATHS.tmpl.source, PATHS.tmpl.json);
-  console.info('Wrote bundle to:', result.file);
+  console.info(`${c.green('Wrote')} bundle to:`, c.gray(Fs.trimCwd(result.file)));
 }
