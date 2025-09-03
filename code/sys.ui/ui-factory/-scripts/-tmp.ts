9e559f73ee7565576837d5c96ca9b8d777ef08fe
Phil Cockfield <phil@cockfield.net>
2025-09-03 12:16:21 +1200

    Update -tmp.ts

--- code/sys.ui/ui-factory/-scripts/-tmp.ts
+++ code/sys.ui/ui-factory/-scripts/-tmp.ts
@@ -1,7 +1,7 @@
 import { Fs } from '@sys/fs';
 
-// const paths = await Fs.glob('./.tmp', { includeDirs: true }).find('*');
-// for (const p of paths) await Fs.remove(p.path, { log: true });
+const paths = await Fs.glob('./.tmp', { includeDirs: true }).find('*');
+for (const p of paths) await Fs.remove(p.path, { log: true });
 
-import { cli } from '@sys/ui-factory/tmpl';
-await cli();
+// import { cli } from '@sys/ui-factory/tmpl';
+// await cli();
