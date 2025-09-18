58a4fb9f57c173b2f7da3e449d0f7eb06ea819f2
Phil Cockfield <phil@cockfield.net>
2025-09-18 16:04:40 +1200

    Update t.ts

--- code/sys.dev/src/catalog.edu/ui/-sample/t.ts
+++ code/sys.dev/src/catalog.edu/ui/-sample/t.ts
@@ -3,9 +3,11 @@ import type { t } from './common.ts';
 /**
  * Component:
  */
-export type SampleEduProps = {
+export type SampleProps = {
   repo?: t.Crdt.Repo;
   path?: t.ObjectPath;
+  signals?: Partial<t.YamlEditorSignals>;
+  localstorage?: t.StringId;
   debug?: boolean;
   theme?: t.CommonTheme;
   style?: t.CssInput;
