2b0af3935db35fa343643f5fda60ac36984fb5a7
Phil Cockfield <phil@cockfield.net>
2025-09-23 11:46:32 +1200

    rename folder

--- code/sys.driver/driver-monaco/src/ui/m.Crdt/common.ts
+++ code/sys.driver/driver-monaco/src/ui/m.Crdt/common.ts
@@ -1,8 +1,8 @@
 import { pkg, Pkg } from '../common.ts';
 
 export * from '../common.ts';
+export { EditorFolding } from '../m.Crdt.Folding/mod.ts';
 export { EditorYaml } from '../m.Yaml/mod.ts';
-export { EditorFolding } from '../m.Folding/mod.ts';
 
 /**
  * Constants:
--- code/sys.driver/driver-monaco/src/ui/mod.ts
+++ code/sys.driver/driver-monaco/src/ui/mod.ts
@@ -4,8 +4,8 @@
  */
 export { Monaco } from '../m.Monaco/mod.ts';
 export { EditorCarets } from './m.Carets/mod.ts';
+export { EditorFolding } from './m.Crdt.Folding/mod.ts';
 export { EditorCrdt } from './m.Crdt/mod.ts';
-export { EditorFolding } from './m.Folding/mod.ts';
 export { EditorYaml } from './m.Yaml/mod.ts';
 export { MonacoEditor } from './ui.Editor.Monaco/mod.ts';
 export { YamlEditor } from './ui.Editor.Yaml/mod.ts';
