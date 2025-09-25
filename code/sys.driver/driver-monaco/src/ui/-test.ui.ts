b70df00c96c16976078116115c02a857d10d054d
Phil Cockfield <phil@cockfield.net>
2025-09-25 14:18:38 +1200

    update sync-server URL

--- code/sys.driver/driver-monaco/src/ui/-test.ui.ts
+++ code/sys.driver/driver-monaco/src/ui/-test.ui.ts
@@ -22,7 +22,8 @@ export function createRepo() {
   const repo = Crdt.repo({
     storage: { database: 'dev.crdt' },
     network: [
-      { ws: 'waiheke.sync.db.team' },
+      // { ws: 'waiheke.sync.db.team' },
+      { ws: 'sync.db.team' },
       isDev && { ws: 'localhost:3030' },
       qsSyncServer && { ws: qsSyncServer },
     ],
