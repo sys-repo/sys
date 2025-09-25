c309aac1e0bdb8f6443a92e50b72865adbd44aae
Phil Cockfield <phil@cockfield.net>
2025-09-26 08:13:43 +1200

    rename

--- code/sys.driver/driver-monaco/src/-test/-specs.ts
+++ code/sys.driver/driver-monaco/src/-test/-specs.ts
@@ -10,8 +10,8 @@ export const ns = 'driver.monaco';
  */
 export const Specs = {
   [`${ns}: ui.MonacoEditor`]: () => import('../ui/ui.MonacoEditor/-spec/-SPEC.tsx'),
-  [`${ns}: ui.MonacoEditor ← (CRDT binding)`]: () => import('../ui/m.Crdt/-spec/-SPEC.tsx'),
-  [`${ns}: ui.Editor.Yaml ← (CRDT binding)`]: () => import('../ui/ui.Editor.Yaml/-spec/-SPEC.tsx'),
+  [`${ns}: ui.MonacoEditor ← (binding: CRDT)`]: () => import('../ui/m.Crdt/-spec/-SPEC.tsx'),
+  [`${ns}: ui.Editor.Yaml ← (bindings)`]: () => import('../ui/ui.Editor.Yaml/-spec/-SPEC.tsx'),
   [`${ns}: ui.Editor.Yaml.Footer`]: () => import('../ui/ui.Editor.Yaml.Footer/-spec/-SPEC.tsx'),
   [`${ns}: Sample`]: () => import('../-sample/-spec/-SPEC.tsx'),
 } as t.SpecImports;
