65f08ecfc4ffaca57193520f37a22df91b41bc46
Phil Cockfield <phil@cockfield.net>
2025-09-27 10:56:22 +1200

    tidy

--- deploy/@tdb.fs/src/common/libs.ts
+++ deploy/@tdb.fs/src/common/libs.ts
@@ -1,4 +1,4 @@
 export { Fs, Pkg } from '@sys/fs';
-export { Arr, Err, Is, Num, Obj, rx, Str, Time } from '@sys/std';
+export { Arr, Err, Is, Num, Obj, Str, Time } from '@sys/std';
 export { TmplEngine } from '@sys/tmpl-engine';
 export { Signal } from '@sys/ui-react';
