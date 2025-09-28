853f4d97d821d49b0c5e48b040c9d2e89f0bc57f
Phil Cockfield <phil@cockfield.net>
2025-09-29 10:44:13 +1300

    tidy

--- code/sys.driver/driver-monaco/src/m.events/u.Filter.ts
+++ code/sys.driver/driver-monaco/src/m.events/u.Filter.ts
@@ -6,27 +6,28 @@ import { type t, Rx } from './common.ts';
 export const Filter: t.EventBusFilterLib = {
   isKind: _isKind,
   hasPrefix: _hasPrefix,
-  ofKind:
-    (...kinds) =>
-    (src) =>
-      src.pipe(Rx.filter(_isKind(...kinds))),
-  ofPrefix: (prefix) => (src) => src.pipe(Rx.filter(_hasPrefix(prefix))),
+  ofPrefix(prefix) {
+    return (src) => src.pipe(Rx.filter(_hasPrefix(prefix)));
+  },
+  ofKind(...kinds) {
+    return (src) => src.pipe(Rx.filter(_isKind(...kinds)));
+  },
 };
 
 /**
  * Helpers:
  */
 function _isKind<K extends t.EditorEvent['kind']>(...kinds: readonly K[]) {
-  return (e: t.EditorEvent): e is Extract<t.EditorEvent, { kind: K }> => inSet(kinds)(e.kind);
+  type T = Extract<t.EditorEvent, { kind: K }>;
+  return (e: t.EditorEvent): e is T => inSet(kinds)(e.kind);
 }
 
 function _hasPrefix<P extends string>(prefix: P) {
-  return (e: t.EditorEvent): e is Extract<t.EditorEvent, { kind: `${P}${string}` }> =>
-    e.kind.startsWith(prefix);
+  type T = Extract<t.EditorEvent, { kind: `${P}${string}` }>;
+  return (e: t.EditorEvent): e is T => e.kind.startsWith(prefix);
 }
 
 /** Helper: Array.includes for string-literal unions without widening. */
-const inSet =
-  <S extends string>(set: readonly S[]) =>
-  (x: string): x is S =>
-    (set as readonly string[]).includes(x);
+function inSet<S extends string>(set: readonly S[]) {
+  return (x: string): x is S => (set as readonly string[]).includes(x);
+}
