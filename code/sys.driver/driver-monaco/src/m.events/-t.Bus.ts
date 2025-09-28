9ad54146ab8efdd293a7ecac37383b72a06d128d
Phil Cockfield <phil@cockfield.net>
2025-09-29 10:01:48 +1300

    tmp

--- code/sys.driver/driver-monaco/src/m.events/-t.Bus.ts
+++ code/sys.driver/driver-monaco/src/m.events/-t.Bus.ts
@@ -3,13 +3,14 @@ import type { t } from './common.ts';
 /** Flags representing async schedule to fire on. */
 export type EmitSchedule = 'sync' | 'micro' | 'macro' | 'raf';
 
-/** Editor event-bus (Subject). */
-export type EditorEventBus = t.Subject<t.EditorEvent>;
-/** Editor events observable. */
-export type EditorEventObservable = t.Observable<t.EditorEvent>;
+/** Type guards (reusable as predicates or inside Rx filter) */
+// export const isKind =
+//   <E extends WithKind, K extends E['kind']>(...kinds: readonly K[]) =>
+//   (e: E): e is ByKind<E, K> =>
+//     kinds.includes(e.kind as K);
 
 /**
- * Event bus helpers:
+ * Event event-bus helpers.
  */
 export type EventBusLib = {
   /** Generate a new event-bus subject. */
@@ -24,3 +25,13 @@ export type EventBusLib = {
    */
   emit(bus$: t.EditorEventBus, evt: t.EditorEvent, schedule?: EmitSchedule): void;
 };
+
+/**
+ * Common filters for the event-bus.
+ */
+export type EventBusFilterLib = {
+  // crdt:
+};
+
+// import { Rx } from './common.ts';
+// const m = Rx.filter((e) => e.kind.startsWith('editor:crdt:'))
