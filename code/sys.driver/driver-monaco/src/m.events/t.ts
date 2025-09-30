ab9640b537df26b2c8669a5a5f3acb1f02aecfbf
Phil Cockfield <phil@cockfield.net>
2025-09-30 13:47:53 +1300

    tidy

--- code/sys.driver/driver-monaco/src/m.events/t.ts
+++ code/sys.driver/driver-monaco/src/m.events/t.ts
@@ -1,3 +1,23 @@
-export type * from './t.event.bus.ts';
+import type { t } from './common.ts';
 export type * from './t.events.def.ts';
-export type * from './t.observable.ts';
+
+/** Editor event-bus (Subject). */
+export type EditorEventBus = t.Subject<t.EditorEvent>;
+
+/** Editor events observable. */
+export type EditorEventObservable = t.Observable<t.EditorEvent>;
+
+/** Event filtering on editor events. */
+export type EditorBusFilterLib = t.EventFilterLib<t.EditorEvent>;
+
+/**
+ * Event-bus helpers.
+ */
+export type EditorBusLib = {
+  /** Event filtering tools. */
+  readonly Filter: EditorBusFilterLib;
+  /** Generate a new event-bus subject. */
+  make(): t.EditorEventBus;
+  /** Emit an event to the bus on a chosen async-schedule. */
+  emit: t.EmitEvent<t.EditorEvent>;
+};
