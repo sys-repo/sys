766746cc779c62dda5f0ef484cb0440a3230c282
Phil Cockfield <phil@cockfield.net>
2025-10-10 17:30:16 +1300

    Update -test.ts

--- code/sys.dev/src/catalog.edu/-test.ts
+++ code/sys.dev/src/catalog.edu/-test.ts
@@ -1,11 +1,13 @@
 export {
+  DomMock,
+  Testing,
   c,
   describe,
-  DomMock,
   expect,
   expectError,
   expectTypeOf,
   it,
-  Testing,
 } from '@sys/testing/server';
+export { act, renderHook } from '@testing-library/react';
+
 export * from './common.ts';
