cba19fe5cd88746a8d1917bc05f2fbfe24338244
philcockfield <phil@cockfield.net>
2025-10-12 12:40:41 +1300

    tidy

--- code/sys.dev/src/catalog.edu/m.slug/mod.ts
+++ code/sys.dev/src/catalog.edu/m.slug/mod.ts
@@ -4,6 +4,6 @@
  */
 export type { CatalogTraitId } from './schema.trait.registry/m.ids.ts';
 
-export { SlugSchema, TraitBindingSchema, TraitDefSchema } from './schema.slug/mod.ts';
-export { TraitRegistryDefault } from './schema.trait.registry/mod.ts';
-export { VideoPlayerPropsSchema, VideoRecorderPropsSchema } from './schema.traits/mod.ts';
+export * from './schema.trait.registry/mod.ts';
+export * from './schema.traits/mod.ts';
+export * from './schema.validation/mod.ts';
