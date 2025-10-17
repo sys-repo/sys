3599c59ab8cdbafea5aa39509d43480a970ec4a2
philcockfield <phil@cockfield.net>
2025-10-17 17:07:04 +1300

    Update -specs.ts

--- code/sys.dev/src/-test/-specs.ts
+++ code/sys.dev/src/-test/-specs.ts
@@ -1,17 +1,17 @@
 /**
- * DevHarness visual specs.
  * @module
+ * DevHarness visual specs.
  */
 import type { t } from './common.ts';
 export const ns = 'sys.dev';
-const nsEdu = `${ns}: catalog.edu`;
+const edu = `${ns}: catalog.edu`;
 
 /**
  * Specs:
  */
 export const Specs = {
-  [`${nsEdu}`]: () => import('../catalog.edu/ui/-sample/-spec/-SPEC.tsx'),
-  [`${nsEdu}: VideoRecorder`]: () => import('../catalog.edu/ui/ui.VideoRecorder/-spec/-SPEC.tsx'),
+  [`${edu}: sample`]: () => import('../catalog.edu/ui/-sample/-spec/-SPEC.tsx'),
+  [`${edu}: VideoRecorder`]: () => import('../catalog.edu/ui/ui.VideoRecorder/-spec/-SPEC.tsx'),
   [`${ns}: catalog.harness`]: () => import('../catalog.harness/-spec/-SPEC.tsx'),
   [`${ns}: sample`]: () => import('../ui/-sample/-spec/-SPEC.tsx'),
 } as t.SpecImports;
