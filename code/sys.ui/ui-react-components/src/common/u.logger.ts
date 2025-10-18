286d5c2933419f3c4a06be2a39cd0f1a51563746
philcockfield <phil@cockfield.net>
2025-10-18 20:11:46 +1300

    Update u.logger.ts

--- code/sys.ui/ui-react-components/src/common/u.logger.ts
+++ code/sys.ui/ui-react-components/src/common/u.logger.ts
@@ -8,6 +8,6 @@
 export function makeLogger(category: string) {
   return (...args: unknown[]) => {
     const time = new Date().toISOString().slice(11, 23);
-    console.log(`%c[${category}] ${time}`, 'color:#0af;font-weight:bold;', ...args);
+    console.info(`%c[${category}] ${time}`, 'color:#0af;font-weight:bold;', ...args);
   };
 }
