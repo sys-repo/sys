afe37790461648f0964cb1589c056a3aa8b68e59
philcockfield <phil@cockfield.net>
2025-10-23 09:17:09 +1300

    tweak tmpl

--- code/-tmpl/-templates/tmpl.m.mod.ui/-spec/-SPEC.Debug.tsx
+++ code/-tmpl/-templates/tmpl.m.mod.ui/-spec/-SPEC.Debug.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { type t, Button, Color, css, D, LocalStorage, Obj, ObjectView, Signal } from '../common.ts';
+import { type t, Color, css, D, LocalStorage, Obj, Signal } from '../common.ts';
+
+import { Button, ObjectView } from '../common.ts';
 
 type P = t.MyComponentProps;
 type Storage = Pick<P, 'theme' | 'debug'>;
