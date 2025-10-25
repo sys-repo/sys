3c98da3a850f0a7f72532c8d05085690f40e7f9f
philcockfield <phil@cockfield.net>
2025-10-25 16:06:49 +1300

    tmpl - Try (export)

--- code/-tmpl/-templates/tmpl.pkg.deno/src/common/libs.ts
+++ code/-tmpl/-templates/tmpl.pkg.deno/src/common/libs.ts
@@ -1,4 +1,4 @@
-export { Arr, Err, Is, Log, Num, Obj, Pkg, Str, Time } from '@sys/std';
+export { Arr, Err, Is, Log, Num, Obj, Pkg, Str, Time, Try } from '@sys/std';
 export { Schedule } from '@sys/std/async';
 export { Rx } from '@sys/std/rx';
 export { Signal } from '@sys/ui-react';
