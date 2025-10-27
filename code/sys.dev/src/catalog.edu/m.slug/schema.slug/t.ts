95bcc6dc88958b82983d29a33cf43a7ecf362a8b
philcockfield <phil@cockfield.net>
2025-10-28 12:52:54 +1300

    type lib export - fix dry

--- code/sys.dev/src/catalog.edu/m.slug/schema.slug/t.ts
+++ code/sys.dev/src/catalog.edu/m.slug/schema.slug/t.ts
@@ -32,3 +32,35 @@ export type SlugTraitBinding = t.Infer<typeof TraitBindingSchema>;
 export type SlugTraitBindingOf<K extends SlugTraitBinding['of']> = Omit<SlugTraitBinding, 'of'> & {
   readonly of: K;
 };
+
+/**
+ * Common schema patterns.
+ */
+export type SlugSchemaPatternLib = {
+  /**
+   * Reference to a CRDT document address.
+   * Accepts either:
+   * - "crdt:<base62-28>/[optional/path]"
+   * - "urn:crdt:<base62-28>/[optional/path]"
+   * - "crdt:create"
+   */
+  crdtRefPattern(): SlugSchemaPattern;
+
+  /**
+   * Stable identifier for traits, aliases, or other named entities.
+   * Accepts strings such as:
+   * - "video"
+   * - "video-player"
+   * - "video.player-01"
+   *
+   * Must begin with a lowercase letter or number,
+   * and may contain lowercase letters, numbers, hyphens, or periods.
+   */
+  idPattern(): SlugSchemaPattern;
+};
+
+/** Definition of a slug pattern */
+export type SlugSchemaPattern = {
+  readonly description: string;
+  readonly pattern: string;
+};
--- code/sys.dev/src/catalog.edu/m.slug/schema.slug/u.Pattern.ts
+++ code/sys.dev/src/catalog.edu/m.slug/schema.slug/u.Pattern.ts
@@ -1,37 +1,17 @@
-type P = {
-  description: string;
-  pattern: string;
-};
+import { type t } from './common.ts';
 
 /**
  * Common Schema Patterns.
  */
-export const Pattern = {
-  /**
-   * Reference to a CRDT document address.
-   * Accepts either:
-   * - "crdt:<base62-28>/[optional/path]"
-   * - "urn:crdt:<base62-28>/[optional/path]"
-   * - "crdt:create"
-   */
-  crdtRefPattern(): P {
+export const Pattern: t.SlugSchemaPatternLib = {
+  crdtRefPattern() {
     return {
       description: `Accepts "crdt:<uuid|base62-28>/[optional/path]" or "urn:crdt:<uuid|base62-28>/[optional/path]" or "crdt:create".`,
       pattern: `^(?:crdt:create|(?:urn:)?crdt:(?:[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}|[A-Za-z0-9]{28})(?:/[A-Za-z0-9._\\-/]*)?)$`,
     };
   },
 
-  /**
-   * Stable identifier for traits, aliases, or other named entities.
-   * Accepts strings such as:
-   * - "video"
-   * - "video-player"
-   * - "video.player-01"
-   *
-   * Must begin with a lowercase letter or number,
-   * and may contain lowercase letters, numbers, hyphens, or periods.
-   */
-  idPattern(): P {
+  idPattern() {
     return {
       description: `Stable identifier. Must start with a lowercase letter or number; may contain lowercase letters, numbers, hyphens, and periods (e.g. "video.player-01").`,
       pattern: '^[a-z0-9][a-z0-9.-]*$',
