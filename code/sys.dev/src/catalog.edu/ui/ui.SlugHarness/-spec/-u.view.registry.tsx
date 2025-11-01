1f8d7c9957a80bc2de889ff77dff9b61fda4d26c
philcockfield <phil@cockfield.net>
2025-11-01 15:15:27 +1300

    logger tidy

--- code/sys.dev/src/catalog.edu/ui/ui.SlugHarness/-spec/-u.view.registry.tsx
+++ code/sys.dev/src/catalog.edu/ui/ui.SlugHarness/-spec/-u.view.registry.tsx
@@ -1,16 +1,16 @@
-import { Color, Crdt, Lens, Log, Obj } from '../common.ts';
+import { Color, Crdt, Lens, Log } from '../common.ts';
 import { makeRegistry } from '../mod.ts';
 import { Sample } from './-ui.Sample.tsx';
 import { Tree } from './-ui.Tree.tsx';
 
-const logInfo = Log.logger('view.registry', { timestamp: null, prefixColor: Color.MAGENTA });
+type O = Record<string, unknown>;
+const logInfo = Log.logger('view:registry', { timestamp: null, prefixColor: Color.MAGENTA });
 
-export const registry = makeRegistry();
-
-registry
+export const registry = makeRegistry()
   .register('foo', (ctx) => <Sample ctx={ctx} prefix={'👋'} />)
   .register('bar', (ctx) => <Sample ctx={ctx} prefix={'🌳'} />)
   .register('programme', (ctx) => {
+    const lens = ctx.doc ? Lens.at<O>(ctx.doc, ctx.path.doc, ctx.path.slug) : undefined;
 
     return <Tree ctx={ctx} theme={ctx.theme} />;
   });
