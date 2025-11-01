93ca75c2cc0bc8b9a898d0c30c7f8190b0821e3c
philcockfield <phil@cockfield.net>
2025-11-01 16:22:36 +1300

    log registry details

--- code/sys.dev/src/catalog.edu/ui/ui.SlugHarness/-spec/-u.view.registry.tsx
+++ code/sys.dev/src/catalog.edu/ui/ui.SlugHarness/-spec/-u.view.registry.tsx
@@ -4,7 +4,11 @@ import { Sample } from './-ui.Sample.tsx';
 import { Tree } from './-ui.Tree.tsx';
 
 type O = Record<string, unknown>;
-const logInfo = Log.logger('view:registry', { timestamp: null, prefixColor: Color.MAGENTA });
+const logInfo = Log.logger('view:registry', {
+  enabled: false,
+  timestamp: null,
+  prefixColor: Color.MAGENTA,
+});
 
 export const registry = makeRegistry()
   .register('foo', (ctx) => <Sample ctx={ctx} prefix={'👋'} />)
@@ -12,5 +16,10 @@ export const registry = makeRegistry()
   .register('programme', (ctx) => {
     const lens = ctx.doc ? Lens.at<O>(ctx.doc, ctx.path.doc, ctx.path.slug) : undefined;
 
+    logInfo('slug renderer factory:');
+    logInfo('- ctx.view:', ctx.view);
+    logInfo(`- ctx.slug:`, Crdt.toObject(ctx.slug));
+    logInfo(`- ctx.slug:/${(ctx.path.slug ?? []).join('/')}`, Crdt.toObject(lens?.get()));
+    logInfo('- ctx:', ctx);
     return <Tree ctx={ctx} theme={ctx.theme} />;
   });
