950686ad474e879fad4f0004538b95936aa708a2
philcockfield <phil@cockfield.net>
2025-11-01 13:54:56 +1300

    Update libs.ts

--- code/sys.dev/src/catalog.edu/common/libs.ts
+++ code/sys.dev/src/catalog.edu/common/libs.ts
@@ -3,6 +3,7 @@
  */
 export { Schema, Type, Value } from '@sys/schema';
 export { Arr, Err, Is, Log, Num, Obj, Pkg, Str, Time, Try, slug } from '@sys/std';
+export { Lens } from '@sys/std/immutable';
 export { Rx } from '@sys/std/rx';
 export { Signal } from '@sys/ui-react';
 export { Yaml } from '@sys/yaml';
