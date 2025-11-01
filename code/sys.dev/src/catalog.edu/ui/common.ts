05a6809f962e1151d2f144616873539d8b0eb244
philcockfield <phil@cockfield.net>
2025-11-02 07:47:05 +1300

    ref - no longer exists

--- code/sys.dev/src/catalog.edu/ui/common.ts
+++ code/sys.dev/src/catalog.edu/ui/common.ts
@@ -8,7 +8,6 @@ export { YamlPipeline } from '../m.yaml/mod.ts';
  * UI:
  */
 export { Crdt } from '@sys/driver-automerge/web/ui';
-export { CommonThemeSchema, CssInputSchema } from '@sys/schema/std/ui';
 export { Color, Style, css } from '@sys/ui-css';
 export { Keyboard, LocalStorage } from '@sys/ui-dom';
 export { usePointer, useSizeObserver } from '@sys/ui-react';
