9606b76ce3f1c1c0d3e27b01822621ae23ba262e
philcockfield <phil@cockfield.net>
2025-11-06 08:09:29 +1300

    tmpl mod

--- code/-tmpl/src/-test.ts
+++ code/-tmpl/src/-test.ts
@@ -1,4 +1,13 @@
-export { c, describe, expect, expectError, Fs, it, Testing } from '@sys/testing/server';
+export {
+  Fs,
+  Testing,
+  c,
+  describe,
+  expect,
+  expectError,
+  expectTypeOf,
+  it,
+} from '@sys/testing/server';
 
 export * from './common.ts';
 export { makeTmpl } from './m.tmpl/u.makeTmpl.ts';
