9d14204a66e9e57d507e97f12739790fcb87fcf5
philcockfield <phil@cockfield.net>
2025-11-06 08:23:32 +1300

    update refs

--- code/sys.dev/src/catalog.edu/common/libs.ts
+++ code/sys.dev/src/catalog.edu/common/libs.ts
@@ -1,10 +1,10 @@
 /**
  * Core:
  */
+export { Lens } from '@sys/immutable/rfc6902';
 export { Schema, Type, Value } from '@sys/schema';
 export { V, toSchema } from '@sys/schema/recipe';
 export { Arr, Delete, Err, Is, Log, Num, Obj, Pkg, Str, Time, Try, isRecord, slug } from '@sys/std';
-export { Lens } from '@sys/std/immutable';
 export { Rx } from '@sys/std/rx';
 export { Signal } from '@sys/ui-react';
 export { Yaml } from '@sys/yaml';
