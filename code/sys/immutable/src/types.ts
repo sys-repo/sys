bba6e24134abb5bda66320efcb7314997e13f66c
philcockfield <phil@cockfield.net>
2025-11-06 07:55:09 +1300

    rename: m.core

--- code/sys/immutable/src/types.ts
+++ code/sys/immutable/src/types.ts
@@ -2,5 +2,5 @@
  * @module
  * @types Type-library module.
  */
-export type * from './core/m.Immutable.Lens/t.ts';
-export type * from './core/m.Immutable/t.ts';
+export type * from './m.core/m.Immutable.Lens/t.ts';
+export type * from './m.core/m.Immutable/t.ts';
