02bff24ee2c05d648845dfd6649ce6665321599e
philcockfield <phil@cockfield.net>
2025-11-08 10:45:31 +1300

    Update mod.ts

--- code/sys/immutable/src/m.worker/mod.ts
+++ code/sys/immutable/src/m.worker/mod.ts
@@ -6,4 +6,4 @@
 import type { t } from './common.ts';
 
 // Placeholder (WIP)
-export const Foo: { msg: string } = { msg: 'placeholder' };
+export const ImmutableWorker: { msg: string } = { msg: '🐷 wip/placeholder' };
