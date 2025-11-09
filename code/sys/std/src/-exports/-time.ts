575430e72277ea812d7bb3b65f5b11274cafcabe
philcockfield <phil@cockfield.net>
2025-11-10 06:29:45 +1300

    exports

---
+++ code/sys/std/src/-exports/-time.ts
@@ -0,0 +1,7 @@
+/**
+ * @module
+ * Tools for working with time and date.
+ */
+export { Date } from '../m.Time.Date/mod.ts';
+export { Duration, Time } from '../m.Time/mod.ts';
+export { Timecode } from '../m.Timecode/mod.ts';
