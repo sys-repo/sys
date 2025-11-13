3bfe377ec00bad3b3db3190b02ae062efb810448
philcockfield <phil@cockfield.net>
2025-11-13 16:19:44 +1300

    tidy

---
+++ code/sys.driver/driver-automerge/src/ui/-test.ui.createRepo.ts
@@ -0,0 +1,30 @@
+/**
+ * @module
+ * Safe: CRDT repository setup for web/workers.
+ * No UI modules leak into this boundary.
+ */
+import { Crdt } from '../-exports/-web.ui/mod.ts';
+import { Url } from '../common.ts';
+
+export { Crdt };
+
+/**
+ * Create "dev" CRDT repository instance:
+ */
+export function createRepo() {
+  const qsSyncServer = Url.parse(location.href).toURL().searchParams.get('ws');
+  const isLocalhost = location.hostname === 'localhost';
+  const isDev = isLocalhost && location.port !== '8080';
+
+  const repo = Crdt.repo({
+    storage: { database: 'dev.crdt' },
+    network: [
+      { ws: 'waiheke.sync.db.team' },
+      // { ws: 'sync.db.team' },
+      isDev && { ws: 'localhost:3030' },
+      qsSyncServer && { ws: qsSyncServer },
+    ],
+  });
+
+  return repo;
+}
--- code/sys.driver/driver-automerge/src/ui/-test.ui.ts
+++ code/sys.driver/driver-automerge/src/ui/-test.ui.ts
@@ -3,31 +3,9 @@
  * Testing tools running in the browser/ui.
  */
 import { Crdt } from '../-exports/-web.ui/mod.ts';
-import { Url } from './common.ts';
 
+export { Dev, Lorem, Spec } from '@sys/ui-react-devharness';
 export { Crdt };
 
-export { expect } from '@sys/std/testing';
-export { Dev, Lorem, Spec } from '@sys/ui-react-devharness';
+export * from './-test.ui.createRepo.ts';
 export * from './common.ts';
-
-/**
- * Create "dev" CRDT repository instance:
- */
-export function createRepo() {
-  const qsSyncServer = Url.parse(location.href).toURL().searchParams.get('ws');
-  const isLocalhost = location.hostname === 'localhost';
-  const isDev = isLocalhost && location.port !== '8080';
-
-  const repo = Crdt.repo({
-    storage: { database: 'dev.crdt' },
-    network: [
-      { ws: 'waiheke.sync.db.team' },
-      // { ws: 'sync.db.team' },
-      isDev && { ws: 'localhost:3030' },
-      qsSyncServer && { ws: qsSyncServer },
-    ],
-  });
-
-  return repo;
-}
