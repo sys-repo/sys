6581b01b791e172c4b9a39ad1000b97bdc76da51
philcockfield <phil@cockfield.net>
2025-11-13 17:34:05 +1300

    tidy

--- code/sys.driver/driver-automerge/src/ui/ui.Repo/-spec.worker/-worker.ts
+++ code/sys.driver/driver-automerge/src/ui/ui.Repo/-spec.worker/-worker.ts
@@ -1,8 +1,9 @@
-import { createRepo } from '../../-test.ui.ts';
-import { Crdt } from '../common.ts';
+import { Crdt } from '../../../-exports/-web/mod.ts'; // NB: pure non-UI (non-react) boundary.
+
+const repo = Crdt.repo({});
+console.info(`[worker] Crdt.Worker.listen: "${repo.id.instance}"`);
 
 /**
- * Single-repo worker host.
+ * Start the CRDT worker message pump.
  */
-const repo = createRepo();
 Crdt.Worker.listen(self, repo);
