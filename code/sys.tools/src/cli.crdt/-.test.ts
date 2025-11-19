56a810bc8552db31fb7b557622e2d5e359fb6c3a
philcockfield <phil@cockfield.net>
2025-11-20 07:12:26 +1300

    delete old tools (crdt)

--- code/sys.tools/src/cli.crdt/-.test.ts
+++ code/sys.tools/src/cli.crdt/-.test.ts
@@ -1,10 +1,6 @@
 import { describe, expect, it } from '../-test.ts';
 import { D } from './common.ts';
-import { CrdtTools } from './mod.ts';
 
 describe(D.toolname, () => {
-  it('API', async () => {
-    const m = await import('@sys/tools/crdt');
-    expect(m.CrdtTools).to.equal(CrdtTools);
-  });
+  it('API', async () => {});
 });
