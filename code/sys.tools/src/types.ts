a4105a70049bf9f437d9a4f4197039866f4cb059
philcockfield <phil@cockfield.net>
2025-11-20 09:47:47 +1300

    Update types.ts

--- code/sys.tools/src/types.ts
+++ code/sys.tools/src/types.ts
@@ -2,13 +2,18 @@
  * @module
  * @types Type-library module.
  */
-export type * from './cli.clipboard/t.ts';
-export type * from './cli.crdt/t.ts';
-export type * from './cli.fs/t.ts';
-export type * from './cli.update/t.ts';
-export type * from './cli.video/t.ts';
 
 /**
  * Command line arguments (argv).
  */
 export type ToolsCliArgs = { help: boolean };
+
+/**
+ * Library.
+ */
+export type * from './cli.clipboard/t.ts';
+export type * from './cli.crdt/t.ts';
+export type * from './cli.fs/t.ts';
+export type * from './cli.update/t.ts';
+export type * from './cli.video/t.ts';
+export type * from './m.ConfigFile/t.ts';
