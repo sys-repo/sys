ad638357452079d28b35db3a80ed4090fc1bd2e9
philcockfield <phil@cockfield.net>
2025-11-21 09:03:14 +1300

    baseline snapshot

--- code/sys.tools/src/cli.crdt/cmd.snapshot/u.process.ts
+++ code/sys.tools/src/cli.crdt/cmd.snapshot/u.process.ts
@@ -1,27 +1,37 @@
-import { type t, Fs, Is, Obj, Str, Time, c, slug } from '../common.ts';
+import { type t, Rx, Fs, Is, Obj, Str, Time, c, slug } from '../common.ts';
 
 const isCrdtUri = (value?: string) => (value || '').trim().startsWith('crdt:');
 const cleanId = (input: string) => (input || '').trim().replace(/^crdt\:/, '');
+
+export type ProcessEvent = {
+  // ???
+};
+
+type Args = {
+  repo: t.Crdt.Repo;
+  id: t.StringId;
+  base: t.StringDir;
+  //
+  dir?: t.StringDir;
+  depth?: number;
+  now?: t.UnixTimestamp;
+  processed?: t.StringId[]; // ← internal DAG tracking
+  //
+  onProgress?: (e: ProcessEvent) => void;
+};
 
 /**
  * Process ("walk") a CRDT reference chain saving each
  * linked "crdt:id" document reference bound in the
  * object-tree.
  */
-export async function process(args: {
-  repo: t.Crdt.Repo;
-  id: t.StringId;
-  root: t.StringDir;
-  //
-  dir?: t.StringDir;
-  depth?: number;
-  now?: t.UnixTimestamp;
-  processed?: t.StringId[];
-}) {
-  const { root, depth = 0, processed = [], repo } = args;
+export async function process(args: Args) {
+  const { base, depth = 0, processed = [], repo } = args;
   const now = args.now ?? Time.now.timestamp;
-  const dir = args.dir ?? Fs.join(root, 'snapshots', `${now}.${slug()}`);
+
   const id = cleanId(args.id);
+  const dir = args.dir ?? Fs.join(base, `crdt.${id}`, `snap.${now}.${slug().slice(3)}`);
+
   const done = () => ({ dir });
   if (processed.includes(id)) return done();
 
@@ -51,11 +61,11 @@ export async function process(args: {
   });
 
   for (const id of refs) {
-    await process({ repo, id, root, dir, now, processed, depth: depth + 1 }); // 🌳 ← recursion
+    await process({ repo, id, base, dir, now, processed, depth: depth + 1 }); // 🌳 ← recursion
   }
 
   async function save(dir: t.StringDir, doc: t.Crdt.Ref, isRoot: boolean) {
-    let filename = `crdt.${doc.id}.json`;
+    let filename = `${doc.id}.json`;
     filename = isRoot ? `-root.${filename}` : filename;
     const out = Fs.join(dir, filename);
     await Fs.writeJson(out, (doc.current ?? {}) as t.JsonMap);
--- code/sys.tools/src/cli.crdt/cmd.snapshot/u.snapshot.ts
+++ code/sys.tools/src/cli.crdt/cmd.snapshot/u.snapshot.ts
@@ -1,17 +1,34 @@
 import { type t, c, Cli, Rx } from '../common.ts';
 import { startRepoWorker } from '../worker/mod.ts';
-import { process } from './u.process.ts';
+import { process, type ProcessEvent } from './u.process.ts';
 
 export async function snapshot(dir: t.StringDir, id: t.StringId) {
+  /**
+   * Prepare CRDT respository on background worker.
+   */
   const spinner = Cli.spinner();
   const repo = await startRepoWorker(dir);
-
   const evt = repo.events();
   const ready$ = evt.ready$.pipe(Rx.take(1));
   await Rx.firstValueFrom(ready$);
   spinner.stop();
 
-  const root = '-backup';
-  const res = await process({ repo, id, root });
+  /**
+   * Process snapshot/backup request.
+   */
+  const progress: ProcessEvent[] = [];
+
+  spinner.start(c.gray('processing...'));
+  const res = await process({
+    repo,
+    id,
+    base: '-backup',
+    onProgress(e) {
+      console.log('progress', e);
+      progress.push(e);
+    },
+  });
+
+  spinner.stop();
 
 }
