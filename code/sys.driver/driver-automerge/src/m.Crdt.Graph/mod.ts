8260c8f783dee83e3cd4476c5139bcad9ff64b16
philcockfield <phil@cockfield.net>
2025-11-21 11:18:50 +1300

    stub in: Crdt.Graph

---
+++ code/sys.driver/driver-automerge/src/m.Crdt.Graph/mod.ts
@@ -0,0 +1,7 @@
+/**
+ * @module
+ * CRDT Graph Utilities
+ *
+ * Provides a generic, repo-backed DAG walker for CRDT documents.
+ */
+import type { t } from './common.ts';
---
+++ code/sys.driver/driver-automerge/src/m.Crdt.Graph/t.ts
@@ -0,0 +1,7 @@
+import type { t } from './common.ts';
+
+/**
+ * CRDT Graph Utilities
+ * Provides a generic, repo-backed DAG walker for CRDT documents.
+ */
+export type CrdtGraphLib = {};
