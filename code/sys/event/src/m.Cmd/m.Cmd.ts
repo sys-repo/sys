e141f4f57a878e6921fabcd8c4c00da807131452
philcockfield <phil@cockfield.net>
2025-11-22 07:49:41 +1300

    initial Cmd implementation

--- code/sys/event/src/m.Cmd/m.Cmd.ts
+++ code/sys/event/src/m.Cmd/m.Cmd.ts
@@ -1,7 +1,10 @@
 import { type t } from './common.ts';
+import { make } from './u.make.ts';
 
 /**
  * Small, transport-agnostic command bus providing typed
  * request/response over any MessagePort-like endpoint.
  */
-export const Cmd: t.CmdLib = {};
+export const Cmd: t.CmdLib = {
+  make,
+};
