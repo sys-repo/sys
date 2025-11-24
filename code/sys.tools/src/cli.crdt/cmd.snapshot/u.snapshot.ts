3a297792284439d2d321e09f95661668793a202f
philcockfield <phil@cockfield.net>
2025-11-24 14:26:12 +1300

    file rename

--- code/sys.tools/src/cli.crdt/cmd.snapshot/u.snapshot.ts
+++ code/sys.tools/src/cli.crdt/cmd.snapshot/u.snapshot.ts
@@ -1,7 +1,7 @@
 import { type t, c, Cli, Crdt, Fs, Str, Time } from '../common.ts';
 import { startRepoWorker } from '../worker/mod.ts';
 import { calcAndSaveDist } from './u.calcAndSaveDist.ts';
-import { process } from './u.snapshot.process.ts';
+import { process } from './u.process.ts';
 
 const Tree = Cli.Fmt.Tree;
 
