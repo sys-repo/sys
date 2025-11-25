9360c56e0fed0afe7c63ec5a7f56ae5389ecb20e
philcockfield <phil@cockfield.net>
2025-11-25 22:33:57 +1300

    Update u.findTasks.ts

--- code/sys.tools/src/cli.crdt/cmd.tasks/u.findTasks.ts
+++ code/sys.tools/src/cli.crdt/cmd.tasks/u.findTasks.ts
@@ -1,5 +1,5 @@
 import { type t, Cli, Str, Rx, slug, Obj } from '../common.ts';
-import { startRepoWorker } from '../worker/mod.ts';
+import { startRepoOnWorker } from '../worker/mod.ts';
 
 const Tree = Cli.Fmt.Tree;
 
@@ -9,7 +9,7 @@ export async function findTasks(dir: t.StringDir, id: t.StringId) {
    * Prepare CRDT repository on background worker.
    */
   const spinner = Cli.spinner();
-  const repo = await startRepoWorker(dir);
+  const repo = await startRepoOnWorker(dir);
   spinner.stop();
 
 }
