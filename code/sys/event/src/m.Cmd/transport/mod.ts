4d66a50b992aceec9fb30ed889f78a8cd120ba37
philcockfield <phil@cockfield.net>
2025-11-26 18:47:55 +1300

    Update mod.ts

--- code/sys/event/src/m.Cmd/transport/mod.ts
+++ code/sys/event/src/m.Cmd/transport/mod.ts
@@ -1,6 +1,8 @@
 import type { t } from '../common.ts';
 import { fromWebSocket } from './u.from.WebSocket.ts';
 
+export { fromWebSocket };
+
 /**
  * Transport adapters for wiring Cmd to various message endpoints.
  */
