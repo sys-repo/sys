4b4adf6a08c303ceebfce58b00f408b0703c2485
philcockfield <phil@cockfield.net>
2025-11-29 13:57:06 +1300

    Update -u.tmp.ts

--- code/sys.tools/src/cli.crdt/-u.tmp.ts
+++ code/sys.tools/src/cli.crdt/-u.tmp.ts
@@ -1,7 +1,7 @@
 import { type t, Str, Is, Yaml, Obj, c, Cli, Cmd, Crdt, D, Time } from './common.ts';
 import { RepoProcess } from './cmd.daemon.repo/mod.ts';
 
-import { swapoutRef } from './u.patch/u.swapoutRef.ts';
+// import { swapoutRef } from './u.patch/u.swapoutRef.ts';
 // import { normalizeAliases } from './u.patch/u.normalizeAliases.ts';
 // import { AliasResolver } from '@sys/std/alias';
 
@@ -16,9 +16,9 @@ export async function tmp(dir: t.StringDir, doc: t.Crdt.Id) {
   const Qi = '21JvXzARPYFXDVMag3x4UhLgHcQi';
   const Gf = '28k1CyQUNXnx74LhBoyvP2kif4GF';
   const dryRun = false;
-  const res = await swapoutRef({ cmd, doc, from: Qi, to: Gf, dryRun });
+  // const res = await swapoutRef({ cmd, doc, from: Qi, to: Gf, dryRun });
   // const res = await swapoutRef({ cmd, doc, from: Gf, to: Qi, dryRun });
-  console.log('res', res);
+  // console.log('res', res);
 
   // const res = await normalizeAliases(cmd, doc);
   // console.log('res', res);
