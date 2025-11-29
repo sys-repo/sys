396634a6c90f53b796b45cbf19a85a7ccc573787
philcockfield <phil@cockfield.net>
2025-11-29 13:30:05 +1300

    fix build

--- code/sys.tools/src/cli.crdt/-u.tmp.ts
+++ code/sys.tools/src/cli.crdt/-u.tmp.ts
@@ -1,58 +1,28 @@
-import { type t, Yaml, Obj, c, Cli, Cmd, Crdt, D, Time } from './common.ts';
-import { RepoProcess } from './cmd.repo.daemon/mod.ts';
-import { Fmt } from './u.fmt.ts';
+import { type t, Str, Is, Yaml, Obj, c, Cli, Cmd, Crdt, D, Time } from './common.ts';
+import { RepoProcess } from './cmd.daemon.repo/mod.ts';
 
+import { swapoutRef } from './u.patch/u.swapoutRef.ts';
+import { normalizeAliases } from './u.patch/u.normalizeAliases.ts';
 import { AliasResolver } from '@sys/std/alias';
 
 type O = Record<string, unknown>;
 type Client = t.Crdt.Cmd.Client;
 
-export async function tmp(dir: t.StringDir, root: t.Crdt.Id) {
+export async function tmp(dir: t.StringDir, doc: t.Crdt.Id) {
   const port = D.port.repo;
   const cmd = await RepoProcess.tryClient(port);
-  console.log('cmd', cmd);
   if (!cmd) return;
 
-  // const spinner = Cli.spinner();
-  const skipped: t.Crdt.Graph.WalkSkipArgs[] = [];
-  const processed: t.Crdt.Id[] = [];
+  const Qi = '21JvXzARPYFXDVMag3x4UhLgHcQi';
+  const Gf = '28k1CyQUNXnx74LhBoyvP2kif4GF';
+  const dryRun = false;
+  const res = await swapoutRef({ cmd, doc, from: Qi, to: Gf, dryRun });
+  // const res = await swapoutRef({ cmd, doc, from: Gf, to: Qi, dryRun });
+  console.log('res', res);
 
-  function process(doc: t.Crdt.Ref) {
-    const yaml = Obj.Path.get<string>(doc.current, ['slug']);
-    const obj = Yaml.parse<O>(yaml).data ?? {};
-    const resolver = AliasResolver.make(obj, { alias: ['alias'] });
+  // const res = await normalizeAliases(cmd, doc);
+  // console.log('res', res);
+  console.log(`-------------------------------------------`);
 
-    console.log();
-    console.info(Fmt.prettyUri(doc.id));
-    console.log('resolver.alias', resolver.alias);
-  }
-
-  /**
-   * Process (walk the graph)
-   */
-  async function walk(cmd: Client) {
-    await Crdt.Graph.walk({
-      id: root,
-      processed,
-      async load(id) {
-        const { doc } = await cmd.send('doc:current', { doc: id });
-        return doc ?? undefined;
-      },
-      async onDoc(e) {
-        process(e.doc);
-      },
-      onSkip: (e) => skipped.push(e),
-      onRefs(e) {},
-    });
-  }
-
-  try {
-    const startedAt = Time.now.timestamp;
-    // spinner.start(Fmt.spinnerText('walking graph...'));
-    await walk(cmd);
-    // spinner.stop();
-    // await print(cmd, startedAt);
-  } finally {
-    cmd.dispose?.(); // Release network resources.
-  }
+  //
 }
