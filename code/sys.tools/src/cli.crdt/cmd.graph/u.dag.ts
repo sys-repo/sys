0dcb817c646faff3cfc72f57df643fcb6001a66f
philcockfield <phil@cockfield.net>
2025-11-30 19:56:57 +1300

    Update u.dag.ts

--- code/sys.tools/src/cli.crdt/cmd.graph/u.dag.ts
+++ code/sys.tools/src/cli.crdt/cmd.graph/u.dag.ts
@@ -5,15 +5,9 @@ import { makeLoad } from './u.load.ts';
 type O = Record<string, unknown>;
 type CmdClient = t.Crdt.Cmd.Client;
 
-export async function buildDocumentDAG(
-  cmd: CmdClient,
-  root: t.Crdt.Id,
-  path: t.ObjectPath,
-  onDoc?: t.DocumentGraphHook,
-) {
+export async function buildDocumentDAG(cmd: CmdClient, root: t.Crdt.Id, path: t.ObjectPath) {
   const load = makeLoad(cmd);
   const discoverRefs = makeDiscoverRefs(path);
-
   return await Crdt.Graph.Dag.build<O>({
     id: root,
     load,
