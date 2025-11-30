e870b074024aac7c0832ca22c6657fb2ff34192c
philcockfield <phil@cockfield.net>
2025-11-30 18:09:34 +1300

    tmpl

--- code/sys.tools/src/cli.crdt/-tmpl/hook.ts
+++ code/sys.tools/src/cli.crdt/-tmpl/hook.ts
@@ -2,13 +2,13 @@ import { Env } from 'jsr:@sys/fs/env';
 import { Obj, Str } from 'jsr:@sys/std';
 import { type t } from 'jsr:@sys/tools';
 
-await Env.init({ silent: true }); // ensure deno environment is setup.
+await Env.init({ silent: true }); // ensure deno environment is setup on first run.
 
 export const onDoc: t.DocumentGraphHook = async (e) => {
   const { cmd } = e;
   const current = e.doc.current;
 
-  // 🐷 Hint: perform actions on each document in the graph here.
+  // 🐷 Hint: perform your actions on each document in the graph here.
 
   if (e.is.root) {
     e.log('hook: root document 👋');
