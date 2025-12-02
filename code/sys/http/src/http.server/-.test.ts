3d414bbc98d403e0030a0407a94ad8f2537648b0
philcockfield <phil@cockfield.net>
2025-12-02 17:59:23 +1300

    tidy tests

--- code/sys/http/src/http.server/-.test.ts
+++ code/sys/http/src/http.server/-.test.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from '../-test.ts';
-import { serveFileWithEtag } from './m.HttpServer/mod.ts';
-import { HttpPull, HttpServer } from './mod.ts';
+import { HttpPull, HttpServer, serveFileWithEtag } from './mod.ts';
+import { Http } from '../http/mod.ts';
 
 describe('HTTP Server', () => {
   it('API', async () => {
     const m = await import('@sys/http/server');
+    expect(m.Http).to.equal(Http);
     expect(m.HttpPull).to.equal(HttpPull);
     expect(m.HttpServer).to.equal(HttpServer);
     expect(m.serveFileWithEtag).to.equal(serveFileWithEtag);
