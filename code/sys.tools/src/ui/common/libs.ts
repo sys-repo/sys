4a46f9fdd6f7d3469343adabf9a186816ff37fa8
philcockfield <phil@cockfield.net>
2025-12-04 19:41:58 +1300

    rip out UI stub in @sys/tool

--- code/sys.tools/src/ui/common/libs.ts
+++
@@ -1,4 +0,0 @@
-export { Color, Style, css } from '@sys/ui-css';
-export { Keyboard, LocalStorage } from '@sys/ui-dom';
-export { Signal, usePointer, useSizeObserver } from '@sys/ui-react';
-export { Button, Cropmarks, ObjectView } from '@sys/ui-react-components';
