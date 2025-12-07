613c434485402594115e7766e8c224b8a143f01d
philcockfield <phil@cockfield.net>
2025-12-08 07:07:07 +1300

    rename

--- code/sys.tools/src/cli.serve/cmd.serve/mod.ts
+++ code/sys.tools/src/cli.serve/cmd.serve/mod.ts
@@ -1,4 +1,4 @@
+export { ensureStartedThenOpen } from './cmd.open.ts';
 export { startServing } from './cmd.serve.ts';
-export { ensureStartedThenOpen } from './cmd.ensureStartedThenOpen.ts';
 export { Mime } from './u.mime.ts';
 export { ServeMenu } from './u.prompt.ts';
