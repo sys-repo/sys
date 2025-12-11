f3cf92c6fd9bce405df828cb762f0fee20b4fb3d
philcockfield <phil@cockfield.net>
2025-12-11 17:31:10 +1300

    rename

--- code/sys.ui/ui-react-components/src/-test/-specs.ts
+++ code/sys.ui/ui-react-components/src/-test/-specs.ts
@@ -45,7 +45,7 @@ export const SpecsComponents = {
   [`${ns}: Player.YouTube`]: () => import('../ui/Player.YouTube/-spec/-SPEC.tsx'),
 
   [`${ns}.media: Recorder`]: () => import('../ui/Media.Recorder/-spec/-SPEC.tsx'),
-  [`${ns}.media: Timecode`]: () => import('../ui/Media.Timecode/-spec.media.playback/-SPEC.tsx'),
+  [`${ns}.media: Timecode`]: () => import('../ui/Media.Timecode/-spec.playback/-SPEC.tsx'),
   [`${ns}.media: Video (Stream)`]: () => import('../ui/Media.Video/-spec/-SPEC.tsx'),
   [`${ns}.media: Devices`]: () => import('../ui/Media.Devices/-spec/-SPEC.tsx'),
   [`${ns}.media: AudioWaveform`]: () => import('../ui/Media.AudioWaveform/-spec/-SPEC.tsx'),
