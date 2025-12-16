2e518079af9eaf0a9523e0d35b18d0bd64649848
philcockfield <phil@cockfield.net>
2025-12-17 11:18:45 +1300

    Update mod.ts

--- code/sys.tools/src/cli.crdt/cmd.doc.index/mod.ts
+++ code/sys.tools/src/cli.crdt/cmd.doc.index/mod.ts
@@ -2,18 +2,4 @@
  * @module
  * Indexing utilities where a CRDT acts as the index of record.
  */
-import { RepoProcess } from '../cmd.repo.daemon/mod.ts';
-import { type t, D } from '../common.ts';
-
-export async function indexDir(
-  cwd: t.StringDir,
-  docid: t.Crdt.Id,
-  path: t.ObjectPath = ['fs:index:dir'],
-) {
-  const port = D.port.repo;
-  const cmd = await RepoProcess.tryClient(port);
-  if (!cmd) return;
-
-
-    `);
-}
+export { indexDir } from './cmd.indexDir.ts';
