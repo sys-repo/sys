bb831032d5763378da2cf7956dc7134a19a99726
philcockfield <phil@cockfield.net>
2025-12-17 08:33:03 +1300

    proper JS function (this) binding

--- code/sys/cli/src/m.cli/m.Input.ts
+++ code/sys/cli/src/m.cli/m.Input.ts
@@ -1,15 +1,14 @@
-import { type t } from './common.ts';
+import { type t, c } from './common.ts';
 import { Prompt } from './m.Prompt.ts';
-import { c } from './common.ts';
 
 export const Input: t.CliInputLib = {
-  Text: { prompt: Prompt.Input.prompt },
-  Confirm: { prompt: Prompt.Confirm.prompt },
-  Number: { prompt: Prompt.Number.prompt },
-  Secret: { prompt: Prompt.Secret.prompt },
-  Toggle: { prompt: Prompt.Toggle.prompt },
-  MultiText: { prompt: Prompt.List.prompt },
-  Select: { prompt: Prompt.Select.prompt },
+  Text: { prompt: Prompt.Input.prompt.bind(Prompt.Input) },
+  Confirm: { prompt: Prompt.Confirm.prompt.bind(Prompt.Confirm) },
+  Number: { prompt: Prompt.Number.prompt.bind(Prompt.Number) },
+  Secret: { prompt: Prompt.Secret.prompt.bind(Prompt.Secret) },
+  Toggle: { prompt: Prompt.Toggle.prompt.bind(Prompt.Toggle) },
+  MultiText: { prompt: Prompt.List.prompt.bind(Prompt.List) },
+  Select: { prompt: Prompt.Select.prompt.bind(Prompt.Select) },
 
   Checkbox: {
     prompt(args) {
