01ae90a7ab6512c3542f5f2f7c71313a6e8b5aab
philcockfield <phil@cockfield.net>
2026-01-03 13:31:18 +1300

    Update t.ts

--- code/sys.ui/ui-react-components/src/ui/Media.Timecode.PlaybackDriver/-dev/-harness/t.ts
+++ code/sys.ui/ui-react-components/src/ui/Media.Timecode.PlaybackDriver/-dev/-harness/t.ts
@@ -4,12 +4,14 @@ import type { t } from './common.ts';
  * Component:
  */
 export type HarnessProps = {
-  debug?: boolean;
   video?: { A: t.VideoPlayerSignals; B: t.VideoPlayerSignals };
   bundle?: t.TimecodePlaybackDriver.Wire.Bundle;
   docid?: t.StringId;
+
+  debug?: boolean;
   layout?: { infopanel?: { bottom?: t.ReactNode } };
   theme?: t.CommonTheme;
   style?: t.CssInput;
+
   onReady?: (e: { readonly controller: t.TimecodePlaybackDriver.TimelineController }) => void;
 };
