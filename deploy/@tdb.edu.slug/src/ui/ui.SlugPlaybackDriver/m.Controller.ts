6da42b757ee5f19551ccc7ab40d54735be01b0f9
philcockfield <phil@cockfield.net>
2026-01-30 16:55:25 +1300

    tidy

--- deploy/@tdb.edu.slug/src/ui/ui.SlugPlaybackDriver/m.Controller.ts
+++ deploy/@tdb.edu.slug/src/ui/ui.SlugPlaybackDriver/m.Controller.ts
@@ -10,21 +10,22 @@ type State = t.SlugPlaybackState;
  */
 export const Controller: t.SlugPlaybackControllerLib = {
   create(props) {
+    const { baseUrl } = props;
     const id = `slug-playback-${slug()}`;
     const ref = Immutable.clonerRef<State>({});
     const controller = EffectController.create({ id, ref, props });
 
-    const setBundle = (bundle: t.TimecodePlaybackDriver.Wire.Bundle | undefined) => {
-      const playback = { ...(controller.current().playback ?? {}), bundle };
+    const setBundle = (bundle?: t.TimecodePlaybackDriver.Wire.Bundle) => {
+      const base = controller.current().playback ?? {};
+      const playback = { ...base, bundle };
       controller.next({ playback });
     };
 
     // Wire effects.
-    const playbackAdapter = makePlaybackAdapter(controller);
     const slugAdapter = makeSlugAdapter(controller);
-
-    attachSlugLoaderEffect(slugAdapter, { baseUrl: props.baseUrl, setBundle });
+    const playbackAdapter = makePlaybackAdapter(controller);
     attachPlaybackDriverEffect(playbackAdapter);
+    attachSlugLoaderEffect(slugAdapter, { baseUrl, setBundle });
 
     return controller;
   },
