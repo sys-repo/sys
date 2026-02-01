bb6f9e598393b77514d5f94d25a06241e0e8f33a
philcockfield <phil@cockfield.net>
2026-02-02 08:19:23 +1300

    rename

--- deploy/@tdb.edu.slug/src/ui/m.effects/t.ts
+++ deploy/@tdb.edu.slug/src/ui/m.effects/t.ts
@@ -1,4 +1,4 @@
 /** Type re-export. */
 export type * from './m.attach.playback/t.ts';
-export type * from './m.attach.slug-tree/t.ts';
+export type * from './m.attach.slug.FileContent/t.ts';
 export type * from './m.attach.slug/t.ts';
