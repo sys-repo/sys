c7bae61f7a7b75d30887ea7597b31254062b8fb1
philcockfield <phil@cockfield.net>
2026-02-01 21:21:42 +1300

    Update -.test.ts

--- deploy/@tdb.edu.slug/src/m.slug.compiler/-test/-.test.ts
+++ deploy/@tdb.edu.slug/src/m.slug.compiler/-test/-.test.ts
@@ -1,9 +1,10 @@
 import { describe, expect, it } from '../-test.ts';
-import { Linter, Slug, Tasks } from '../mod.ts';
+import { Bundler, Linter, Slug, Tasks } from '../mod.ts';
 
 describe(`slug system: compiler`, () => {
   it('API', async () => {
     const m = await import('@tdb/edu-slug/compiler');
+    expect(m.Bundler).to.equal(Bundler);
     expect(m.Linter).to.equal(Linter);
     expect(m.Tasks).to.equal(Tasks);
     expect(m.Slug).to.equal(Slug);
