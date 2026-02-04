8f24a04cbee3c1c11c9daf054a544d3bf74aad10
philcockfield <phil@cockfield.net>
2026-02-05 10:44:10 +1300

    scripts

--- code/sys.tools/-scripts/-tmp.ts
+++ code/sys.tools/-scripts/-tmp.ts
@@ -1,7 +1,6 @@
-import { type t, Fs, Is, c, Fmt } from '../src/common.ts';
+import { Fs, Is, Json, Str } from '../src/common.ts';
 import { backupConfig } from './-tmp.backup-config.ts';
 // import { provisionObiter } from './-tmp.provision-orbiter.ts';
-// import { provisionObiter } from './-tmp.provision/orbiter.ts';
 
 await backupConfig();
 // await provisionObiter();
