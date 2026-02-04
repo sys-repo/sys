1670026c7444f670937779a053ffac8cf8c43a46
philcockfield <phil@cockfield.net>
2026-02-05 12:02:58 +1300

    Update t.ts

--- deploy/@tdb.edu.slug/src/common/t.ts
+++ deploy/@tdb.edu.slug/src/common/t.ts
@@ -11,17 +11,26 @@ export type * from '@sys/types';
 export type * from '@sys/model-slug/t';
 export type * from '@sys/model/timecode/playback/t';
 
+export type { MenuResult, MenuResultKind } from '@sys/cli/t';
 export type { Crdt } from '@sys/driver-automerge/t';
 export type { Graph } from '@sys/immutable/t';
 export type { TSchema, ValueError } from '@sys/schema/t';
-export type { Alias, ObjLens, ShardCount, ShardIndex, ShardStrategy, StrBuilder, Timecode } from '@sys/std/t';
-export type { MenuResult, MenuResultKind } from '@sys/cli/t';
+export type {
+  Alias,
+  ObjLens,
+  ShardCount,
+  ShardIndex,
+  ShardStrategy,
+  StrBuilder,
+  Timecode,
+} from '@sys/std/t';
 
 /** User interface */
 export type { ColorTheme } from '@sys/color/t';
 export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
 export type { KeyboardModifierFlags } from '@sys/ui-dom/t';
 export type {
+  HttpOriginSpecMap,
   KeyValueItem,
   SheetProps,
   TimecodePlaybackDriver,
