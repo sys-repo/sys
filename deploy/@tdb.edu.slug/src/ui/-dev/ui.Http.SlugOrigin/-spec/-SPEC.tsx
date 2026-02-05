571b2613ecc3c401806a8306501935d57004fbea
philcockfield <phil@cockfield.net>
2026-02-05 17:27:15 +1300

    Update -SPEC.tsx

--- deploy/@tdb.edu.slug/src/ui/-dev/ui.Http.SlugOrigin/-spec/-SPEC.tsx
+++ deploy/@tdb.edu.slug/src/ui/-dev/ui.Http.SlugOrigin/-spec/-SPEC.tsx
@@ -25,7 +25,7 @@ export default Spec.describe(D.displayName, async (e) => {
     Dev.Theme.signalEffect(ctx, p.theme, 1);
 
     ctx.subject
-      .size([360, null])
+      .size([450, null])
       .display('grid')
       .render(() => <Root />);
   });
