3a228b40de7b5637d3ab65ca0feac5583c0caf21
philcockfield <phil@cockfield.net>
2026-02-06 16:06:47 +1300

    refactor(driver-vite): remove unused template folder

--- code/sys.driver/driver-vite/src/-tmpl/src/mod.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @module
- */
-export { pkg } from './pkg.ts';
-
-/** Module types. */
-export type * as t from './types.ts';
--- code/sys.driver/driver-vite/src/-tmpl/src/pkg.ts
+++
@@ -1,8 +0,0 @@
-import { Pkg, type t } from '@sys/std';
-import { default as deno } from '../deno.json' with { type: 'json' };
-
-
-/**
- * Package meta-data.
- */
-export const pkg: t.Pkg = Pkg.fromJson(deno);
