9506e56fcce7d3d86445d7c52060bb61dd154015
philcockfield <phil@cockfield.net>
2026-02-07 14:39:37 +1300

    Update t.ts

--- deploy/@tdb.edu.slug/src/ui/-dev/ui.Http.SlugLoader/-spec/t.ts
+++ deploy/@tdb.edu.slug/src/ui/-dev/ui.Http.SlugLoader/-spec/t.ts
@@ -1,16 +1,2 @@
-import type { t } from './common.ts';
-
 export type * from '../../../../common/t.ts';
 export type * from './-SPEC.Debug.tsx';
-
-export type ProbeRun__ = (e: t.ProbeRunArgs__) => Promise<void>;
-export type ProbeRunArgs__ = {
-  readonly is: { readonly local: boolean };
-  readonly origin: t.SlugUrlOrigin;
-  readonly result: (value: unknown) => void;
-};
-
-export type ProbeSpec__ = {
-  readonly label: t.ReactNode | (() => t.ReactNode);
-  readonly run: ProbeRun__;
-};
