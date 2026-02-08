adaa5e67cdf73aaf49d79186ba8a55775b14bd81
philcockfield <phil@cockfield.net>
2026-02-08 17:58:02 +1300

    Update -tmp.ts

--- code/sys.tools/-scripts/-tmp.ts
+++ code/sys.tools/-scripts/-tmp.ts
@@ -3,6 +3,7 @@ import { Fs, Is, Json, Str } from '../src/common.ts';
 import { backupConfig } from './-tmp.backup-config.ts';
 import { runVoice } from './-tmp.voice.ts';
 
-await runVoice();
-// await backupConfig({ from: './.tmp/-config/', to: '~/code.data/-backup.from.sys-tmp/' });
-// await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
+// await runVoice(); // ElevenLabs Demo
+
+await backupConfig({ from: './.tmp/-config/', to: '~/code.data/-backup.from.sys-tmp/' });
+await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
