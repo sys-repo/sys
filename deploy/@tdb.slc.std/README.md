c37d43fdcf5816b83ffb6ef773f2fa081315270b
philcockfield <phil@cockfield.net>
2026-02-09 15:11:38 +1300

    stub in: @tdb/slc-std

---
+++ deploy/@tdb.slc.std/README.md
@@ -0,0 +1,7 @@
+# @tdb/slc-std
+Social Lean Canvas standard libs.
+
+### Example
+```ts
+import { pkg } from 'jsr:@tdb/slc-std';
+```
---
+++ deploy/@tdb.slc.std/src/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * @module
+ * Social Lean Canvas standard libs.
+ */
+export { pkg } from './pkg.ts';
+
+/** Type library (barrel file). */
+export type * as t from './types.ts';
