4c591a331edb258bbb1ff8acbfc92d8204513f27
philcockfield <phil@cockfield.net>
2026-02-09 14:59:04 +1300

    tmpl

--- code/-tmpl/-templates/tmpl.m.mod.ui/t.ts
+++ code/-tmpl/-templates/tmpl.m.mod.ui/t.ts
@@ -3,7 +3,7 @@ import type { t } from './common.ts';
 /**
  *
  */
-export namespace MyComponent {
+export declare namespace MyComponent {
   export type Lib = { readonly UI: t.FC<Props> };
   export type Props = {
     debug?: boolean;
