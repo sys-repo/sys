d74f75d641703fa345d758f0af60d55af3e39c96
philcockfield <phil@cockfield.net>
2026-02-10 18:07:06 +1300

    Update -.test.ts

--- code/sys/std/src/m.EffectController/-test/-.test.ts
+++ code/sys/std/src/m.EffectController/-test/-.test.ts
@@ -1,10 +1,9 @@
-import { type t, describe, it, expect, expectTypeOf } from '../../-test.ts';
+import { describe, expect, it } from '../../-test.ts';
 import { EffectController } from '../mod.ts';
 
 describe('EffectController', () => {
   it('API', async () => {
     const m = await import('@sys/std/effect');
     expect(m.EffectController).to.equal(EffectController);
-    expect(typeof m.EffectController.Causal.mirrorToken).to.eql('function');
   });
 });
