1edd288ba6f10123e91dc14eeb5c641ca7f95f1b
philcockfield <phil@cockfield.net>
2026-02-10 19:31:15 +1300

    Update -specs.ts

--- deploy/@tdb.edu.slug/src/-test/-specs.ts
+++ deploy/@tdb.edu.slug/src/-test/-specs.ts
@@ -13,13 +13,12 @@ export const Specs = {
   [`${ns}: ui.SlugSheetStack`]: () => import('../ui/ui.SlugSheetStack/-spec/-SPEC.tsx'),
   [`${ns}: ui.TreeHost`]: () => import('../ui/ui.TreeHost/-spec/-SPEC.tsx'),
 
+  [`${ns}: ui.TreeDriver.Tmpl`]: () => import('../ui/ui.TreeDriver.Tmpl/-spec/-SPEC.tsx'),
+  [`${ns}: ui.TreeContentDriver`]: () => import('../ui/ui.TreeContentDriver/-spec/-SPEC.tsx'),
   [`${ns}: 🐷 _OLD_: ui.driver.SlugKB`]: () =>
     import('../ui/ui.__Legacy__SlugKbDriver/-spec/-SPEC.tsx'),
   [`${ns}: 🐷 _OLD_: ui.driver.SlugPlayback`]: () =>
     import('../ui/ui.__Legacy__SlugPlaybackDriver/-spec/-SPEC.tsx'),
-
-  [`${ns}: ui.TreeDriver.Tmpl`]: () => import('../ui/ui.TreeDriver.Tmpl/-spec/-SPEC.tsx'),
-  [`${ns}: ui.TreeContentDriver`]: () => import('../ui/ui.TreeContentDriver/-spec/-SPEC.tsx'),
 
   [`${ns}.dev: Http.SlugOrigin`]: () => import('../ui/-dev/ui.Http.SlugOrigin/-spec/-SPEC.tsx'),
   [`${ns}.dev: Http.SlugLoader ← (data cards)`]: () =>
