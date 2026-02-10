01b8f5d366524b8f9dbafccecbc15bf10b1879d3
philcockfield <phil@cockfield.net>
2026-02-11 07:13:04 +1300

    tweak

--- code/sys.ui/ui-react-components/src/ui/ActionProbe/m.Signals.ts
+++ code/sys.ui/ui-react-components/src/ui/ActionProbe/m.Signals.ts
@@ -90,7 +90,7 @@ export const Signals: t.ActionProbeSignalsLib = {
         if (runTitle && !hasTitle) {
           props.result.items.value = [
             { kind: 'title', v: runTitle },
-            { kind: 'hr', thickness: 2 },
+            { kind: 'hr' },
             ...props.result.items.value,
           ];
         }
