b37af5b2445433b5d17981599b5da93b9267af38
philcockfield <phil@cockfield.net>
2026-02-11 22:28:51 +1300

    stub in module

--- deploy/@tdb.edu.slug/src/types.ts
+++ deploy/@tdb.edu.slug/src/types.ts
@@ -8,10 +8,12 @@ export type * from './ui/m.data/t.ts';
 export type * from './ui/m.effects/t.ts';
 
 export type * from './ui/-dev/t.ts';
-export type * from './ui/ui.SlugSheet/t.ts';
-export type * from './ui/ui.SlugSheetStack/t.ts';
+export type * from './ui/ui.Driver.FileContent/t.ts';
+export type * from './ui/ui.Driver.MediaPlayback/t.ts';
 export type * from './ui/ui.Driver.Tree.Tmpl/t.ts';
 export type * from './ui/ui.Driver.TreeContent/t.ts';
+export type * from './ui/ui.SlugSheet/t.ts';
+export type * from './ui/ui.SlugSheetStack/t.ts';
 export type * from './ui/ui.TreeHost/t.ts';
 
 export type * from './ui/ui.__Legacy__SlugKbDriver/t.ts';
---
+++ deploy/@tdb.edu.slug/src/ui/ui.Driver.MediaPlayback/-test/-.test.ts
@@ -0,0 +1,5 @@
+import { describe, expect, expectTypeOf, it } from '../../../-test.ts';
+
+describe(`MediaPlaybackDriver`, () => {
+  it('API', async () => {});
+});
