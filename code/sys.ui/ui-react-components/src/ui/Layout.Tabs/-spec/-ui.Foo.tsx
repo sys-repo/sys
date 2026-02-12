d1132a95757147b71187ddf24a4b7b54257f9a07
philcockfield <phil@cockfield.net>
2026-02-12 21:03:48 +1300

    Update -ui.Foo.tsx

--- code/sys.ui/ui-react-components/src/ui/Layout.Tabs/-spec/-ui.Foo.tsx
+++ code/sys.ui/ui-react-components/src/ui/Layout.Tabs/-spec/-ui.Foo.tsx
@@ -23,22 +23,13 @@ export const Foo: React.FC<FooProps> = (props) => {
       userSelect: 'none',
       backgroundColor: Color.ruby(debug),
       color: theme.fg,
-      padding: 6,
-      display: 'grid',
-    }),
-    body: css({
-      backgroundColor: Color.ruby(0.1),
-      border: `dashed 1px ${Color.alpha(theme.fg, 0.1)}`,
-      borderRadius: 8,
       padding: 20,
     }),
   };
 
   return (
     <div className={css(styles.base, props.style).class}>
-      <div className={styles.body.class}>
-        <ObjectView theme={theme.name} data={tab} name={'tab:item'} expand={1} />
-      </div>
+      <ObjectView theme={theme.name} data={tab} name={'tab:item'} expand={1} />
     </div>
   );
 };
