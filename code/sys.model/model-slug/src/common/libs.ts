027249c3a51602d534d566f6ea094770dfdbd21b
philcockfield <phil@cockfield.net>
2026-02-16 12:31:28 +1300

    Update libs.ts

--- code/sys.model/model-slug/src/common/libs.ts
+++ code/sys.model/model-slug/src/common/libs.ts
@@ -4,4 +4,5 @@ export { toSchema, V } from '@sys/schema/recipe';
 export { PlaybackSchema } from '@sys/model/timecode/playback';
 
 export { Arr, Err, Is, Num, Obj, Pkg, Str } from '@sys/std';
+export { Shard } from '@sys/std/shard';
 export { Url } from '@sys/std/url';
