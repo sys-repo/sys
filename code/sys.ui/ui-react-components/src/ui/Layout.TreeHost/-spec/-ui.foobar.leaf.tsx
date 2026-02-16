b2779c5546a4e9850b19fb91a73e79a09acbc497
philcockfield <phil@cockfield.net>
2026-02-16 18:19:23 +1300

    Update -ui.foobar.leaf.tsx

--- code/sys.ui/ui-react-components/src/ui/Layout.TreeHost/-spec/-ui.foobar.leaf.tsx
+++ code/sys.ui/ui-react-components/src/ui/Layout.TreeHost/-spec/-ui.foobar.leaf.tsx
@@ -15,6 +15,7 @@ export const FooLeaf: React.FC<FooLeafProps> = (props) => {
   const styles = {
     base: css({
       position: 'relative',
+      backgroundColor: Color.ruby(0.1),
       color: theme.fg,
       fontSize: 14,
       display: 'grid',
