2b35874436c3cc58f6adf71a6a693334558c26b7
philcockfield <phil@cockfield.net>
2026-02-19 07:44:36 +1300

    Update u.menu.ts

--- code/sys.tools/src/-tmpl.cli/u.menu.ts
+++ code/sys.tools/src/-tmpl.cli/u.menu.ts
@@ -6,7 +6,7 @@ import { type t, c, Cli, opt } from './common.ts';
 export async function promptTemplateVariant(): Promise<t.__NAME__Tool.TemplateVariant | undefined> {
   while (true) {
     const picked = (await Cli.Input.Select.prompt<t.__NAME__Tool.MenuCmd>({
-      message: 'Template Variant:',
+      message: '\nTemplate Variant:',
       options: [
         opt(`  clone: ${c.green('stateless')} template`, 'option-a:stateless'),
         opt(`  clone: ${c.green('stateful (yaml-config)')} template`, 'option-a:yaml'),
