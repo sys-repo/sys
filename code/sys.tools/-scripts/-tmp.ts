318b8ad55a086c2034061b31bfea146cc9189541
philcockfield <phil@cockfield.net>
2026-02-18 15:53:41 +1300

    Update -tmp.ts

--- code/sys.tools/-scripts/-tmp.ts
+++ code/sys.tools/-scripts/-tmp.ts
@@ -1,7 +1,7 @@
 import { backupConfig } from './-tmp.backup-config.ts';
 import { mdFileToPdf } from './-tmp.pdf.ts';
 
-// await backupConfig({ from: './.tmp/-config/', to: '~/code.data/-backup.from.sys-tmp/' });
-// await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
+await backupConfig({ from: './.tmp/-config/', to: '~/code.data/-backup.from.sys-tmp/' });
+await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
 
-await mdFileToPdf('.tmp/SUMMARY.md');
+// await mdFileToPdf('.tmp/SUMMARY.md');
