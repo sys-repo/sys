fb4f65a691aabf8c5ad214f132da11c2b09b3a57
philcockfield <phil@cockfield.net>
2026-02-19 18:43:47 +1300

    tidy

--- code/sys/cli/src/m.core/m.Fmt.ts
+++ code/sys/cli/src/m.core/m.Fmt.ts
@@ -5,21 +5,9 @@
 import { Format as PathFormat } from '@sys/std/path';
 import { type t, c } from './common.ts';
 import { Table } from './m.Table.ts';
+import { Tree } from './m.Fmt.Tree.ts';
 
 export { c, Color, stripAnsi } from '../common.ts';
-
-export const Tree: t.CliFormatLib['Tree'] = {
-  vert: '│',
-  mid: '├',
-  last: '└',
-  bar: '─',
-  branch(last, extend = 1) {
-    const isLast = Array.isArray(last) ? last[0] === last[1].length - 1 : last;
-    const head = isLast ? Tree.last : Tree.mid;
-    const bar = '─'.repeat(extend);
-    return head + bar;
-  },
-};
 
 export const Path: t.CliFormatLib['Path'] = {
   str: (path) => c.gray(Fmt.path(path, Fmt.Path.fmt())),
