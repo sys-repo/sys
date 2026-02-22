af667e220f49c1efc8db797630914dfd63dfa523
philcockfield <phil@cockfield.net>
2026-02-22 17:50:03 +1300

    tidy

--- code/sys.tools/-scripts/-tmp.ts
+++ code/sys.tools/-scripts/-tmp.ts
@@ -5,5 +5,5 @@ import { resendSample } from './-tmp.resend.ts';
 // await resendSample();
 // await mdFileToPdf('.tmp/SUMMARY.md');
 
+// await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
 await backupConfig({ from: './.tmp/-config/', to: '~/code.data/-backup.from.sys-tmp/' });
-await backupConfig({ from: './.tmp/-config/', to: '../../-config/', snapshot: false, merge: true });
