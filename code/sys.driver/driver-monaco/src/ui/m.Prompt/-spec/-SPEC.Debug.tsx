3888cc6a6935f1b5ea8ebd2ea008d0da85e7acd3
philcockfield <phil@cockfield.net>
2026-02-23 15:26:37 +1300

    tweak

--- code/sys.driver/driver-monaco/src/ui/m.Prompt/-spec/-SPEC.Debug.tsx
+++ code/sys.driver/driver-monaco/src/ui/m.Prompt/-spec/-SPEC.Debug.tsx
@@ -126,16 +126,16 @@ export const Debug: React.FC<DebugProps> = (props) => {
       <hr />
       <Button
         block
-        label={() => `max-lines: ${p.maxLines.value}`}
-        onClick={() => Signal.cycle<number>(p.maxLines, [2, 3, 5, 10])}
-      />
-      <Button
-        block
         label={() => `single-line`}
         onClick={() => {
           p.maxLines.value = 1;
           p.overflow.value = 'clamp';
         }}
+      />
+      <Button
+        block
+        label={() => `max-lines: ${p.maxLines.value}`}
+        onClick={() => Signal.cycle<number>(p.maxLines, [2, 3, 5, 10])}
       />
 
       <hr />
