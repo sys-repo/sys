aa124adefff7c03a7c05847ced11e9ff203d5cca
philcockfield <phil@cockfield.net>
2026-03-04 08:07:32 +1300

    feat(cli.tmpl): delegate argv execution to @sys/tmpl CLI

--- code/sys.tools/src/cli.tmpl/m.cli.ts
+++ code/sys.tools/src/cli.tmpl/m.cli.ts
@@ -1,31 +1,21 @@
-import { type t, D, Fs, Is, done } from './common.ts';
-import { parseArgs } from './u.args.ts';
-import { Fmt } from './u.fmt.ts';
+import { type t, Fs, Is, Process } from './common.ts';
 
 /**
  * Main entry:
  */
 export const cli: t.TmplToolsLib['cli'] = async (cwd, argv) => {
-  const args = parseArgs(argv);
-  const toolname = D.tool.name;
   cwd = cwd ?? Fs.cwd('terminal');
-
-  if (args.help) return void console.info(await Fmt.help(toolname, cwd));
-
-  /* Run */
-  console.info(await Fmt.header(toolname));
-  const res = await run(cwd);
-  console.info(Fmt.signoff(toolname));
-
-  /* Exit */
-  const exit = res.exit === true ? 0 : Is.num(res.exit) ? res.exit : -1;
-  if (exit > -1) Deno.exit(exit);
+  argv = argv ?? [];
+  const res = await run(cwd, argv);
+  if (!res.success && Is.num(res.code)) Deno.exitCode = res.code;
 };
 
 /**
  * Execution:
  */
-async function run(_cwd: t.StringDir): Promise<t.RunReturn> {
-  console.info('No local subcommands yet. Use @sys/tmpl directly.');
-  return done(0);
+async function run(cwd: t.StringDir, argv: string[]) {
+  return await Process.inherit({
+    cwd,
+    args: ['run', '-A', 'jsr:@sys/tmpl', ...argv],
+  });
 }
