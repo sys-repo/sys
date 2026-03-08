258cc4e696001f15ed87fad04a2d0c244880b398
philcockfield <phil@cockfield.net>
2026-03-08 19:37:51 +1300

    bump

--- code/sys.driver/driver-vite/src/-test/vite.sample-bridge-published/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-bridge-published/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.294';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.295';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
