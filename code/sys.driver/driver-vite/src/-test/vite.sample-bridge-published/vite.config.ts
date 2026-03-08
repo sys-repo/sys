f1175a8dd3b8996b67f8e0cce79374de1d0e9120
philcockfield <phil@cockfield.net>
2026-03-08 20:01:24 +1300

    bump (restore full ci)

--- code/sys.driver/driver-vite/src/-test/vite.sample-bridge-published/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-bridge-published/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.295';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.296';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
