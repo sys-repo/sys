f9719eb29a617ee906993305ea5a69277dfdb45d
philcockfield <phil@cockfield.net>
2026-03-08 13:24:14 +1300

    Narrow CI

--- -scripts/-PATHS.ts
+++ -scripts/-PATHS.ts
@@ -19,56 +19,56 @@ export const Paths = {
     'code/sys.tools',
   ],
   all: [
-    /** types: */
-    'code/sys/types',
-
-    /**
-     * sys:
-     */
-    'code/sys/std',
-    'code/sys/crdt.t',
-    'code/sys/color',
-    'code/sys/cli',
-    'code/sys/crypto',
-    'code/sys/immutable',
-    'code/sys/fs',
-
-    'code/sys/testing',
-    'code/sys/process',
-    'code/sys/net',
-    'code/sys/event',
-    'code/sys/http',
-    'code/sys/yaml',
-    'code/sys/schema',
-    'code/sys/monorepo',
-
-    'code/sys/jsr',
-    'code/sys/text',
-    'code/sys/tmpl-engine',
-
-    /**
-     * sys.model:
-     */
-    'code/sys.model/model',
-    'code/sys.model/model-slug',
-
-    /**
-     * sys.ui:
-     */
-    'code/sys.ui/ui-state',
-    'code/sys.ui/ui-css',
-    'code/sys.ui/ui-dom',
-    'code/sys.ui/ui-react',
-    'code/sys.ui/ui-react-devharness',
-    'code/sys.ui/ui-react-components',
-    'code/sys.ui/ui-factory',
-
-    /**
-     * sys.driver:
-     */
-    'code/sys.driver/driver-deno',
-    'code/sys.driver/driver-process',
-    'code/sys.driver/driver-signer',
+    //     /** types: */
+    //     'code/sys/types',
+    //
+    //     /**
+    //      * sys:
+    //      */
+    //     'code/sys/std',
+    //     'code/sys/crdt.t',
+    //     'code/sys/color',
+    //     'code/sys/cli',
+    //     'code/sys/crypto',
+    //     'code/sys/immutable',
+    //     'code/sys/fs',
+    //
+    //     'code/sys/testing',
+    //     'code/sys/process',
+    //     'code/sys/net',
+    //     'code/sys/event',
+    //     'code/sys/http',
+    //     'code/sys/yaml',
+    //     'code/sys/schema',
+    //     'code/sys/monorepo',
+    //
+    //     'code/sys/jsr',
+    //     'code/sys/text',
+    //     'code/sys/tmpl-engine',
+    //
+    //     /**
+    //      * sys.model:
+    //      */
+    //     'code/sys.model/model',
+    //     'code/sys.model/model-slug',
+    //
+    //     /**
+    //      * sys.ui:
+    //      */
+    //     'code/sys.ui/ui-state',
+    //     'code/sys.ui/ui-css',
+    //     'code/sys.ui/ui-dom',
+    //     'code/sys.ui/ui-react',
+    //     'code/sys.ui/ui-react-devharness',
+    //     'code/sys.ui/ui-react-components',
+    //     'code/sys.ui/ui-factory',
+    //
+    //     /**
+    //      * sys.driver:
+    //      */
+    //     'code/sys.driver/driver-deno',
+    //     'code/sys.driver/driver-process',
+    //     'code/sys.driver/driver-signer',
     'code/sys.driver/driver-vite',
     'code/sys.driver/driver-automerge',
     'code/sys.driver/driver-monaco',
