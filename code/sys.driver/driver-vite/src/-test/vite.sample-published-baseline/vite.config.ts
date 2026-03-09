c113235053e19fa0939fcc0fe84ee962f9624ab9
philcockfield <phil@cockfield.net>
2026-03-09 20:57:32 +1300

    bump

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.298';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.299';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.298';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.299';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() =>
