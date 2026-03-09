c371e9b4a9d51af5018e2a2dab01f77fa7e0a64b
philcockfield <phil@cockfield.net>
2026-03-09 20:34:00 +1300

    fix(driver-vite): combine child sys permission flags

--- code/sys.driver/driver-vite/src/m.vite/-test/-wrangle.test.ts
+++ code/sys.driver/driver-vite/src/m.vite/-test/-wrangle.test.ts
@@ -25,9 +25,8 @@ describe('Vite.Wrangle', () => {
     expect(allowWrite).to.include('node_modules/.vite');
     expect(res.args).to.include('--allow-env');
     expect(res.args).to.not.include('--allow-net');
-    expect(res.args).to.include('--allow-sys=homedir');
-    expect(res.args).to.include('--allow-sys=osRelease');
-    expect(res.args.filter((item) => item.startsWith('--allow-sys=')).length).to.eql(2);
+    expect(res.args).to.include('--allow-sys=osRelease,homedir');
+    expect(res.args.filter((item) => item.startsWith('--allow-sys=')).length).to.eql(1);
     expect(res.args).to.include(`--allow-run=${res.env.ESBUILD_BINARY_PATH},${Deno.execPath()}`);
     expect(res.args).to.not.include('--allow-run');
     expect(res.args).to.not.include('-A');
@@ -51,10 +50,9 @@ describe('Vite.Wrangle', () => {
     expect(allowWrite).to.include('node_modules/.vite');
     expect(res.args).to.include('--allow-env');
     expect(res.args).to.include('--allow-net=localhost,127.0.0.1,0.0.0.0');
-    expect(res.args).to.include('--allow-sys=homedir');
-    expect(res.args).to.include('--allow-sys=osRelease');
+    expect(res.args).to.include('--allow-sys=osRelease,homedir');
     expect(res.args).to.include('--allow-sys=networkInterfaces');
-    expect(res.args.filter((item) => item.startsWith('--allow-sys=')).length).to.eql(3);
+    expect(res.args.filter((item) => item.startsWith('--allow-sys=')).length).to.eql(2);
     expect(res.args).to.include(`--allow-run=${res.env.ESBUILD_BINARY_PATH},${Deno.execPath()}`);
     expect(res.args.filter((item) => item.startsWith('--allow-run=')).length).to.eql(1);
   });
