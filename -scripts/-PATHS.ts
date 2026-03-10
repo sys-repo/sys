6bb5f7e0f1b76a5bfcb77d1c9c0a80b56db495e1
philcockfield <phil@cockfield.net>
2026-03-10 20:52:30 +1300

    deps order

--- -scripts/-PATHS.ts
+++ -scripts/-PATHS.ts
@@ -41,9 +41,9 @@ export const Paths = {
     'code/sys/http',
     'code/sys/yaml',
     'code/sys/schema',
+    'code/sys/jsr',
     'code/sys/monorepo',
 
-    'code/sys/jsr',
     'code/sys/text',
     'code/sys/tmpl-engine',
 
