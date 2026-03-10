bcddee60d55f9004e4092528202652dc9cd515b8
philcockfield <phil@cockfield.net>
2026-03-11 10:19:28 +1300

    fix(exports): jsr

--- code/sys/jsr/src/m.client/-.test.ts
+++ code/sys/jsr/src/m.client/-.test.ts
@@ -3,8 +3,10 @@ import { Jsr, Fetch } from './mod.ts';
 
 describe('@sys/jsr/client', () => {
   it('API', async () => {
-    const m = await import('@sys/jsr/client');
-    expect(m.Jsr).to.equal(Jsr);
-    expect(m.Fetch).to.equal(Fetch);
+    const root = await import('@sys/jsr');
+    const client = await import('@sys/jsr/client');
+    expect(root.Jsr).to.equal(Jsr);
+    expect(client.Jsr).to.equal(Jsr);
+    expect(client.Fetch).to.equal(Fetch);
   });
 });
