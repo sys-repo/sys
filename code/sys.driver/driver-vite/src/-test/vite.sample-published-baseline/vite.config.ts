2b88be54b98a65b552539ae766ec6fe48e78661c
philcockfield <phil@cockfield.net>
2026-03-18 08:53:08 +1300

    chore(release): bump @sys workspace versions

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.321';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.322';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
--- code/sys/skills/pkg.ts
+++ code/sys/skills/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@sys/skills', version: '0.0.0' };
+export const pkg: Pkg = { name: '@sys/skills', version: '0.0.1' };
