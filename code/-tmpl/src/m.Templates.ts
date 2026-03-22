17fc8941aed1bc49c1b2030eb09fac4dd899b799
philcockfield <phil@cockfield.net>
2026-03-23 11:19:59 +1300

    refactor(tmpl): rename internal controller template folder to match public m.mod.ui.controller key

--- code/-tmpl/src/m.Templates.ts
+++ code/-tmpl/src/m.Templates.ts
@@ -4,7 +4,7 @@
 export const Templates = {
   'm.mod': () => import('../-templates/tmpl.m.mod/.tmpl.ts'),
   'm.mod.ui': () => import('../-templates/tmpl.m.mod.ui/.tmpl.ts'),
-  'm.mod.ui.controller': () => import('../-templates/tmpl.m.mod.ui.controller-signal/.tmpl.ts'),
+  'm.mod.ui.controller': () => import('../-templates/tmpl.m.mod.ui.controller/.tmpl.ts'),
   pkg: () => import('../-templates/tmpl.pkg.deno/.tmpl.ts'),
   'repo': () => import('../-templates/tmpl.repo/.tmpl.ts'),
 } as const;
@@ -14,7 +14,7 @@ export type TemplateName = keyof typeof Templates;
 export const TemplateSourceRoots: Record<TemplateName, string> = {
   'm.mod': 'm.mod',
   'm.mod.ui': 'm.mod.ui',
-  'm.mod.ui.controller': 'm.mod.ui.controller-signal',
+  'm.mod.ui.controller': 'm.mod.ui.controller',
   pkg: 'pkg.deno',
   repo: 'repo',
 } as const;
