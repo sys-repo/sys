aa06fc3e776d8e7ba8b00ad8e8a5bd6ea1a83a9d
philcockfield <phil@cockfield.net>
2026-03-23 11:30:30 +1300

    fix(ui-react/testing): declare React act environment in shared server test harness

--- code/sys.ui/ui-react/src/m.testing.server/common.ts
+++ code/sys.ui/ui-react/src/m.testing.server/common.ts
@@ -1,2 +1,8 @@
 export { DomMock, Testing } from '@sys/testing/server';
 export * from '../common.ts';
+
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean | undefined;
+}
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
