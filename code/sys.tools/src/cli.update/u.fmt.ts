c9e68b4e64e8b8b685234797445c1e75e333e059
philcockfield <phil@cockfield.net>
2026-03-26 21:52:06 +1300

    refactor(cli.update): show local version before latest in update summary

--- code/sys.tools/src/cli.update/u.fmt.ts
+++ code/sys.tools/src/cli.update/u.fmt.ts
@@ -43,8 +43,8 @@ export const Fmt = {
     const updateReq = upToDate ? '' : c.gray(`← ${c.italic(c.yellow('(update available)'))}`);
 
     table.push([c.gray('Package'), pkg.name]);
-    table.push([c.gray(`  latest`), remote]);
     table.push([c.gray('  local'), `${local}     ${updateReq}`.trim()]);
+    table.push([c.gray(`  latest`), remote]);
     return Str.trimEdgeNewlines(String(table));
   },
 
