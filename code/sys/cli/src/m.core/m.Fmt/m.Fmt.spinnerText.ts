4ba74881412ef579002297afb1dfb7e8b7c3c707
philcockfield <phil@cockfield.net>
2026-03-28 12:53:50 +1300

    refactor(cli): add trailing spacing to spinner text formatting

--- code/sys/cli/src/m.core/m.Fmt/m.Fmt.spinnerText.ts
+++ code/sys/cli/src/m.core/m.Fmt/m.Fmt.spinnerText.ts
@@ -1,4 +1,4 @@
 import { c } from '../common.ts';
 import type { t } from '../common.ts';
 
-export const spinnerText: t.CliFormatLib['spinnerText'] = (text) => c.gray(c.italic(text));
+export const spinnerText: t.CliFormatLib['spinnerText'] = (text) => `${c.gray(c.italic(text))}\n`;
