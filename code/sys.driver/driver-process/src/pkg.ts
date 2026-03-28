4604b4a6943ef4e0b18595357a9dff0da4b81bf1
philcockfield <phil@cockfield.net>
2026-03-28 13:17:54 +1300

    chore(workspace): prepared 6 submodules (44 jsr:publish modules)
    refactor(workspace): tighten spinner spacing above active prep and upgrade progress output

--- code/sys.driver/driver-process/src/pkg.ts
+++ code/sys.driver/driver-process/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@sys/driver-process', version: '0.0.65' };
+export const pkg: Pkg = { name: '@sys/driver-process', version: '0.0.66' };
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.335';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.336';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() =>
