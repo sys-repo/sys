5f740727fde192cfb0ee8ab1cd239b37f3165753
philcockfield <phil@cockfield.net>
2026-04-01 11:27:33 +1300

    core(text): tidy exports

--- code/sys/text/src/-exports/-diff.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @module
- * Helpers for determining differences between text strings.
- */
-export * from '../m.diff/mod.ts';
--- code/sys/text/src/-exports/-gpt.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @module
- * Text tools for working with GPT large-language-models.
- */
-export * from '../m.gpt/mod.ts';
