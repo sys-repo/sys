a48ec818b211f6b1b6cb67063bd75f5b7050159a
philcockfield <phil@cockfield.net>
2026-04-01 11:25:49 +1300

    refactor(text): keep all concrete text capabilities on explicit subpaths

--- code/sys/text/src/m.update/-test/-.test.ts
+++ code/sys/text/src/m.update/-test/-.test.ts
@@ -1,11 +1,9 @@
-import { describe, expect, expectTypeOf, it } from '../../-test.ts';
+import { describe, expect, it } from '../../-test.ts';
 import { Update } from '../mod.ts';
 
 describe(`Update`, () => {
   it('API', async () => {
-    const m = await import('@sys/text');
-    const mm = await import('@sys/text/update');
+    const m = await import('@sys/text/update');
     expect(m.Update).to.equal(Update);
-    expect(mm.Update).to.equal(Update);
   });
 });
--- code/sys/text/src/mod.ts
+++ code/sys/text/src/mod.ts
@@ -6,8 +6,3 @@ export { pkg } from './pkg.ts';
 
 /** Type library (barrel file). */
 export type * as t from './types.ts';
-
-/**
- * Library
- */
-export { Update } from './m.update/mod.ts';
