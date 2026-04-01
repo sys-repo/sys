f7469485b283d315e692801e4c37c1bd5d53e2f1
philcockfield <phil@cockfield.net>
2026-04-01 18:04:20 +1300

    refactor(deploy): rename tmp.proxy to sample.proxy

--- -scripts/-PATHS.ts
+++ -scripts/-PATHS.ts
@@ -63,7 +63,7 @@ export const Paths = {
     'deploy/@tdb.slc.fs',
     'deploy/@tdb.slc.std',
     'deploy/@tdb.edu.slug',
-    'deploy/tmp.proxy',
+    'deploy/sample.proxy',
     // generated:end workspace-topological
   ],
 } as const;
