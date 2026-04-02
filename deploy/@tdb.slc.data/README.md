18634d4e5927d016376eba61484d1e9f3cfa52a9
philcockfield <phil@cockfield.net>
2026-04-02 14:44:01 +1300

    chore(slc-data): scaffold package workspace from repo template

---
+++ deploy/@tdb.slc.data/README.md
@@ -0,0 +1,4 @@
+# @tdb/slc-data
+Data-side SLC tooling.
+
+This package is the working home for SLC content generation, local serving, and related data/UI glue.
---
+++ deploy/@tdb.slc.data/src/-test/-specs.ts
@@ -0,0 +1,13 @@
+/**
+ * @module
+ * DevHarness visual specs.
+ */
+import type { t } from './common.ts';
+export const ns = 'tdb.slc.data';
+
+/**
+ * Specs:
+ */
+export const Specs = {
+  // [`${ns}: name`]: () => import('../ui/component/-spec/-SPEC.tsx'),
+} as t.SpecImports;
