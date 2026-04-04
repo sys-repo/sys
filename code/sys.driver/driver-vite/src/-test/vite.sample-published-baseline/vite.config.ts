bfda7c85a65c2e9043184506d3fb5706a73a2e10
philcockfield <phil@cockfield.net>
2026-04-04 17:17:34 +1300

    chore(workspace): prepared 6 submodules (6 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.342';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.343';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.342';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.343';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() =>
