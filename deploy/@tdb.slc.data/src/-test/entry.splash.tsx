5901c29a389be47fa5147c9c51e9b9e5e731dfb2
philcockfield <phil@cockfield.net>
2026-04-04 19:16:37 +1300

    refactor(slc-data): backport HttpOrigin splash harness

--- deploy/@tdb.slc.data/src/-test/entry.splash.tsx
+++ deploy/@tdb.slc.data/src/-test/entry.splash.tsx
@@ -7,7 +7,8 @@ import React from 'react';
 
 import type * as t from '@sys/types';
 import { Color, css } from '@sys/ui-css';
-import { pkg } from '../pkg.ts';
+import { Button } from './common.ts';
+import { HttpOrigin } from '../ui/mod.ts';
 
 export type SplashProps = { theme?: t.CommonTheme };
 
@@ -18,12 +19,19 @@ export const Splash: React.FC<SplashProps> = (props) => {
   const styles = {
     base: css({ Absolute: 0, backgroundColor: theme.bg, color: theme.fg }),
     center: css({ display: 'grid', placeItems: 'center' }),
-    body: css({ fontSize: 32, fontFamily: 'system-ui' }),
+    body: css({
+      width: 420,
+      display: 'grid',
+      rowGap: 30,
+    }),
   };
 
   return (
     <div className={css(styles.base, styles.center).class}>
-      <div className={styles.body.class}>{`👋 hello ${pkg.name}`}</div>
+      <div className={styles.body.class}>
+        <HttpOrigin.UI theme={theme.name} />
+        <Button theme={theme.name} label={'verify:🐷'} enabled={false} />
+      </div>
     </div>
   );
 };
