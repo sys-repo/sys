3e87a9b61d3d3901338f61cac37b6ff1b382b116
philcockfield <phil@cockfield.net>
2026-04-05 16:00:03 +1200

    refactor(slc-data): move client modules under src/m

--- deploy/@tdb.slc.data/src/ui/-dev/ui.HttpDataCards/-spec.cards/common.ts
+++ deploy/@tdb.slc.data/src/ui/-dev/ui.HttpDataCards/-spec.cards/common.ts
@@ -1,3 +1,3 @@
 export * from '../common.ts';
-export { SlcDataClient } from '../../../../m.client/mod.ts';
+export { SlcDataClient } from '../../../../m/m.client/mod.ts';
 export type * as t from '../t.ts';
