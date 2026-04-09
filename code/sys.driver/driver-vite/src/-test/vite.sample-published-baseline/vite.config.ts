b87aa7cae532d24bf8ae7f5e176117293aa07e30
philcockfield <phil@cockfield.net>
2026-04-09 14:38:44 +1200

    chore(workspace): refreshed 6 workspace packages (32 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.349';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.350';
 import { defineConfig } from 'npm:vite';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
--- deploy/@tdb.slc.data/src/pkg.ts
+++ deploy/@tdb.slc.data/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@tdb/slc-data', version: '0.0.17' };
+export const pkg: Pkg = { name: '@tdb/slc-data', version: '0.0.18' };
