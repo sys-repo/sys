aae76576d7384ad01e675f279f4413080a7bfea9
philcockfield <phil@cockfield.net>
2026-04-12 13:24:17 +1200

    chore(workspace): refreshed 7 workspace packages (46 jsr:publish modules)

--- code/sys.driver/driver-prosemirror/src/pkg.ts
+++ code/sys.driver/driver-prosemirror/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@sys/driver-prosemirror', version: '0.0.183' };
+export const pkg: Pkg = { name: '@sys/driver-prosemirror', version: '0.0.184' };
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.354';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.355';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() =>
