46380c525eafbb572f4319711e2f1c3d8ec0c225
philcockfield <phil@cockfield.net>
2026-04-17 19:48:46 +1200

    fix(driver-vite): align optimize-imports rules with public exports

--- code/sys.driver/driver-vite/src/m.vite.plugins/m.OptimizeImports/u.rules.ts
+++ code/sys.driver/driver-vite/src/m.vite.plugins/m.OptimizeImports/u.rules.ts
@@ -5,7 +5,6 @@ export const DEFAULT_PACKAGE_RULES: readonly t.OptimizeImportsPlugin.PackageRule
     packageId: '@sys/ui-react-devharness',
     imports: [
       { importName: 'useKeyboard', subpath: './hooks', kind: 'value' },
-      { importName: 'useRubberband', subpath: './hooks', kind: 'value' },
     ],
   },
 ] as const;
