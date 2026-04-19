9f157743523b02ca1d1c1ad2d58bdfb37ceb1501
philcockfield <phil@cockfield.net>
2026-04-19 21:29:46 +1200

    fix(std): remove conflicting testing re-export fan-in

--- code/sys/std/src/m.Testing/u.ts
+++ code/sys/std/src/m.Testing/u.ts
@@ -1,4 +1,4 @@
-export * from './u.expectError.ts';
-export * from './u.randomPort.ts';
+export { expectError } from './u.expectError.ts';
+export { randomPort } from './u.randomPort.ts';
 export * from './u.retry.ts';
 export * from './u.until.ts';
