743118a926ecd9cf31d60bb937778d9902059438
philcockfield <phil@cockfield.net>
2026-04-23 10:37:49 +1200

    chore(workspace): refreshed 8 workspace packages (3 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-components/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-components/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.372';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.373';
 import { defineConfig } from 'vite';
 
 export default defineConfig(async () =>
