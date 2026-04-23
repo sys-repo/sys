493e47cc6a3d40c6c322ba2e4784e12062c158d0
philcockfield <phil@cockfield.net>
2026-04-23 14:08:30 +1200

    chore(workspace): refreshed 8 workspace packages (45 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.375';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.376';
 import { defineConfig } from 'vite';
 
 export default defineConfig(async () =>
--- code/sys.ui/ui-react/src/pkg.ts
+++ code/sys.ui/ui-react/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@sys/ui-react', version: '0.0.293' };
+export const pkg: Pkg = { name: '@sys/ui-react', version: '0.0.294' };
