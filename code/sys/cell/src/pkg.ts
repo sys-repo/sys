11c2c4232541f53b46100f9d4911d75489d516e2
philcockfield <phil@cockfield.net>
2026-04-28 13:14:06 +1200

    chore(workspace): refreshed 7 workspace packages (1 jsr:publish module)

--- code/sys/cell/src/pkg.ts
+++ code/sys/cell/src/pkg.ts
@@ -1,3 +1,16 @@
 import type { Pkg } from '@sys/types';
 
-export const pkg: Pkg = { name: '@sys/cell', version: '0.0.0' };
+/**
+ * Package metadata.
+ *
+ * AUTO-GENERATED:
+ *    This file is generated from the package `deno.json`
+ *    during repo prep. See command:
+ *
+ *        cd ./<system-repo-root>
+ *        deno task prep
+ *
+ *    - DO check this file in to source control.
+ *    - Do NOT manually alter this file.
+ */
+export const pkg: Pkg = { name: '@sys/cell', version: '0.0.1' };
