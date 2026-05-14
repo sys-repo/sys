ae7f46ab98f68eecaf3bcb134ef25365528f78a9
Phil Cockfield <phil@cockfield.net>
2026-05-15 11:32:32 +1200

    refactor(cell): pass task config refs through deploy sample

--- code/sys/cell/-sample/cell.deploy/-scripts/deploy.ts
+++ code/sys/cell/-sample/cell.deploy/-scripts/deploy.ts
@@ -3,13 +3,13 @@ import { Deploy } from '@sys/tools/deploy';
 import { Pull } from '@sys/tools/pull';
 
 export const PullViewTask: t.Cell.Task.Endpoint = {
-  run: ({ cwd, paths }) => Pull.run({ cwd, config: paths.config! }),
+  run: ({ cwd, paths }) => Pull.run({ cwd, paths: { config: paths.config! } }),
 };
 
 export const DeployPrepTask: t.Cell.Task.Endpoint = {
-  run: ({ cwd, paths }) => Deploy.stage({ cwd, config: paths.config! }),
+  run: ({ cwd, paths }) => Deploy.stage({ cwd, paths: { config: paths.config! } }),
 };
 
 export const DeployPushTask: t.Cell.Task.Endpoint = {
-  run: ({ cwd, paths }) => Deploy.push({ cwd, config: paths.config! }),
+  run: ({ cwd, paths }) => Deploy.push({ cwd, paths: { config: paths.config! } }),
 };
