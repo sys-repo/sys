9db145405d261ca17c0ad30fe363d336791de25c
Phil Cockfield <phil@cockfield.net>
2026-05-19 15:44:38 +1200

    fix(model): throw native files/fs errors

--- code/sys.model/model/src/m.files.fs/u.error.ts
+++ code/sys.model/model/src/m.files.fs/u.error.ts
@@ -4,5 +4,5 @@ import { Err, type t } from './common.ts';
  * Create a standard Files/fs error.
  */
 export const fail = (kind: t.FilesFs.Error.Kind, message: string) => {
-  return Err.std(message, { name: kind });
+  return Err.normalize(Err.std(message, { name: kind }));
 };
