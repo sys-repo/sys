71a4baec3db0db34cddf9e00a02267158a862b62
Phil Cockfield <phil@cockfield.net>
2026-05-20 20:47:59 +1200

    refactor(model): inline files/static namespace export

--- code/sys.model/model/src/m.files.static/m.FilesStatic.ts
+++
@@ -1,7 +0,0 @@
-import type { t } from './common.ts';
-import { fromDist } from './m.fromDist.ts';
-
-/** Static backing adapters for the Files model. */
-export const FilesStatic: t.FilesStatic.Lib = {
-  fromDist,
-};
--- code/sys.model/model/src/m.files.static/mod.ts
+++ code/sys.model/model/src/m.files.static/mod.ts
@@ -3,9 +3,11 @@
  * Static dist.json backing adapter for the Files model.
  */
 import type { t } from './common.ts';
-import { FilesStatic as Static } from './m.FilesStatic.ts';
+import { fromDist } from './m.fromDist.ts';
 
 export type * from './t.ts';
 
 /** Static backing adapters for the Files model. */
-export const FilesStatic: t.FilesStatic.Lib = Static;
+export const FilesStatic: t.FilesStatic.Lib = {
+  fromDist,
+};
