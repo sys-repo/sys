da4d1ff7d6e80dfa766bd85008eb0324574f774c
Phil Cockfield <phil@cockfield.net>
2026-05-21 20:03:24 +1200

    Update mod.ts

--- code/sys/server/src/m.server.files/mod.ts
+++ code/sys/server/src/m.server.files/mod.ts
@@ -5,9 +5,7 @@
 import type { t } from './common.ts';
 import { WebSocket } from './m.WebSocket/mod.ts';
 
-/** Files server facades over bounded Files model backings. */
-export const FilesServer: t.FilesServer.Lib = {
-  WebSocket,
-};
-
 export type * from './t.ts';
+
+/** Files server facades over bounded Files model backings. */
+export const FilesServer: t.FilesServer.Lib = { WebSocket };
