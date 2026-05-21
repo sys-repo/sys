0c05c9e66a832d3d5c85f69913c90ccd5926a1a3
Phil Cockfield <phil@cockfield.net>
2026-05-21 19:59:18 +1200

    refactor(server): inline files facade module

--- code/sys/server/src/m.server.files/m.FilesServer.ts
+++
@@ -1,7 +0,0 @@
-import type { t } from './common.ts';
-import { WebSocket } from './m.WebSocket/mod.ts';
-
-/** Files server facades over bounded Files model backings. */
-export const FilesServer: t.FilesServer.Lib = {
-  WebSocket,
-};
--- code/sys/server/src/m.server.files/mod.ts
+++ code/sys/server/src/m.server.files/mod.ts
@@ -2,5 +2,12 @@
  * @module
  * Files server facades over bounded Files model backings.
  */
-export { FilesServer } from './m.FilesServer.ts';
+import type { t } from './common.ts';
+import { WebSocket } from './m.WebSocket/mod.ts';
+
+/** Files server facades over bounded Files model backings. */
+export const FilesServer: t.FilesServer.Lib = {
+  WebSocket,
+};
+
 export type * from './t.ts';
