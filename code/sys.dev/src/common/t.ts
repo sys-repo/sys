66fff4fa17193a8ec1debf405567048a5d020482
Phil Cockfield <phil@cockfield.net>
2026-05-22 12:14:17 +1200

    chore(sys.dev): trim retired dev harness leftovers

--- code/sys.dev/src/common/t.ts
+++ code/sys.dev/src/common/t.ts
@@ -1,26 +1,8 @@
 /**
- * @external
- */
-export type { ReactElement, ReactNode } from 'react';
-
-/**
  * @system
  */
 export type * from '@sys/types';
-
-export type { ColorTheme } from '@sys/color/t';
-export type { Static, TSchema } from '@sys/schema/t';
-export type { SpecImports, TestingDir } from '@sys/testing/t';
-export type { CssEdgesInput, CssInput, CssMarginArray, CssProps, CssValue } from '@sys/ui-css/t';
-export type { KeyboardModifierFlags } from '@sys/ui-dom/t';
-export type { ButtonFlags } from '@sys/ui-react-components/t';
-export type { DevCtx } from '@sys/ui-react-devharness/t';
-
-/**
- * @drivers
- */
-export type { Crdt } from '@sys/driver-automerge/t';
-export type { EditorYaml, Monaco } from '@sys/driver-monaco/t';
+export type { SpecImports } from '@sys/testing/t';
 
 /**
  * @local
