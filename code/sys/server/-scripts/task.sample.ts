45497122e86cc4ee44c55861fecc45e08236dbba
Phil Cockfield <phil@cockfield.net>
2026-05-28 16:40:46 +1200

    fix(server): simplify sample task menu title

--- code/sys/server/-scripts/task.sample.ts
+++ code/sys/server/-scripts/task.sample.ts
@@ -3,6 +3,6 @@ import { DenoTask } from '@sys/driver-deno/runtime';
 await DenoTask.Menu.main({
   cwd: '.',
   argv: Deno.args,
-  title: '@sys/server samples',
+  title: '@sys/server',
   include: ['sample:*'],
 });
