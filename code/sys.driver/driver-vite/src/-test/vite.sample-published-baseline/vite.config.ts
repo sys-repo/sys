0c14bee3c6963718cc49611193f9ca3bec0f54bf
Phil Cockfield <phil@cockfield.net>
2026-06-04 08:30:57 +1200

    chore(workspace): refreshed 10 workspace packages (43 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.419';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.420';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
 
--- code/sys.driver/driver-vite/src/pkg.ts
+++ code/sys.driver/driver-vite/src/pkg.ts
@@ -13,4 +13,4 @@ import type { Pkg } from '@sys/types';
  *    - DO check this file in to source control.
  *    - Do NOT manually alter this file.
  */
-export const pkg: Pkg = { name: '@sys/driver-vite', version: '0.0.419' };
+export const pkg: Pkg = { name: '@sys/driver-vite', version: '0.0.420' };
