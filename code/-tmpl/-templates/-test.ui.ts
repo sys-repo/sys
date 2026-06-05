b841b82d3ca9faa70b6ce16b7f9459f849181aca
Phil Cockfield <phil@cockfield.net>
2026-06-05 12:57:23 +1200

    chore(workspace): refresh bump outputs

--- code/-tmpl/-templates/-test.ui.ts
+++ code/-tmpl/-templates/-test.ui.ts
@@ -2,5 +2,5 @@
  * Testing tools running in the browser/ui.
  */
 export { expect } from '@sys/std/testing';
-export { Dev, Spec } from '@sys/ui-react-devharness/react';
+export { Dev, Spec } from '@sys/ui-react-devharness';
 export * from './common.ts';
