7ea7b3c8fd9dfbf5dd9b5474b25522e61bb473bd
Phil Cockfield <phil@cockfield.net>
2026-06-05 14:20:45 +1200

    fix(tmpl): move DevHarness template imports to react lane

--- code/-tmpl/-templates/-test.ui.ts
+++ code/-tmpl/-templates/-test.ui.ts
@@ -2,5 +2,5 @@
  * Testing tools running in the browser/ui.
  */
 export { expect } from '@sys/std/testing';
-export { Dev, Spec } from '@sys/ui-react-devharness';
+export { Dev, Spec } from '@sys/ui-react-devharness/react';
 export * from './common.ts';
--- code/-tmpl/-templates/tmpl.pkg/src/-test/entry.tsx
+++ code/-tmpl/-templates/tmpl.pkg/src/-test/entry.tsx
@@ -42,7 +42,7 @@ export async function main() {
    * DevHarness:
    */
   async function renderDev() {
-    const { render, useKeyboard } = await import('@sys/ui-react-devharness');
+    const { render, useKeyboard } = await import('@sys/ui-react-devharness/react');
     const { Specs } = await import('./-specs.ts');
     const el = await render(pkg, Specs, {
       style: { Absolute: 0 },
