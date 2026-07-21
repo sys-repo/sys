8a3992b72c7a6e0ab9786648e246272ab91313c0
Phil Cockfield <phil@cockfield.net>
2026-07-21 14:17:22 +1200

    chore(workspace): refreshed 10 workspace packages (36 jsr:publish modules)

--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.460';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.461';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
 
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-ui-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.460';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.461';
 
 export default Vite.Config.define(async () =>
   await Vite.Config.app({
