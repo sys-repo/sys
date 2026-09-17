1b9377722bc93d56c23b8d37e392dee6f2817536
Phil Cockfield <phil@cockfield.net>
2026-09-17 12:04:15 +1200

    chore(workspace): refreshed 9 workspace packages (34 jsr:publish modules)

--- code/-tmpl/src/pkg.ts
+++ code/-tmpl/src/pkg.ts
@@ -15,5 +15,5 @@ import type { Pkg } from '@sys/types';
  */
 export const pkg: Readonly<Pkg> = Object.freeze({
   name: '@sys/tmpl',
-  version: '0.0.483',
+  version: '0.0.484',
 });
--- code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
+++ code/sys.driver/driver-vite/src/-test/vite.sample-published-baseline/vite.config.ts
@@ -1,4 +1,4 @@
-import { Vite } from 'jsr:@sys/driver-vite@0.0.476';
+import { Vite } from 'jsr:@sys/driver-vite@0.0.477';
 
 const workspace = `${import.meta.dirname ?? '.'}/deno.json`;
 
